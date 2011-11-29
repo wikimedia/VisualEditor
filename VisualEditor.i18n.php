7e4461922d9c6cfb618b6f74ba1de661492fd460
Raimond Spekking <raymond@users.mediawiki.org>
2011-11-29 20:32:09 +0000

    Localisation updates for core and extension messages from translatewiki.net

--- VisualEditor.i18n.php
+++ VisualEditor.i18n.php
@@ -9,3 +9,31 @@ $messages['en'] = array(
 	'visualeditor-desc' => 'Visual editor for MediaWiki',
 	'visualeditor-sandbox-title' => 'VisualEditor Sandbox',
 );
+
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'visualeditor' => 'WYSIWYG-Editor',
+	'visualeditor-desc' => 'Ermöglicht einen WYSIWYG-Editor',
+	'visualeditor-sandbox-title' => 'WYSIWYG-Editor - Spielwiese',
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'visualeditor' => 'Editor visual',
+	'visualeditor-desc' => 'Editor visual para MediaWiki',
+	'visualeditor-sandbox-title' => 'Zona de probas do editor visual',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'visualeditor' => 'Визуелен уредник',
+	'visualeditor-desc' => 'Визуелен уредник за МедијаВики',
+	'visualeditor-sandbox-title' => 'Песочник — Визуелен уредник',
+);
+
