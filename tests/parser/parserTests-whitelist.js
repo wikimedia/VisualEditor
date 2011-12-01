edf40c616c970dc8a228e0cd1e932f6cf46debb3
Gabriel Wicke <gwicke@users.mediawiki.org>
2011-12-01 11:47:22 +0000

    Make whitelist usage an option; tweak comment a bit

--- tests/parser/parserTests-whitelist.js
+++ tests/parser/parserTests-whitelist.js
@@ -4,8 +4,8 @@
 
 testWhiteList = {};
 
-// The order of li/b is swapped in this test, but the resulting formatting is
-// identical
+// The nesting of italic/bold tags is changed in this test, but the resulting
+// formatting is identical
 testWhiteList['Italics and bold'] = "<ul><li> plain</li><li> plain<i>italic</i>plain</li><li> plain<i>italic</i>plain<i>italic</i>plain</li><li> plain<b>bold</b>plain</li><li> plain<b>bold</b>plain<b>bold</b>plain</li><li> plain<i>italic</i>plain<b>bold</b>plain</li><li> plain<b>bold</b>plain<i>italic</i>plain</li><li> plain<i>italic<b>bold-italic</b>italic</i>plain</li><li> plain<b>bold<i>bold-italic</i>bold</b>plain</li><li> plain<i><b>bold-italic</b>italic</i>plain</li><li> plain<i><b>bold-italic</b></i><b>bold</b>plain</li><li> plain<i>italic<b>bold-italic</b></i>plain</li><li> plain<b>bold<i>bold-italic</i></b>plain</li><li> plain l'<i>italic</i>plain</li><li> plain l'<b>bold</b> plain</li></ul>";
 
 if (typeof module == "object") {
