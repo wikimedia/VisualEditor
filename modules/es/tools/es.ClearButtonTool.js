8dc7f148df7e74b15b51214f238cd0c9d6fabfa9
Trevor Parscal <tparscal@users.mediawiki.org>
2011-12-01 23:10:09 +0000

    Fixed clear button to only clear testStyle and link annotations, and also only be enabled when these kinds of annotations are in the current selection

--- modules/es/tools/es.ClearButtonTool.js
+++ modules/es/tools/es.ClearButtonTool.js
@@ -1,20 +1,22 @@
 es.ClearButtonTool = function( toolbar, name ) {
 	es.ButtonTool.call( this, toolbar, name );
 	this.$.addClass( 'es-toolbarButtonTool-disabled' );
+	this.pattern = /(textStyle\/|link\/).*/;
 };
 
 es.ClearButtonTool.prototype.onClick = function() {
 	var tx = this.toolbar.surfaceView.model.getDocument().prepareContentAnnotation(
 		this.toolbar.surfaceView.currentSelection,
 		'clear',
-		/.*/
+		this.pattern
 	);
 	this.toolbar.surfaceView.model.transact( tx );
 	this.toolbar.surfaceView.clearInsertionAnnotations();
 };
 
 es.ClearButtonTool.prototype.updateState = function( annotations ) {
-	if ( annotations.length === 0 ) {
+	var matchingAnnotations = es.DocumentModel.getMatchingAnnotations( annotations, this.pattern );
+	if ( matchingAnnotations.length === 0 ) {
 		this.$.addClass( 'es-toolbarButtonTool-disabled' );
 	} else {
 		this.$.removeClass( 'es-toolbarButtonTool-disabled' );
