0ef949eed36e44e90113f95dcd1c8ad67e967a9c
Inez Korczynski <inez@users.mediawiki.org>
2011-12-02 01:22:51 +0000

    Small refactoring of updateState in FormatDropdownTool

--- modules/es/tools/es.FormatDropdownTool.js
+++ modules/es/tools/es.FormatDropdownTool.js
@@ -2,20 +2,53 @@ es.FormatDropdownTool = function( toolbar, name ) {
 	es.DropdownTool.call( this, toolbar, name );
 
 	this.formats = [
-		{ 'name': 'Paragraph', 'type' : 'paragraph' },
-		{ 'name': 'Heading Level 1', 'type' : 'heading', 'attributes': { 'level': 1 } },
-		{ 'name': 'Heading Level 2', 'type' : 'heading', 'attributes': { 'level': 2 } },
-		{ 'name': 'Heading Level 3', 'type' : 'heading', 'attributes': { 'level': 3 } },
-		{ 'name': 'Heading Level 4', 'type' : 'heading', 'attributes': { 'level': 4 } },
-		{ 'name': 'Heading Level 5', 'type' : 'heading', 'attributes': { 'level': 5 } },
-		{ 'name': 'Heading Level 6', 'type' : 'heading', 'attributes': { 'level': 6 } },
-		{ 'name': 'Preformatted', 'type' : 'pre' }
+		{ 
+			'name': 'Paragraph',
+			'type' : 'paragraph'
+		},
+		{
+			'name': 'Heading Level 1',
+			'type' : 'heading',
+			'attributes': { 'level': 1 }
+		},
+		{
+			'name': 'Heading Level 2',
+			'type' : 'heading',
+			'attributes': { 'level': 2 }
+		},
+		{
+			'name': 'Heading Level 3',
+			'type' : 'heading',
+			'attributes': { 'level': 3 }
+		},
+		{
+			'name': 'Heading Level 4',
+			'type' : 'heading',
+			'attributes': { 'level': 4 }
+		},
+		{
+			'name': 'Heading Level 5',
+			'type' : 'heading',
+			'attributes': { 'level': 5 }
+		},
+		{
+			'name': 'Heading Level 6',
+			'type' : 'heading',
+			'attributes': { 'level': 6 }
+		},
+		{
+			'name': 'Preformatted',
+			'type' : 'pre'
+		}
 	];
 	
 	this.$select.append( '<option>' );
 
 	for ( var i = 0; i < this.formats.length; i++ ) {
-		$( '<option>' ).val( i ).html( this.formats[i].name ).appendTo( this.$select );
+		$( '<option>' )
+			.val( i )
+			.html( this.formats[i].name )
+			.appendTo( this.$select );
 	}
 };
 
@@ -36,23 +69,23 @@ es.FormatDropdownTool.prototype.onChange = function() {
 es.FormatDropdownTool.prototype.updateState = function( annotations, nodes ) {
 	var format = {
 		'type': nodes[0].getElementType(),
-		'attributes': nodes[0].element.attributes
+		'attributes': nodes[0].getElement().attributes
 	};
 
 	for( var i = 1; i < nodes.length; i++ ) {
-		if ( nodes[i].getElementType() != format.type
-			|| !es.compareObjects( nodes[0].element.attributes, format.attributes ) ) {
+		if ( format.type != nodes[i].getElementType()
+			|| !es.compareObjects( format.attributes, nodes[i].element.attributes ) ) {
 			format = null;
 			break;
 		}
 	}
-
+	
 	if ( format === null ) {
 		this.$select.val( null );
 	} else {
 		for ( var i = 0; i < this.formats.length; i++ ) {
-			if ( this.formats[i].type === format.type
-				&& es.compareObjects( this.formats[i].attributes, format.attributes ) ) {
+			if ( format.type === this.formats[i].type
+				&& es.compareObjects( format.attributes, this.formats[i].attributes ) ) {
 					this.$select.val( i );
 					break;
 			}
