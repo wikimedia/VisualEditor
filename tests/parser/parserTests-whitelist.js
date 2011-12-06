7e1585d3600f2dc133b5862cd265164c743b05fd
Gabriel Wicke <gwicke@users.mediawiki.org>
2011-12-06 22:05:43 +0000

    Add empty tables to the whitelist (legal in HTML5). Also add one more functionally identical italic/bold/link permmutation on the whitelist.

--- tests/parser/parserTests-whitelist.js
+++ tests/parser/parserTests-whitelist.js
@@ -10,6 +10,12 @@ testWhiteList['Italics and bold'] = "<ul><li> plain</li><li> plain<i>italic</i>p
 
 testWhiteList["Bug 2702: Mismatched <i>, <b> and <a> tags are invalid"] = "<p><i><a href=\"http://example.com\">text</a></i><a href=\"http://example.com\"><b>text</b></a><b></b><i>Something <a href=\"http://example.com\">in italic</a></i><i>Something <a href=\"http://example.com\">mixed</a></i><a href=\"http://example.com\"><b>, even bold</b></a><b></b><i><b>Now <a href=\"http://example.com\">both</a></b></i></p>";
 
+testWhiteList["Unclosed and unmatched quotes"] = "<p><i><b>Bold italic text </b>with bold deactivated<b> in between.</b></i></p><p><i><b>Bold italic text </b></i><b>with italic deactivated<i> in between.</i></b></p><p><b>Bold text..</b></p><p>..spanning two paragraphs (should not work).<b></b></p><p><b>Bold tag left open</b></p><p><i>Italic tag left open</i></p><p>Normal text.<!-- Unmatching number of opening, closing tags: -->\n</p><p><b>This year'</b>s election <i>should</i> beat <b>last year'</b>s.</p><p><i>Tom<b>s car is bigger than </b></i><b>Susan</b>s.</p>";
+
+// empty table tags / with only a caption are legal in HTML5.
+testWhiteList["A table with no data."] = "<table></table>";
+testWhiteList["A table with nothing but a caption"] = "<table><caption> caption</caption></table>";
+
 if (typeof module == "object") {
 	module.exports.testWhiteList = testWhiteList;
 }
