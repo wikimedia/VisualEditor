55ddb4fd666bf00e877b09904169160a940febde
Gabriel Wicke <gwicke@users.mediawiki.org>
2012-02-11 17:59:17 +0000

    Remove WikiDom default serialization and --html argument from parse.js wrapper. HTML ist now the only supported format. The DOMConverter is now no longer used. Roan, feel free to remove / butcher it for direct HTML to linear model conversion.

--- modules/parser/parse.js
+++ modules/parser/parse.js
@@ -13,11 +13,6 @@ var ParserPipeline = require('./mediawiki.parser.js').ParserPipeline,
 
 ( function() { 
 	var argv = optimist.usage( 'Usage: $0', {
-		'html': {
-			description: 'Produce html output instead of WikiDom',
-			'boolean': true,
-			'default': false
-		},
 		'debug': {
 			description: 'Debug mode',
 			'boolean': true,
@@ -75,15 +70,8 @@ var ParserPipeline = require('./mediawiki.parser.js').ParserPipeline,
 	process.stdin.on( 'end', function() { 
 		var input = inputChunks.join('');
 		parser.on('document', function ( document ) {
-			if ( ! argv.html ) {
-				var wikiDom = new DOMConverter().HTMLtoWiki( document.body ),
-				// Serialize the WikiDom with indentation
-				output = JSON.stringify( wikiDom, null, 2 );
-				process.stdout.write( output );
-			} else {
-				// Print out the html
-				process.stdout.write( document.body.innerHTML );
-			}
+			// Print out the html
+			process.stdout.write( document.body.innerHTML );
 			// add a trailing newline for shell user's benefit
 			process.stdout.write( "\n" );
 			process.exit(0);
