d5ad9a9ad48cf0a50d050277f78340b9b1e5c7b9
Inez Korczynski <inez@users.mediawiki.org>
2012-02-13 22:45:18 +0000

    New approach to text input and IME (part I)

--- modules/ve/ce/ve.es.Surface.js
+++ modules/ve/ce/ve.es.Surface.js
@@ -24,8 +24,29 @@ ve.es.Surface = function( $container, model ) {
 		.addClass( 'es-surfaceView' )
 		.append( this.documentView.$ );
 	this.emitUpdateTimeout = undefined;
+	this.node = null;
 
 	// Events
+	this.documentView.$.bind( {
+		'focus': function( e ) {
+			$document.unbind( '.ce-surfaceView' );
+			$document.bind( {
+				'keydown.ce-surfaceView': function( e ) {
+					return _this.onKeyDown( e );
+				},
+			} );
+			_this.setNode();
+		},
+		'blur': function( e ) {
+			$document.unbind( '.ce-surfaceView' );
+		}
+	} );
+
+	this.$.mousedown( function(e) {
+		return _this.onMouseDown( e );
+	} );
+
+	/*
 	this.model.getDocument().on( 'update', function() {
 		_this.emitUpdate( 25 );
 	} );
@@ -46,17 +67,206 @@ ve.es.Surface = function( $container, model ) {
 	this.$.on('cut copy', function( e ) {
 		_this.onCutCopy( e );
 	} );
+	*/
 
 	// Initialization
 	this.documentView.renderContent();
-	
-	this.worker = null;
-	this.node = null;
-
 };
 
 /* Methods */
 
+ve.es.Surface.prototype.attachContextView = function( contextView ) {
+	this.contextView = contextView;
+};
+
+ve.es.Surface.prototype.getModel = function() {
+	return this.model;
+};
+
+ve.es.Surface.prototype.onKeyDown = function( e ) {
+	switch ( e.keyCode ) {
+		// Page up
+		case 33:
+		// Page down
+		case 34:
+		// Home
+		case 36:
+		// End
+		case 35:
+		// Up arrow
+		case 38:
+		// Down arrow
+		case 40:
+			this.setNode();
+			break;
+		// Left arrow
+		case 37:
+			this.setNode( 'left' );
+			break;
+		// Right arrow
+		case 39:
+			this.setNode( 'right' );
+			break;
+	}
+};
+
+ve.es.Surface.prototype.onMouseDown = function( e ) {
+	this.setNode();
+};
+
+ve.es.Surface.prototype.setNode = function( direction ) {
+	var _this = this;
+	
+	setTimeout( function() {
+		var rangySelection = rangy.getSelection();
+
+		if ( rangySelection.anchorNode === _this.node ) {
+			return;
+		}
+
+		if ( rangySelection.anchorNode.nodeType !== 3 ) {
+			if ( _this.node === null ) {
+				throw "Value of this.node shouldn't be a null";
+			}
+			if ( direction !== 'left' && direction !== 'right' ) {
+				throw "At this point value of direction should be 'left' or 'right'";
+			}
+			var	oldOffset = _this.getOffset( _this.node, 0 ),
+				newOffset;
+
+			if ( direction === 'left' ) {
+				newOffset = _this.documentView.model.getRelativeContentOffset( oldOffset, -1 );
+			} else if ( direction === 'right' ) {
+				newOffset = _this.documentView.model.getRelativeContentOffset( oldOffset + _this.node.length, 1 );
+			}
+			_this.showCursorAt( newOffset );
+			_this.setNode();
+			return;
+		}
+
+		_this.node = rangySelection.anchorNode;
+		
+		console.log(_this.node); 
+	}, 0 );
+};
+
+ve.es.Surface.prototype.getSelection = function() {
+	var selection = rangy.getSelection();
+
+	if ( selection.anchorNode === selection.focusNode && selection.anchorOffset === selection.focusOffset ) {
+		// cursor
+		var offset = this.getOffset( selection.anchorNode, selection.anchorOffset );
+		return new ve.Range( offset, offset );
+	} else {
+		// selection		
+		var offset1 = this.getOffset( selection.anchorNode, selection.anchorOffset );
+		var offset2 = this.getOffset( selection.focusNode, selection.focusOffset );
+		return new ve.Range( offset1, offset2 );
+	}
+};
+
+ve.es.Surface.prototype.getOffset = function( localNode, localOffset ) {
+	var $node = $( localNode );
+	
+	if ( $node.hasClass( 'ce-leafNode' ) ) {
+		return this.documentView.getOffsetFromNode( $node.data('view') ) + 1;
+	}
+	
+	while( !$node.hasClass( 'ce-leafNode' ) ) {
+		$node = $node.parent();
+	}
+	
+	var current = [$node.contents(), 0];
+	var stack = [current];
+	
+	var offset = 0;
+	
+	while ( stack.length > 0 ) {
+		if ( current[1] >= current[0].length ) {
+			stack.pop();
+			current = stack[ stack.length - 1 ];
+			continue;
+		}
+		var item = current[0][current[1]];
+		var $item = current[0].eq( current[1] );
+		
+		if ( item.nodeType === 3 ) {
+			if ( item === localNode ) {
+				offset += localOffset;
+				break;
+			} else {
+				offset += item.textContent.length;
+			}
+		} else if ( item.nodeType === 1 ) {
+			if ( $( item ).attr('contentEditable') === "false" ) {
+				offset += 1;
+			} else {
+				if ( item === localNode ) {
+					offset += localOffset;
+					break;
+				}
+			
+				stack.push( [$item.contents(), 0] );
+				current[1]++;
+				current = stack[stack.length-1];
+				continue;
+			}
+		}
+		current[1]++;
+	}
+
+	return this.documentView.getOffsetFromNode( $node.data('view') ) + 1 + offset;
+};
+
+ve.es.Surface.prototype.showCursorAt = function( offset ) {
+	var $node = this.documentView.getNodeFromOffset( offset ).$;
+	var current = [$node.contents(), 0];
+	var stack = [current];
+	var node;
+	var localOffset;
+	
+	var index = 1 + this.documentView.getOffsetFromNode( $node.data('view') );
+		
+	while ( stack.length > 0 ) {
+		if ( current[1] >= current[0].length ) {
+			stack.pop();
+			current = stack[ stack.length - 1 ];
+			continue;
+		}
+		var item = current[0][current[1]];
+		var $item = current[0].eq( current[1] );
+		
+		if ( item.nodeType === 3 ) {
+			var length = item.textContent.length;
+			if ( offset >= index && offset <= index + length ) {
+				node = item;
+				localOffset = offset - index;
+				break;
+			} else {
+				index += length;
+			}
+		} else if ( item.nodeType === 1 ) {
+			if ( $( item ).attr('contentEditable') === "false" ) {
+				index += 1;
+			} else {
+				stack.push( [$item.contents(), 0] );
+				current[1]++;
+				current = stack[stack.length-1];
+				continue;
+			}
+		}
+		current[1]++;
+	}
+	var range = document.createRange();
+	range.collapsed = true;
+	range.setStart(node, localOffset);
+
+	var sel = window.getSelection();
+	sel.removeAllRanges();
+	sel.addRange(range);
+};
+
+/*
 ve.es.Surface.prototype.onCutCopy = function( e ) {
 	var _this = this,
 		key = rangy.getSelection().getRangeAt(0).toString().replace(/( |\r\n|\n|\r|\t)/gm,"");
@@ -159,14 +369,6 @@ ve.es.Surface.prototype.onMouseDown = function( e ) {
 	}, 1 );
 };
 
-ve.es.Surface.prototype.attachContextView = function( contextView ) {
-	this.contextView = contextView;
-};
-
-ve.es.Surface.prototype.getModel = function() {
-	return this.model;
-};
-
 ve.es.Surface.prototype.emitUpdate = function( delay ) {
 	if ( delay ) {
 		if ( this.emitUpdateTimeout !== undefined ) {
@@ -181,76 +383,6 @@ ve.es.Surface.prototype.emitUpdate = function( delay ) {
 		this.emit( 'update' );	
 	}
 };
-
-ve.es.Surface.prototype.getOffset = function( localNode, localOffset ) {
-	var $node = $( localNode );
-	
-	if ( $node.hasClass( 'ce-leafNode' ) ) {
-		return this.documentView.getOffsetFromNode( $node.data('view') ) + 1;
-	}
-	
-	while( !$node.hasClass( 'ce-leafNode' ) ) {
-		$node = $node.parent();
-	}
-	
-	var current = [$node.contents(), 0];
-	var stack = [current];
-	
-	var offset = 0;
-	
-	while ( stack.length > 0 ) {
-		if ( current[1] >= current[0].length ) {
-			stack.pop();
-			current = stack[ stack.length - 1 ];
-			continue;
-		}
-		var item = current[0][current[1]];
-		var $item = current[0].eq( current[1] );
-		
-		if ( item.nodeType === 3 ) {
-			if ( item === localNode ) {
-				offset += localOffset;
-				break;
-			} else {
-				offset += item.textContent.length;
-			}
-		} else if ( item.nodeType === 1 ) {
-			if ( $( item ).attr('contentEditable') === "false" ) {
-				offset += 1;
-			} else {
-				if ( item === localNode ) {
-					offset += localOffset;
-					break;
-				}
-			
-				stack.push( [$item.contents(), 0] );
-				current[1]++;
-				current = stack[stack.length-1];
-				continue;
-			}
-		}
-		current[1]++;
-	}
-
-	return this.documentView.getOffsetFromNode( $node.data('view') ) + 1 + offset;
-}
-
-ve.es.Surface.prototype.getSelection = function() {
-	var selection = rangy.getSelection();
-
-	if ( selection.anchorNode === selection.focusNode && selection.anchorOffset === selection.focusOffset ) {
-		// only one offset
-		var offset = this.getOffset( selection.anchorNode, selection.anchorOffset );
-		return new ve.Range( offset, offset );
-	} else {
-		// two offsets		
-		var offset1 = this.getOffset( selection.anchorNode, selection.anchorOffset );
-		var offset2 = this.getOffset( selection.focusNode, selection.focusOffset );
-
-		return new ve.Range( offset1, offset2 );
-	}
-};
-
 
 ve.es.Surface.prototype.showCursorAt = function( offset ) {
 	var $node = this.documentView.getNodeFromOffset( offset ).$;
@@ -358,7 +490,7 @@ ve.es.Surface.prototype.showFakeCursorAt = function( offset ) {
 		sel.setSingleRange(range1);
 	}, 3000);
 }
-
+*/
 
 /* Inheritance */
 
