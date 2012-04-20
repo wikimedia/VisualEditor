e84b6b656b70265e97e1acb68c74e315d91eed79
Catrope <roan.kattouw@gmail.com>
2012-04-20 14:44:41 -0700

    Rename ve.dm.NodeFactory to ve.NodeFactory
    Change-Id: Ic2a0389ca4855ac1afc3fb9a151145fea2b4795e

---
+++ modules/ve2/ve.NodeFactory.js
@@ -0,0 +1,15 @@
+/**
+ * Data model node factory.
+ * 
+ * @class
+ * @constructor
+ */
+ve.NodeFactory = function() {
+	//
+};
+
+/* Methods */
+
+ve.NodeFactory.prototype.register = function( name, constructor ) {
+	//
+};
