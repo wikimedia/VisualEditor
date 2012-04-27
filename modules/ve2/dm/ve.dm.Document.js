42eaba05e7018b2f47816abf86edfde930c2e73b
Trevor Parscal <trevorparscal@gmail.com>
2012-04-26 19:44:44 -0700

    First go at ve.dm.Document and ve.dm.DocumentFragment
    Change-Id: I6099ec0dbdac8d00cdde138b6b9a1b3a5781daea

---
+++ modules/ve2/dm/ve.dm.Document.js
@@ -0,0 +1,70 @@
+/**
+ * Document object.
+ * 
+ * @class
+ * @constructor
+ * @param {Array} data Linear model data to start with
+ */
+ve.dm.Document = function( data ) {
+	// Inheritance
+	ve.dm.DocumentFragment.call( this, data );
+};
+
+/* Methods */
+
+/**
+ * Rebuild one or more nodes from a linear model fragment.
+ * 
+ * The data provided to this method may contain either one node or multiple sibling nodes, but it
+ * must be balanced and valid. Data provided to this method also may not contain any content at the
+ * top level. The tree and offset map are updated during this operation.
+ * 
+ * Process:
+ *  1. Nodes between {index} and {index} + {numNodes} in {parent} will be removed
+ *  2. Data will be retrieved from this.data using {offset} and {newLength}
+ *  3. A document fragment will be generated from the retrieved data
+ *  4. The document fragment's offset map will be inserted into this document at {offset}
+ *  5. The document fragment's nodes will be inserted into {parent} at {index}
+ * 
+ * Use cases:
+ *  1. Rebuild old nodes and offset data after a change to the linear model.
+ *  2. Insert new nodes and offset data after a insertion in the linear model.
+ * 
+ * @param {ve.dm.Node} parent Parent of the node(s) being rebuilt
+ * @param {Integer} index Index within parent to rebuild or insert nodes
+ *   - If {numNodes} == 0: Index to insert nodes at
+ *   - If {numNodes} >= 1: Index of first node to rebuild
+ * @param {Integer} numNodes Total number of nodes to rebuild
+ *   - If {numNodes} == 0: Nothing  will be rebuilt, but the node(s) built from data will be
+ *     inserted before {index}. To insert nodes at the end, use number of children in {parent}
+ *   - If {numNodes} == 1: Only the node at {index} will be rebuilt
+ *   - If {numNodes} > 1: The node at {index} and the next {numNodes-1} nodes will be rebuilt
+ * @param {Integer} offset Linear model offset to rebuild or insert offset map data
+ *   - If {numNodes} == 0: Offset to insert offset map data at
+ *   - If {numNodes} >= 1: Offset to remove old and insert new offset map data at
+ * @param {Integer} newLength Length of data in linear model to rebuild or insert nodes for
+ * @returns {ve.dm.Node[]} Array containing the rebuilt/inserted nodes
+ */
+ve.dm.DocumentNode.prototype.rebuildNodes = function( parent, index, numNodes, offset, newLength ) {
+	// Compute the length of the old nodes (so we can splice their offsets out of the offset map)
+	var oldLength = 0;
+	for ( var i = index; i < index + numNodes; i++ ) {
+		oldLength += parent.children[i].getElementLength();
+	}
+	// Get a slice of the document where it's been changed
+	var data = this.data.slice( offset, offset + newLength );
+	// Build document fragment from data
+	var fragment = new ve.dm.DocumentFragment( this, data );
+	// Get generated child nodes from the document fragment
+	var nodes = fragment.getRootNode().getChildren();
+	// Replace nodes in the model tree
+	ve.batchedSplice( parent, index, numNodes, nodes );
+	// Update offset map
+	ve.batchedSplice( this.offsetMap, offset, oldLength, fragment.getOffsetMap() );
+	// Return inserted nodes
+	return nodes;
+};
+
+/* Inheritance */
+
+ve.extendClass( ve.dm.Document, ve.dm.DocumentFragment );
---
+++ modules/ve2/dm/ve.dm.DocumentFragment.js
@@ -0,0 +1,147 @@
+/**
+ * Document fragment.
+ * 
+ * @class
+ * @constructor
+ * @param {Array} data Linear model data to start with
+ */
+ve.dm.DocumentFragment = function( parentDocument, data ) {
+	// Properties
+	this.parentDocument = parentDocument;
+	this.data = data || [];
+	this.rootNode = new ve.dm.DocumentNode();
+	this.offsets = new Array( data.length );
+
+	// Initialization
+	var root = parentDocument ? parentDocument.getRootNode() : this.rootNode;
+	/*
+	 * The offsetMap is always one element longer than data because it includes a reference to the
+	 * root node at the offset just past the end. To make population work correctly, we have to
+	 * start out with that one extra reference.
+	 */
+	this.offsetMap.push( this.rootNode );
+	this.rootNode.setRoot( root );
+	/*
+	 * Build a tree of nodes and nodes that will be added to them after a full scan is complete,
+	 * then from the bottom up add nodes to their potential parents. This avoids massive length
+	 * updates being broadcast upstream constantly while building is underway. Also populate the
+	 * offset map as we go.
+	 */
+	var node,
+		textLength = 0,
+		inTextNode = false,
+		// TODO document this stack of stacks
+		stack = [[this.rootNode], []],
+		children,
+		openingIndex,
+		currentStack = stack[1],
+		parentStack = stack[0],
+		currentNode = this.rootNode;
+
+	for ( var i = 0, length = this.data.length; i < length; i++ ) {
+		// Set the node reference for this offset in the offset cache
+		// This looks simple, but there are three cases that result in the same thing:
+		// 1. data[i] is an opening, so offset i is before the opening, so we
+		//    need to point to the parent of the opened element. currentNode
+		//    will be set to the opened element later, but right now it's
+		//    still set to the parent of the opened element.
+		// 2. data[i] is a closing, so offset i is before the closing, so we
+		//    need to point to the closed element. currentNode will be set to
+		//    the parent of the closed element later, but right now it's still
+		//    set to the closed element
+		// 3. data[i] is content, so offset i is in the middle of an element,
+		//    so obviously we need currentNode, which won't be changed by this
+		//    iteration
+		this.offsets[i] = currentNode;
+
+		if ( this.data[i].type === undefined ) {
+			// Text node
+			if ( !inTextNode ) {
+				// Create a lengthless text node
+				node = new ve.dm.TextNode();
+				// Set the root pointer now, to prevent cascading updates
+				node.setRoot( root );
+				// Put the node on the current inner stack
+				currentStack.push( node );
+				currentNode = node;
+				// Set a flag saying we're inside a text node
+				inTextNode = true;
+			}
+			// Track the length
+			textLength++;
+		} else {
+			if ( inTextNode ) {
+				// Finish the text node by setting the length
+				currentNode.setLength( textLength );
+				// But the state variables back as they were
+				currentNode = parentStack[parentStack.length - 1];
+				inTextNode = false;
+				textLength = 0;
+			}
+
+			if ( this.data[i].type.charAt( 0 ) != '/' ) {
+				// Branch or leaf node opening
+				// Create a childless node
+				node = ve.dm.factory.createNode( this.data[i].type, [], this.data[i].attributes );
+				// Set the root pointer now, to prevent cascading updates
+				node.setRoot( root );
+				// Put the childless node on the current inner stack
+				currentStack.push( node );
+				// Create a new inner stack for this node
+				parentStack = currentStack;
+				currentStack = [];
+				stack.push( currentStack );
+				currentNode = node;
+			} else {
+				// Branch or leaf node closing
+				// Pop this node's inner stack from the outer stack. It'll have all of the node's
+				// child nodes fully constructed 
+				children = stack.pop();
+				currentStack = parentStack;
+				parentStack = stack[stack.length - 2];
+				// Attach the children to the node
+				ve.batchedSplice( currentNode, 0, 0, children );
+				currentNode = parentStack[parentStack.length - 1];
+			}
+		}
+	}
+	// The end state is stack = [ [this.rootNode] [ array, of, its, children ] ]
+	// so attach all nodes in stack[1] to the root node
+	ve.batchedSplice( this.rootNode, 0, 0, stack[1] );
+};
+
+/* Methods */
+
+/**
+ * Gets slice or copy of the document data.
+ * 
+ * @method
+ * @param {ve.Range} [range] Range of data to get, all data will be given by default
+ * @param {Boolean} [deep=false] Whether to return a deep copy (WARNING! This may be very slow)
+ * @returns {Array} Slice or copy of document data
+ */
+ve.dm.DocumentNode.prototype.getData = function( range, deep ) {
+	var start = 0,
+		end;
+	if ( range !== undefined ) {
+		range.normalize();
+		start = Math.max( 0, Math.min( this.data.length, range.start ) );
+		end = Math.max( 0, Math.min( this.data.length, range.end ) );
+	}
+	// IE work-around: arr.slice( 0, undefined ) returns [] while arr.slice( 0 ) behaves correctly
+	var data = end === undefined ? this.data.slice( start ) : this.data.slice( start, end );
+	// Return either the slice or a deep copy of the slice
+	return deep ? ve.copyArray( data ) : data;
+};
+
+ve.dm.DocumentFragment.prototype.getOffsetMap = function() {
+	return this.offsetMap;
+};
+
+ve.dm.DocumentFragment.prototype.getRootNode = function() {
+	return this.rootNode;
+};
+
+ve.dm.DocumentFragment.prototype.getNodeFromOffset = function( offset ) {
+	return this.offsetMap[offset];
+};
