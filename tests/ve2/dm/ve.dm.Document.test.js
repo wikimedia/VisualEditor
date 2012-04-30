93390847bc505be44a59dd17e29975ad8bbaefcf
Trevor Parscal <trevorparscal@gmail.com>
2012-04-30 13:48:20 -0700

    Added getOuterLength tests for text and document nodes
    Neither of these nodes have elements around them, so they must override the default behavior of ve.dm.Node
    
    Change-Id: I19c02c210bfc04b6e2ee1a37b8890e84a236eee3

--- tests/ve2/dm/ve.dm.Document.test.js
+++ tests/ve2/dm/ve.dm.Document.test.js
@@ -2,6 +2,15 @@ module( 've.dm.Document' );
 
 /* Tests */
 
+test( 'getOuterLength', 1, function() {
+	var fragment = new ve.dm.DocumentFragment( ve.dm.example.data );
+	strictEqual(
+		fragment.getDocumentNode().getOuterLength(),
+		ve.dm.example.data.length,
+		'document does not have elements around it'
+	);
+} );
+
 test( 'rebuildNodes', 88, function() {
 	var doc = new ve.dm.Document( ve.dm.example.data ),
 		documentNode = doc.getDocumentNode();
