0cd31fba2ea3a2d28385094685440c8c09cafb6c
Rob Moen <rmoen@wikimedia.org>
2012-05-03 16:14:54 -0700

    rename surfaceModel to model in ce.Surface
    Change-Id: I5d49915b4645464729ab1e4552982cbd38f712b1

--- modules/ve2/ce/ve.ce.Surface.js
+++ modules/ve2/ce/ve.ce.Surface.js
@@ -9,7 +9,7 @@ ve.ce.Surface = function( $container, model ) {
 	// Inheritance
 	ve.EventEmitter.call( this );
 	// Properties
-	this.surfaceModel = model;
+	this.model = model;
 	this.documentView = new ve.ce.Document( model.documentModel );
 	$container.append(this.documentView.documentNode.$);
 	this.documentView.documentNode.render();
