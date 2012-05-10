ee39e837b8e3cac1acb7c01be42bd47b68628317
Inez Korczynski <inez@wikia-inc.com>
2012-05-09 22:36:25 -0700

    Get Alien node (leaftnode) working - it reads its content from attribute 'html'
    Change-Id: Idd84da9af61369277df18fee577032de765b1205

--- modules/ve2/ce/nodes/ve.ce.AlienNode.js
+++ modules/ve2/ce/nodes/ve.ce.AlienNode.js
@@ -9,6 +9,34 @@
 ve.ce.AlienNode = function( model ) {
 	// Inheritance
 	ve.ce.LeafNode.call( this, 'alien', model );
+
+	// Events
+	this.model.addListenerMethod( this, 'update', 'onUpdate' );
+
+	// Intialization
+	var _this = this;
+
+	/* We might need some of those at some point */
+	/*
+	this.$.on( {
+		'mousedown': function() {
+			_this.$.css( '-webkit-user-select', 'none' );
+		},
+		'mouseup': function() {
+			_this.$.css( '-webkit-user-select', '' );
+		},
+	} );
+
+	this.$.attr( 'contenteditable', false );
+	*/
+
+	// TODO: move to .css file
+	this.$.css( {
+		'display': 'inline',
+		'border': 'rgba(0,0,0,0.3) dashed 1px',
+		'background-color': 'rgba(255,255,186,0.3)'
+	} );
+	this.onUpdate();
 };
 
 /* Static Members */
@@ -24,6 +52,12 @@ ve.ce.AlienNode.rules = {
 	'canHaveChildren': false,
 	'canHaveGrandchildren': false,
 	'canBeSplit': false
+};
+
+/* Methods */
+
+ve.ce.AlienNode.prototype.onUpdate = function() {
+	this.$.html( this.model.getAttribute( 'html' ) );
 };
 
 /* Registration */
