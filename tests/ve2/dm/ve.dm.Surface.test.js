f958297102b4c76ec7ee93b83805803fcea929b8
Rob Moen <rmoen@wikimedia.org>
2012-06-04 15:02:56 -0700

    Create tests for surface model annotate method. Fix up surface model tests
    Change-Id: I02b57edc5c3faeb39e0b3c1f473f03fbd49d85b4

--- tests/ve2/dm/ve.dm.Surface.test.js
+++ tests/ve2/dm/ve.dm.Surface.test.js
@@ -1,8 +1,13 @@
 module( 've.dm.Surface' );
 
-ve.dm.SurfaceStub = function() {
+ve.dm.SurfaceStub = function( data ) {
 	// Inheritance
-	this.dm = new ve.dm.Document ( [{ 'type': 'paragraph' }, 'hi', { 'type': '/paragraph' }] );
+
+	if ( data !== undefined ) {
+		this.dm = new ve.dm.Document ( data );
+	} else {
+		this.dm = new ve.dm.Document ( [{ 'type': 'paragraph' }, 'h', 'i', { 'type': '/paragraph' }] );
+	}
 	ve.dm.Surface.call( this, this.dm );
 };
 
@@ -38,3 +43,66 @@ test( 'transact', 1, function() {
 	} );
 	surface.transact( tx );
 } );
+
+
+test('annotate', 1, function(){
+	var surface,
+		cases = [
+		{
+			'msg': 'Set Bold',
+			'data': [
+				'b', 'o', 'l', 'd'
+			],
+			'expected':
+			[
+				[
+					"b",
+						{
+							"{\"type\":\"textStyle/bold\"}": {
+								"type": "textStyle/bold"
+							}
+						}
+				],
+				[
+					"o",
+						{
+							"{\"type\":\"textStyle/bold\"}": {
+								"type": "textStyle/bold"
+							}
+						}
+				],
+				[
+					"l",
+						{
+							"{\"type\":\"textStyle/bold\"}": {
+								"type": "textStyle/bold"
+							}
+						}
+				],
+				[
+					"d",
+						{
+							"{\"type\":\"textStyle/bold\"}": {
+								"type": "textStyle/bold"
+							}
+						}
+				]
+			],
+			'annotate': {
+				'method': 'set',
+				'annotation': { 'type': 'textStyle/bold' }
+			}
+		}
+	];
+
+	expect( cases.length );
+	for ( var i = 0; i < cases.length; i++ ) {
+		surface = new ve.dm.SurfaceStub( cases[i].data );
+		surface.setSelection(new ve.Range( 0, surface.getDocument().getData().length ));
+		surface.annotate( cases[i].annotate['method'], cases[i].annotate['annotation']);
+		deepEqual(
+			surface.getDocument().getData(), cases[i].expected, cases[i].msg
+		);
+
+	}
+});
