b26649a2e8e88223bd4735ba69637c98788f0b20
Catrope <roan.kattouw@gmail.com>
2012-06-11 14:48:46 -0700

    Guard against passing undefined into ve.compareObjects(), was causing errors

--- modules/ve2/ui/tools/ve.ui.FormatDropdownTool.js
+++ modules/ve2/ui/tools/ve.ui.FormatDropdownTool.js
@@ -88,7 +88,7 @@ ve.FormatDropdownTool.prototype.updateState = function( annotations, nodes ) {
 	// Look for mismatches, in which case format should be null
 	for ( i = 1; i < nodes.length; i++ ) {
 		if ( format.type != nodes[i].getType() ||
-			!ve.compareObjects( format.attributes, nodes[i].attributes ) ) {
+			!ve.compareObjects( format.attributes || {}, nodes[i].attributes || {} ) ) {
 			format = null;
 			break;
 		}
@@ -101,7 +101,7 @@ ve.FormatDropdownTool.prototype.updateState = function( annotations, nodes ) {
 		for ( i = 0; i < items.length; i++ ) {
 			if (
 				format.type === items[i].type &&
-				ve.compareObjects( format.attributes, items[i].attributes )
+				ve.compareObjects( format.attributes || {}, items[i].attributes || {} )
 			) {
 				this.$label.text( items[i].label );
 				break;
