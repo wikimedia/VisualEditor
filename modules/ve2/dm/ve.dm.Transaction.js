efa93c24be354961f37dee53234128bbfbfc88fb
Catrope <roan.kattouw@gmail.com>
2012-06-13 16:56:51 -0700

    Copy objects in newFromWrap() to prevent reference pollution
    Change-Id: Ia1f71341342bea44f6ca08088a1276d54187b22a

--- modules/ve2/dm/ve.dm.Transaction.js
+++ modules/ve2/dm/ve.dm.Transaction.js
@@ -363,7 +363,7 @@ ve.dm.Transaction.newFromWrap = function( doc, range, unwrapOuter, wrapOuter, un
 		}
 		// Instead of putting in unwrapOuter as given, put it in the
 		// way it appears in the mode,l so we pick up any attributes
-		tx.pushReplace( unwrapOuterData, wrapOuter );
+		tx.pushReplace( unwrapOuterData, ve.copyObject( wrapOuter ) );
 	}
 
 	if ( wrapEach.length > 0 || unwrapEach.length > 0 ) {
@@ -397,7 +397,7 @@ ve.dm.Transaction.newFromWrap = function( doc, range, unwrapOuter, wrapOuter, un
 						}
 						// Instead of putting in unwrapEach as given, put it in the
 						// way it appears in the model, so we pick up any attributes
-						tx.pushReplace( unwrapEachData, wrapEach );
+						tx.pushReplace( ve.copyObject( unwrapEachData ), ve.copyObject( wrapEach ) );
 
 						// Store this offset for later
 						startOffset = i;
@@ -411,7 +411,7 @@ ve.dm.Transaction.newFromWrap = function( doc, range, unwrapOuter, wrapOuter, un
 						// Retain the contents of what we're wrapping
 						tx.pushRetain( i - startOffset + 1 - unwrapEach.length*2 );
 						// Replace the closing elements
-						tx.pushReplace( closingUnwrapEach, closingWrapEach );
+						tx.pushReplace( ve.copyObject( closingUnwrapEach ), ve.copyObject( closingWrapEach ) );
 					}
 				}
 			}
