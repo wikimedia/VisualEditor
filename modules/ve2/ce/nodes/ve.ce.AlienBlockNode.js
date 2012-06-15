a4589106ec37ef1e5a4fc593215c74a5f9314965
Catrope <roan.kattouw@gmail.com>
2012-06-14 23:57:40 -0700

    Add a TODO note about preventDefault in alien nodes
    Change-Id: I9aa0c6bec1587bdc65f1585c9ea3562c6d2153e6

--- modules/ve2/ce/nodes/ve.ce.AlienBlockNode.js
+++ modules/ve2/ce/nodes/ve.ce.AlienBlockNode.js
@@ -37,6 +37,7 @@ ve.ce.AlienBlockNode.rules = {
 /* Methods */
 
 ve.ce.AlienBlockNode.prototype.onUpdate = function() {
+	// TODO preventDefault on click for links inside, user should not leave the page
 	this.$.html( this.model.getAttribute( 'html' ) );
 };
 
