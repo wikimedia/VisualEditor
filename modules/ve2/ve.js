78028ff588ddea8fef412429f4e2dd8e70786b0c
Inez Korczynski <inez@wikia-inc.com>
2012-06-18 15:03:38 -0700

    Ported method debounce from underscore.js to ve.

--- modules/ve2/ve.js
+++ modules/ve2/ve.js
@@ -302,3 +302,28 @@ ve.log = function() {
 ve.dir = function( obj ) {
 	// don't do anything, this is just a stub
 };
+
+/**
+ * Ported from: http://underscorejs.org/underscore.js
+ *
+ * Returns a function, that, as long as it continues to be invoked, will not
+ * be triggered. The function will be called after it stops being called for
+ * N milliseconds. If `immediate` is passed, trigger the function on the
+ * leading edge, instead of the trailing.
+ *
+ * @static
+ * @method
+ */
+ve.debounce = function(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		if (immediate && !timeout) func.apply(context, args);
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+	};
+};
