f731125804b4a43c0c58694cbe58a7848fe95494
Gabriel Wicke <gwicke@wikimedia.org>
2012-06-23 17:30:52 +0200

    Explain reasoning behind number of worker calculation
    Change-Id: I92ae0bb6e02caef98b0d68de4424e775d8922651

--- api/server.js
+++ api/server.js
@@ -5,6 +5,9 @@
 
 var cluster = require('cluster');
 var app = require('./ParserService.js');
+// Start a few more workers than there are cpus visible to the OS, so that we
+// get some degree of parallelism even on single-core systems. A single
+// long-running request would otherwise hold up all concurrent short requests.
 var numCPUs = require('os').cpus().length + 3;
 
 if (cluster.isMaster) {
