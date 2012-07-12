2ee1514552bb3c2e6ff714c79ca6d27639c6b87e
Subramanya Sastry <ssastry@wikimedia.org>
2012-07-12 15:39:38 -0500

    Added mergeProperties function to Object.prototype
    Change-Id: I50346029a9bd8a7d2cf954b0cca011f73a6fae07

--- modules/parser/core-upgrade.js
+++ modules/parser/core-upgrade.js
@@ -6,3 +6,14 @@
 if(!Array.prototype.last) Object.defineProperty(Array.prototype, 'last', {
 	value: function() { return this[this.length - 1] }
 });
+
+/** Good-old monkey-patching to let us merge properties of another object into this one. */
+if(!Object.prototype.mergeProperties) Object.defineProperty(Object.prototype, 'mergeProperties', {
+	value: function (o) {
+		var self = this;
+		Object.keys(o).forEach(function(k) {
+			self[k] = o[k];
+		});
+		return this;
+	}
+});
