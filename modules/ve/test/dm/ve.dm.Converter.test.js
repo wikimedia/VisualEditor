e123a39b4e3de82c886c4f856f6622b9591002ca
Catrope <roan.kattouw@gmail.com>
2012-11-23 18:44:54 -0800

    Handle annotated inline nodes in the converter
    Was broken both on the way in and on the way out.
    
    * Move alien restoration (data->DOM) out of the main getDomFromData()
      function and into getDomElementFromDataElement(). This means the
      comment about District 9 is gone (sniff), but moving this here ensures
      all code paths hit it (previously, it was assumed annotated nodes
      could never be aliens).
    * In the DOM->data converter, add annotation application to
      getDataElementFromDomElement() (for content nodes) and createAlien()
      (for aliens). Previously, these nodes would not get annotations.
    ** ve.AnnotationSet doesn't have a constructor that takes an array, we
       should fix that.
    
    Change-Id: I65f8e9a322111ca3af275bf9997b0b1e7ee93769

--- modules/ve/test/dm/ve.dm.Converter.test.js
+++ modules/ve/test/dm/ve.dm.Converter.test.js
@@ -35,7 +35,7 @@ QUnit.test( 'getDomElementFromDataElement', 20, function ( assert ) {
 	}
 } );
 
-QUnit.test( 'getDataFromDom', 41, function ( assert ) {
+QUnit.test( 'getDataFromDom', 42, function ( assert ) {
 	var msg,
 		cases = ve.dm.example.domToDataCases;
 
@@ -51,7 +51,7 @@ QUnit.test( 'getDataFromDom', 41, function ( assert ) {
 	}
 } );
 
-QUnit.test( 'getDomFromData', 45, function ( assert ) {
+QUnit.test( 'getDomFromData', 46, function ( assert ) {
 	var msg,
 		cases = ve.dm.example.domToDataCases;
 
