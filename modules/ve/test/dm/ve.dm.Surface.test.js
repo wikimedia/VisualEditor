84fbd1db2ca36cc7f99fa1bf37a2f7c05e0b607c
Ed Sanders <esanders@wikimedia.org>
2013-06-18 16:55:03 +0100

    Only undo on MWMetaDialog close if small stack is non-empty
    Added return boolean to ve.dm.Surface#breakpoint to indicate if
    and transactions were pushed to the big stack. Use this value in
    MWMetaDialog to see if we need to undo on close.
    
    Added tests for ve.dm.Surface#breakpoint.
    
    Bug: 49630
    Change-Id: Ieb2e9e361afe057af93c4d374acc85df58bfb4c3

--- modules/ve/test/dm/ve.dm.Surface.test.js
+++ modules/ve/test/dm/ve.dm.Surface.test.js
@@ -57,3 +57,26 @@ QUnit.test( 'change', 3, function ( assert ) {
 	surface.change( tx, new ve.Range( 2, 2 ) );
 	assert.deepEqual( events, { 'transact': 2, 'select': 2, 'change': 3 } );
 } );
+
+QUnit.test( 'breakpoint', 7, function ( assert ) {
+	var surface = new ve.dm.SurfaceStub(),
+		tx = new ve.dm.Transaction.newFromInsertion( surface.dm, 1, ['x'] ),
+		selection = new ve.Range( 1, 1 );
+
+	assert.equal( surface.breakpoint(), false, 'Returns false if no transactions applied' );
+
+	surface.change( tx, selection );
+	assert.deepEqual( surface.bigStack, [], 'Big stack data matches before breakpoint' );
+	assert.deepEqual( surface.smallStack, [tx], 'Small stack data matches before breakpoint' );
+
+	assert.equal( surface.breakpoint(), true, 'Returns true after transaction applied' );
+	assert.equal( surface.breakpoint(), false, 'Returns false if no transactions applied since last breakpoint' );
+
+	assert.deepEqual( surface.bigStack, [ {
+			'stack': [tx],
+			'selection': selection
+		} ],
+		'Big stack data matches after breakpoint'
+	);
+	assert.deepEqual( surface.smallStack, [], 'Small stack data matches after breakpoint' );
+} );
