eafe5d844ccc89b8fa3c2d7885ae671a94fbc246
James D. Forrester <jforrester@wikimedia.org>
2013-06-20 14:56:54 -0700

    Discourage (ab)use of H1s, and relabel them
    Re-label the headings in the MWFormatDropdownTool with MW-specific
    headings per bug 43334, reduce the size of the dropdown's contents a
    little to make the headings less vibrant, and move the H1 option to the
    end of the list to further discourage its use.
    
    Note that there are some issues with the underlying structure here and our
    ability to split the repos into VE-core and VE-MW will need this to be
    refactored.
    
    Bug: 43334
    Change-Id: I5a58b4dcebd6ceae0ffcd24f663429f25bdc3db9

--- modules/ve/ui/tools/dropdowns/ve.ui.MWFormatDropdownTool.js
+++ modules/ve/ui/tools/dropdowns/ve.ui.MWFormatDropdownTool.js
@@ -26,15 +26,24 @@ ve.inheritClass( ve.ui.MWFormatDropdownTool, ve.ui.FormatDropdownTool );
 
 ve.ui.MWFormatDropdownTool.static.name = 'mwFormat';
 
-ve.ui.MWFormatDropdownTool.static.cssName = 'format';
+ve.ui.MWFormatDropdownTool.static.cssName = 'format ve-ui-dropdownTool-mwFormat';
 
 ve.ui.MWFormatDropdownTool.static.items[1].data.type = 'mwHeading';
+ve.ui.MWFormatDropdownTool.static.items[1].label = 'visualeditor-formatdropdown-format-mw-heading1';
 ve.ui.MWFormatDropdownTool.static.items[2].data.type = 'mwHeading';
+ve.ui.MWFormatDropdownTool.static.items[2].label = 'visualeditor-formatdropdown-format-mw-heading2';
 ve.ui.MWFormatDropdownTool.static.items[3].data.type = 'mwHeading';
+ve.ui.MWFormatDropdownTool.static.items[3].label = 'visualeditor-formatdropdown-format-mw-heading3';
 ve.ui.MWFormatDropdownTool.static.items[4].data.type = 'mwHeading';
+ve.ui.MWFormatDropdownTool.static.items[4].label = 'visualeditor-formatdropdown-format-mw-heading4';
 ve.ui.MWFormatDropdownTool.static.items[5].data.type = 'mwHeading';
+ve.ui.MWFormatDropdownTool.static.items[5].label = 'visualeditor-formatdropdown-format-mw-heading5';
 ve.ui.MWFormatDropdownTool.static.items[6].data.type = 'mwHeading';
+ve.ui.MWFormatDropdownTool.static.items[6].label = 'visualeditor-formatdropdown-format-mw-heading6';
 ve.ui.MWFormatDropdownTool.static.items[7].data.type = 'mwPreformatted';
+
+// Move the H1 (item 1 in the list) to the end (7) so as to make it less prominent and tempting to users
+ve.ui.MWFormatDropdownTool.static.items.splice( 7, 0, ve.ui.MWFormatDropdownTool.static.items.splice( 1, 1 )[0] );
 
 /* Registration */
 
