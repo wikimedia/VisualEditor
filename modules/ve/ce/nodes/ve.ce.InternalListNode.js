921a95de780f2e716e1dd1a63658b19ef44af6a0
Roan Kattouw <roan.kattouw@gmail.com>
2013-06-19 21:31:29 -0700

    Don't build the CE tree and DOM for the internal list
    This is a waste of time, because we hide it anyway. This took
    quite a bit of time for long articles.
    
    Change-Id: I1bcfd5b908c89dc2e08d5520e8870b145177ca10

--- modules/ve/ce/nodes/ve.ce.InternalListNode.js
+++ modules/ve/ce/nodes/ve.ce.InternalListNode.js
@@ -32,6 +32,15 @@ ve.ce.InternalListNode.static.name = 'internalList';
 
 ve.ce.InternalListNode.static.tagName = 'span';
 
+/* Methods */
+
+/**
+ * Deliberately empty: don't build an entire CE tree with DOM elements for things that won't render
+ * @inheritdoc
+ */
+ve.ce.InternalListNode.prototype.onSplice = function () {
+};
+
 /* Registration */
 
 ve.ce.nodeFactory.register( ve.ce.InternalListNode );
