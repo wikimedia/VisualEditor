1b5d9509047e7d66ffc4740856063c5b4857e5e7
Ed Sanders <esanders@wikimedia.org>
2013-07-21 14:09:19 +0100

    Add bold/italic for more latin langages
    es, eu, fr, gl, hu, it, nl, pt(-br) all covered now.
    
    Bug: 51702
    Bug: 51703
    Bug: 51704
    Bug: 51705
    Bug: 51706
    Bug: 51707
    Bug: 51709
    Bug: 51710
    Change-Id: Ie75cc0941245dffabf64f09d649982ba48fae340

--- modules/ve/ui/tools/buttons/ve.ui.ItalicButtonTool.js
+++ modules/ve/ui/tools/buttons/ve.ui.ItalicButtonTool.js
@@ -33,13 +33,21 @@ ve.ui.ItalicButtonTool.static.icon = {
 	'cs': 'italic-i',
 	'da': 'italic-k',
 	'de': 'italic-k',
+	'es': 'italic-c',
+	'eu': 'italic-e',
+	'fr': 'italic-i',
+	'gl': 'italic-c',
 	'he': 'italic-i',
+	'hu': 'italic-d',
+	'it': 'italic-c',
 	'ml': 'italic-i',
+	'nl': 'italic-c',
 	'nn': 'italic-k',
 	'no': 'italic-k',
 	'pl': 'italic-i',
+	'pt': 'italic-i',
+	'pt-br': 'italic-i',
 	'sv': 'italic-k'
-
 };
 
 ve.ui.ItalicButtonTool.static.titleMessage = 'visualeditor-annotationbutton-italic-tooltip';
