wikimedia/VisualEditor

commit 7557dd39ed37be787c3dfad5dc6ed0bd05d6dc67

Show error when trying to save in read-only mode
We already correctly show the read only error if the user tries
to load VE which the database is locked, but if the database gets
locked after they've loaded VE, we also need to show the error
when they try to save.

Bug: 51636
Change-Id: I7a56f1b4387e7ea594a2a7f939c81626c9eee834

modules/ve-mw/init/targets/ve.init.mw.ViewPageTarget.js
@@ -452,6 +452,13 @@ ve.init.mw.ViewPageTarget.prototype.onSaveError = function ( jqXHR, status, data
 		return;
 	}
 
+	if ( data.error ) {
+		ve.init.mw.Target.onSerializeError.call(
+			this, null, 'Unsuccessful request: ' + data.error.info, null
+		);
+		return;
+	}
+
 	editApi = data && data.visualeditoredit && data.visualeditoredit.edit;
 
 	// Handle spam blacklist error (either from core or from Extension:SpamBlacklist)

commit e23df9f5a7d51b818b7e8c2d1f975de28336e21b

Fix badtoken handling broken by 7557dd39ed3
7557dd39ed3 make the badtoken handling code unreachable. Revert that
change, and fix the rest of the function to deal with the possibility
that editApi is undefined. Let handling of the read-only mode error
and any other errors fall through to the bottom of the function.

Change-Id: I0673f2bb629e5cc9449675c1074d283e3926e1d5

modules/ve-mw/init/targets/ve.init.mw.ViewPageTarget.js
@@ -456,13 +456,6 @@ ve.init.mw.ViewPageTarget.prototype.onSaveError = function ( jqXHR, status, data
 		return;
 	}
 
-	if ( data.error ) {
-		ve.init.mw.Target.onSerializeError.call(
-			this, null, 'Unsuccessful request: ' + data.error.info, null
-		);
-		return;
-	}
-
 	editApi = data && data.visualeditoredit && data.visualeditoredit.edit;
 
 	// Handle spam blacklist error (either from core or from Extension:SpamBlacklist)
@@ -481,7 +474,7 @@ ve.init.mw.ViewPageTarget.prototype.onSaveError = function ( jqXHR, status, data
 
 	// Handle warnings/errors from Extension:AbuseFilter
 	// TODO: Move this to a plugin
-	if ( editApi.info && editApi.info.indexOf( 'Hit AbuseFilter:' ) === 0 && editApi.warning ) {
+	if ( editApi && editApi.info && editApi.info.indexOf( 'Hit AbuseFilter:' ) === 0 && editApi.warning ) {
 		this.showMessage(
 			'api-save-error',
 			$.parseHTML( editApi.warning ),
@@ -620,9 +613,9 @@ ve.init.mw.ViewPageTarget.prototype.onSaveError = function ( jqXHR, status, data
 	this.showMessage(
 		'api-save-error',
 		document.createTextNode(
-			editApi.info ||
+			( editApi && editApi.info ) ||
 				( data.error && data.error.info ) ||
-				editApi.code ||
+				( editApi && editApi.code ) ||
 				( data.error && data.error.code ) ||
 				'Unknown error'
 		),