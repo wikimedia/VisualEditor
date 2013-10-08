971b0267869ae9020f05c89ec34b9c4f5495cb0a
cmcmahon <cmcmahon@wikimedia.org>
2013-10-08 06:00:19 -0700

    id for ip warning changed
    Change-Id: I6623751228be1aa550ec8c7e1e3071a38563ada0

--- modules/ve-mw/test/browser/features/support/pages/visual_editor_page.rb
+++ modules/ve-mw/test/browser/features/support/pages/visual_editor_page.rb
@@ -19,7 +19,7 @@ class VisualEditorPage
   div(:insert_references, class: 've-ui-window-title')
   div(:internal_diff_view, class: 've-init-mw-viewPageTarget-saveDialog-viewer')
   span(:internal_linksuggestion, text: 'Main Page')
-  div(:ip_warning, class: 've-init-mw-viewPageTarget-editNotices-notice')
+  div(:ip_warning, class: 've-init-mw-viewPageTarget-toolbar-editNotices-notice')
   span(:linksuggestion, text: 'http://www.example.com')
   span(:looks_good, class: 've-ui-labeledElement-label', text: 'Looks good to me')
   checkbox(:minor_edit, id: 'wpMinoredit')
