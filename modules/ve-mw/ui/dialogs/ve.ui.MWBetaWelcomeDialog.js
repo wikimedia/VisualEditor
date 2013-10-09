wikimedia/VisualEditor

commit 4aa86d0f87ba1e79f8c57005fcb1df69f3abd5f3

Make dialogs, inspectors windows and window sets generic
Objective:
* Remove surface dependencies in dialogs, inspectors, windows and window sets
* Introduce surface-specific versions of dialogs, inspectors and window sets

Change-Id: I2db59127d2085b02e173a3605e174317e419e213

modules/ve-mw/ui/inspectors/ve.ui.MWExtensionInspector.js
@@ -10,7 +10,7 @@
  *
  * @class
  * @abstract
- * @extends ve.ui.Inspector
+ * @extends ve.ui.SurfaceInspector
  *
  * @constructor
  * @param {ve.ui.Surface} surface
@@ -18,12 +18,12 @@
  */
 ve.ui.MWExtensionInspector = function VeUiMWExtensionInspector( surface, config ) {
 	// Parent constructor
-	ve.ui.Inspector.call( this, surface, config );
+	ve.ui.SurfaceInspector.call( this, surface, config );
 };
 
 /* Inheritance */
 
-ve.inheritClass( ve.ui.MWExtensionInspector, ve.ui.Inspector );
+ve.inheritClass( ve.ui.MWExtensionInspector, ve.ui.SurfaceInspector );
 
 /* Static properties */
 
@@ -42,7 +42,7 @@ ve.ui.MWExtensionInspector.static.removeable = false;
  */
 ve.ui.MWExtensionInspector.prototype.initialize = function () {
 	// Parent method
-	ve.ui.Inspector.prototype.initialize.call( this );
+	ve.ui.SurfaceInspector.prototype.initialize.call( this );
 
 	this.input = new ve.ui.TextInputWidget( {
 		'$$': this.frame.$$,
@@ -63,7 +63,7 @@ ve.ui.MWExtensionInspector.prototype.onOpen = function () {
 	var extsrc = '';
 
 	// Parent method
-	ve.ui.Inspector.prototype.onOpen.call( this );
+	ve.ui.SurfaceInspector.prototype.onOpen.call( this );
 
 	this.node = this.surface.getView().getFocusedNode();
 
@@ -89,7 +89,7 @@ ve.ui.MWExtensionInspector.prototype.onClose = function ( action ) {
 		surfaceModel = this.surface.getModel();
 
 	// Parent method
-	ve.ui.Inspector.prototype.onClose.call( this, action );
+	ve.ui.SurfaceInspector.prototype.onClose.call( this, action );
 
 	if ( this.node instanceof this.constructor.static.nodeView ) {
 		mwData = ve.copy( this.node.getModel().getAttribute( 'mw' ) );

modules/ve-mw/ui/inspectors/ve.ui.MWMathInspector.js
@@ -98,7 +98,7 @@ ve.ui.MWMathInspector.prototype.onClose = function ( action ) {
 		ve.ui.MWExtensionInspector.prototype.onClose.call( this, action );
 	} else {
 		// Grandparent method; we're overriding the parent behavior in this case
-		ve.ui.Inspector.prototype.onClose.call( this, action );
+		ve.ui.SurfaceInspector.prototype.onClose.call( this, action );
 
 		// The user tried to empty the node, remove it
 		surfaceModel.change( ve.dm.Transaction.newFromRemoval(

modules/ve/ui/inspectors/ve.ui.AnnotationInspector.js
@@ -10,7 +10,7 @@
  *
  * @class
  * @abstract
- * @extends ve.ui.Inspector
+ * @extends ve.ui.SurfaceInspector
  *
  * @constructor
  * @param {ve.ui.Surface} surface
@@ -18,7 +18,7 @@
  */
 ve.ui.AnnotationInspector = function VeUiAnnotationInspector( surface, config ) {
 	// Parent constructor
-	ve.ui.Inspector.call( this, surface, config );
+	ve.ui.SurfaceInspector.call( this, surface, config );
 
 	// Properties
 	this.initialAnnotation = null;
@@ -29,7 +29,7 @@ ve.ui.AnnotationInspector = function VeUiAnnotationInspector( surface, config ) 
 
 /* Inheritance */
 
-ve.inheritClass( ve.ui.AnnotationInspector, ve.ui.Inspector );
+ve.inheritClass( ve.ui.AnnotationInspector, ve.ui.SurfaceInspector );
 
 /**
  * Annotation models this inspector can edit.
@@ -59,7 +59,7 @@ ve.ui.AnnotationInspector.prototype.onSetup = function () {
 		annotation = this.getMatchingAnnotations( fragment, true ).get( 0 );
 
 	// Parent method
-	ve.ui.Inspector.prototype.onSetup.call( this );
+	ve.ui.SurfaceInspector.prototype.onSetup.call( this );
 	// Initialize range
 	if ( !annotation ) {
 		if ( fragment.getRange().isCollapsed() && !this.surface.view.hasSlugAtOffset( fragment.getRange().start ) ) {
@@ -102,7 +102,7 @@ ve.ui.AnnotationInspector.prototype.onOpen = function () {
 		initialAnnotation = this.getMatchingAnnotations( fragment ).get( 0 );
 
 	// Parent method
-	ve.ui.Inspector.prototype.onOpen.call( this );
+	ve.ui.SurfaceInspector.prototype.onOpen.call( this );
 
 	// Initialization
 	this.initialAnnotation = initialAnnotation;
@@ -116,7 +116,7 @@ ve.ui.AnnotationInspector.prototype.onOpen = function () {
  */
 ve.ui.AnnotationInspector.prototype.onClose = function ( action ) {
 	// Parent method
-	ve.ui.Inspector.prototype.onClose.call( this, action );
+	ve.ui.SurfaceInspector.prototype.onClose.call( this, action );
 
 	var i, len, annotations,
 		insert = false,

modules/ve/ui/ve.ui.SurfaceDialog.js
@@ -1,26 +1,29 @@
 /*!
- * VisualEditor UserInterface MWDialog class.
+ * VisualEditor UserInterface SurfaceDialog class.
  *
  * @copyright 2011-2013 VisualEditor Team and others; see AUTHORS.txt
  * @license The MIT License (MIT); see LICENSE.txt
  */
 
 /**
- * UserInterface MediaWiki dialog.
+ * UserInterface surface dialog.
  *
  * @class
  * @abstract
- * @extends ve.ui.SurfaceDialog
+ * @extends ve.ui.Window
  *
  * @constructor
  * @param {ve.ui.SurfaceWindowSet} windowSet Window set this dialog is part of
  * @param {Object} [config] Configuration options
  */
-ve.ui.MWDialog = function VeUiMWDialog( windowSet, config ) {
+ve.ui.SurfaceDialog = function VeUiSurfaceDialog( windowSet, config ) {
 	// Parent constructor
-	ve.ui.SurfaceDialog.call( this, windowSet, config );
+	ve.ui.Dialog.call( this, windowSet, config );
+
+	// Properties
+	this.surface = windowSet.getSurface();
 };
 
 /* Inheritance */
 
-ve.inheritClass( ve.ui.MWDialog, ve.ui.SurfaceDialog );
+ve.inheritClass( ve.ui.SurfaceDialog, ve.ui.Dialog );

modules/ve/ui/ve.ui.SurfaceInspector.js
@@ -1,0 +1,49 @@
+/*!
+ * VisualEditor UserInterface SurfaceInspector class.
+ *
+ * @copyright 2011-2013 VisualEditor Team and others; see AUTHORS.txt
+ * @license The MIT License (MIT); see LICENSE.txt
+ */
+
+/**
+ * UserInterface surface inspector.
+ *
+ * @class
+ * @abstract
+ * @extends ve.ui.Inspector
+ *
+ * @constructor
+ * @param {ve.ui.SurfaceWindowSet} windowSet Window set this dialog is part of
+ * @param {Object} [config] Configuration options
+ */
+ve.ui.SurfaceInspector = function VeUiSurfaceInspector( windowSet, config ) {
+	// Parent constructor
+	ve.ui.Inspector.call( this, windowSet, config );
+
+	// Properties
+	this.surface = windowSet.getSurface();
+};
+
+/* Inheritance */
+
+ve.inheritClass( ve.ui.SurfaceInspector, ve.ui.Inspector );
+
+/* Methods */
+
+/**
+ * Handle inspector setup events.
+ *
+ * @method
+ */
+ve.ui.SurfaceInspector.prototype.onSetup = function () {
+	this.previousSelection = this.surface.getModel().getSelection();
+};
+
+/**
+ * Handle inspector open events.
+ *
+ * @method
+ */
+ve.ui.SurfaceInspector.prototype.onOpen = function () {
+	this.initialSelection = this.surface.getModel().getSelection();
+};

VisualEditor.php
@@ -429,9 +429,12 @@ $wgResourceModules += array(
 			've/ui/ve.ui.Frame.js',
 			've/ui/ve.ui.Window.js',
 			've/ui/ve.ui.WindowSet.js',
+			've/ui/ve.ui.SurfaceWindowSet.js',
 			've/ui/ve.ui.Inspector.js',
+			've/ui/ve.ui.SurfaceInspector.js',
 			've/ui/ve.ui.InspectorFactory.js',
 			've/ui/ve.ui.Dialog.js',
+			've/ui/ve.ui.SurfaceDialog.js',
 			've/ui/ve.ui.DialogFactory.js',
 			've/ui/ve.ui.Layout.js',
 			've/ui/ve.ui.Widget.js',

demos/ve/index.php
@@ -229,9 +229,12 @@ $html = file_get_contents( $page );
 		<script src="../../modules/ve/ui/ve.ui.Frame.js"></script>
 		<script src="../../modules/ve/ui/ve.ui.Window.js"></script>
 		<script src="../../modules/ve/ui/ve.ui.WindowSet.js"></script>
+		<script src="../../modules/ve/ui/ve.ui.SurfaceWindowSet.js"></script>
 		<script src="../../modules/ve/ui/ve.ui.Inspector.js"></script>
+		<script src="../../modules/ve/ui/ve.ui.SurfaceInspector.js"></script>
 		<script src="../../modules/ve/ui/ve.ui.InspectorFactory.js"></script>
 		<script src="../../modules/ve/ui/ve.ui.Dialog.js"></script>
+		<script src="../../modules/ve/ui/ve.ui.SurfaceDialog.js"></script>
 		<script src="../../modules/ve/ui/ve.ui.DialogFactory.js"></script>
 		<script src="../../modules/ve/ui/ve.ui.Layout.js"></script>
 		<script src="../../modules/ve/ui/ve.ui.Widget.js"></script>

modules/syntaxhighlight/ve.ui.MWSyntaxHighlightDialog.js
@@ -9,15 +9,15 @@
  * Document dialog.
  *
  * @class
- * @extends ve.ui.Dialog
+ * @extends ve.ui.MWDialog
  *
  * @constructor
- * @param {ve.ui.Surface} surface
+ * @param {ve.ui.WindowSet} windowSet Window set this dialog is part of
  * @param {Object} [config] Config options
  */
-ve.ui.MWSyntaxHighlightDialog = function VeUiMWSyntaxHighlightDialog( surface, config ) {
+ve.ui.MWSyntaxHighlightDialog = function VeUiMWSyntaxHighlightDialog( windowSet, config ) {
 	// Parent constructor
-	ve.ui.MWDialog.call( this, surface, config );
+	ve.ui.MWDialog.call( this, windowSet, config );
 };
 
 /* Inheritance */
@@ -41,7 +41,7 @@ ve.ui.MWSyntaxHighlightDialog.static.name = 'mwSyntaxHighlight';
  */
 ve.ui.MWSyntaxHighlightDialog.prototype.initialize = function () {
 	// Call parent method
-	ve.ui.Dialog.prototype.initialize.call( this );
+	ve.ui.MWDialog.prototype.initialize.call( this );
 	this.editPanel = new ve.ui.PanelLayout( {
 		'$$': this.frame.$$, 'scrollable': false, 'padded': false
 	} );
@@ -60,7 +60,7 @@ ve.ui.MWSyntaxHighlightDialog.prototype.initialize = function () {
  */
 ve.ui.MWSyntaxHighlightDialog.prototype.onOpen = function () {
 	// Parent method
-	ve.ui.Dialog.prototype.onOpen.call( this );
+	ve.ui.MWDialog.prototype.onOpen.call( this );
 	// Properties
 	this.sourceNode = this.surface.getView().getFocusedNode();
 	this.sourceText = this.sourceNode.getModel().getAttribute( 'body' );
@@ -81,7 +81,7 @@ ve.ui.MWSyntaxHighlightDialog.prototype.onClose = function ( action ) {
 	var tx,
 		doc = this.surface.getModel().getDocument();
 	// Parent method
-	ve.ui.Dialog.prototype.onClose.call( this );
+	ve.ui.MWDialog.prototype.onClose.call( this );
 	// Save changes via Transaction
 	if ( action === 'apply' ) {
 		tx = ve.dm.Transaction.newFromAttributeChanges(

commit 75270e24d58f763955533f9c9edea298281b5a95

Fixes for Trevor's crazy half-baked commit
Fix things that 4aa86d0f8 broke:
* Update surface parameter to windowSet in all ve.ui.SurfaceDialog subclasses
* Do the same for ve.ui.SurfaceInspector subclasses
* Fix @extends documentation for SurfaceDialog
* Fix documentation for ve.ui.SurfaceInspector, copypasta from SurfaceDialog

Bonus:
* Add .getMetaList() getter to dm.Surface

Change-Id: I843e99e45e9b013cb9cb559f050384d39bbbddf2

modules/ve-mw/ui/dialogs/ve.ui.MWBetaWelcomeDialog.js
@@ -13,15 +13,15 @@
  * @extends ve.ui.MWDialog
  *
  * @constructor
- * @param {ve.ui.Surface} surface
+ * @param {ve.ui.SurfaceWindowSet} windowSet Window set this dialog is part of
  * @param {Object} [config] Configuration options
  */
-ve.ui.MWBetaWelcomeDialog = function VeUiMWBetaWelcomeDialog( surface, config ) {
+ve.ui.MWBetaWelcomeDialog = function VeUiMWBetaWelcomeDialog( windowSet, config ) {
 	// Configuration initialization
 	config = ve.extendObject( { 'small': true, 'footless': false }, config );
 
 	// Parent constructor
-	ve.ui.MWDialog.call( this, surface, config );
+	ve.ui.MWDialog.call( this, windowSet, config );
 };
 
 /* Inheritance */

modules/ve-mw/ui/dialogs/ve.ui.MWMediaEditDialog.js
@@ -12,12 +12,12 @@
  * @extends ve.ui.MWDialog
  *
  * @constructor
- * @param {ve.ui.Surface} surface
+ * @param {ve.ui.SurfaceWindowSet} windowSet Window set this dialog is part of
  * @param {Object} [config] Configuration options
  */
-ve.ui.MWMediaEditDialog = function VeUiMWMediaEditDialog( surface, config ) {
+ve.ui.MWMediaEditDialog = function VeUiMWMediaEditDialog( windowSet, config ) {
 	// Parent constructor
-	ve.ui.MWDialog.call( this, surface, config );
+	ve.ui.MWDialog.call( this, windowSet, config );
 
 	// Properties
 	this.mediaNode = null;

modules/ve-mw/ui/dialogs/ve.ui.MWMediaInsertDialog.js
@@ -12,15 +12,15 @@
  * @extends ve.ui.MWDialog
  *
  * @constructor
- * @param {ve.ui.Surface} surface
+ * @param {ve.ui.SurfaceWindowSet} windowSet Window set this dialog is part of
  * @param {Object} [config] Configuration options
  */
-ve.ui.MWMediaInsertDialog = function VeUiMWMediaInsertDialog( surface, config ) {
+ve.ui.MWMediaInsertDialog = function VeUiMWMediaInsertDialog( windowSet, config ) {
 	// Configuration initialization
 	config = ve.extendObject( { 'footless': true }, config );
 
 	// Parent constructor
-	ve.ui.MWDialog.call( this, surface, config );
+	ve.ui.MWDialog.call( this, windowSet, config );
 
 	// Properties
 	this.item = null;

modules/ve-mw/ui/dialogs/ve.ui.MWMetaDialog.js
@@ -14,15 +14,15 @@
  * @extends ve.ui.MWDialog
  *
  * @constructor
- * @param {ve.ui.Surface} surface
+ * @param {ve.ui.SurfaceWindowSet} windowSet Window set this dialog is part of
  * @param {Object} [config] Configuration options
  */
-ve.ui.MWMetaDialog = function VeUiMWMetaDialog( surface, config ) {
+ve.ui.MWMetaDialog = function VeUiMWMetaDialog( windowSet, config ) {
 	// Parent constructor
-	ve.ui.MWDialog.call( this, surface, config );
+	ve.ui.MWDialog.call( this, windowSet, config );
 
 	// Properties
-	this.metaList = surface.getModel().metaList;
+	this.metaList = windowSet.getSurface().getModel().metaList;
 	this.defaultSortKeyChanged = false;
 	this.fallbackDefaultSortKey = mw.config.get( 'wgTitle' );
 

modules/ve-mw/ui/dialogs/ve.ui.MWReferenceDialog.js
@@ -12,12 +12,12 @@
  * @extends ve.ui.MWDialog
  *
  * @constructor
- * @param {ve.ui.Surface} surface
+ * @param {ve.ui.SurfaceWindowSet} windowSet Window set this dialog is part of
  * @param {Object} [config] Configuration options
  */
-ve.ui.MWReferenceDialog = function VeUiMWReferenceDialog( surface, config ) {
+ve.ui.MWReferenceDialog = function VeUiMWReferenceDialog( windowSet, config ) {
 	// Parent constructor
-	ve.ui.MWDialog.call( this, surface, config );
+	ve.ui.MWDialog.call( this, windowSet, config );
 
 	// Properties
 	this.ref = null;

modules/ve-mw/ui/dialogs/ve.ui.MWReferenceListDialog.js
@@ -12,12 +12,12 @@
  * @extends ve.ui.MWDialog
  *
  * @constructor
- * @param {ve.ui.Surface} surface
+ * @param {ve.ui.SurfaceWindowSet} windowSet Window set this dialog is part of
  * @param {Object} [config] Configuration options
  */
-ve.ui.MWReferenceListDialog = function VeUiMWReferenceListDialog( surface, config ) {
+ve.ui.MWReferenceListDialog = function VeUiMWReferenceListDialog( windowSet, config ) {
 	// Parent constructor
-	ve.ui.MWDialog.call( this, surface, config );
+	ve.ui.MWDialog.call( this, windowSet, config );
 };
 
 /* Inheritance */

modules/ve-mw/ui/dialogs/ve.ui.MWSaveDialog.js
@@ -14,15 +14,15 @@
  * @extends ve.ui.MWDialog
  *
  * @constructor
- * @param {ve.ui.Surface} surface
+ * @param {ve.ui.SurfaceWindowSet} windowSet Window set this dialog is part of
  * @param {Object} [config] Config options
  */
-ve.ui.MWSaveDialog = function VeUiMWSaveDialog( surface, config ) {
+ve.ui.MWSaveDialog = function VeUiMWSaveDialog( windowSet, config ) {
 	// Configuration initialization
 	config = ve.extendObject( { 'small': true }, config );
 
 	// Parent constructor
-	ve.ui.MWDialog.call( this, surface, config );
+	ve.ui.MWDialog.call( this, windowSet, config );
 
 	// Properties
 	this.sanityCheckVerified = false;

modules/ve-mw/ui/dialogs/ve.ui.MWTransclusionDialog.js
@@ -15,12 +15,12 @@
  * @extends ve.ui.MWDialog
  *
  * @constructor
- * @param {ve.ui.Surface} surface
+ * @param {ve.ui.SurfaceWindowSet} windowSet Window set this dialog is part of
  * @param {Object} [config] Configuration options
  */
-ve.ui.MWTransclusionDialog = function VeUiMWTransclusionDialog( surface, config ) {
+ve.ui.MWTransclusionDialog = function VeUiMWTransclusionDialog( windowSet, config ) {
 	// Parent constructor
-	ve.ui.MWDialog.call( this, surface, config );
+	ve.ui.MWDialog.call( this, windowSet, config );
 
 	// Properties
 	this.node = null;

modules/ve-mw/ui/inspectors/ve.ui.MWAlienExtensionInspector.js
@@ -12,12 +12,12 @@
  * @extends ve.ui.MWExtensionInspector
  *
  * @constructor
- * @param {ve.ui.Surface} surface
+ * @param {ve.ui.SurfaceWindowSet} windowSet Window set this inspector is part of
  * @param {Object} [config] Configuration options
  */
-ve.ui.MWAlienExtensionInspector = function VeUiMWAlienExtensionInspector( surface, config ) {
+ve.ui.MWAlienExtensionInspector = function VeUiMWAlienExtensionInspector( windowSet, config ) {
 	// Parent constructor
-	ve.ui.MWExtensionInspector.call( this, surface, config );
+	ve.ui.MWExtensionInspector.call( this, windowSet, config );
 };
 
 /* Inheritance */

modules/ve-mw/ui/inspectors/ve.ui.MWExtensionInspector.js
@@ -13,12 +13,12 @@
  * @extends ve.ui.SurfaceInspector
  *
  * @constructor
- * @param {ve.ui.Surface} surface
+ * @param {ve.ui.SurfaceWindowSet} windowSet Window set this inspector is part of
  * @param {Object} [config] Configuration options
  */
-ve.ui.MWExtensionInspector = function VeUiMWExtensionInspector( surface, config ) {
+ve.ui.MWExtensionInspector = function VeUiMWExtensionInspector( windowSet, config ) {
 	// Parent constructor
-	ve.ui.SurfaceInspector.call( this, surface, config );
+	ve.ui.SurfaceInspector.call( this, windowSet, config );
 };
 
 /* Inheritance */

modules/ve-mw/ui/inspectors/ve.ui.MWHieroInspector.js
@@ -12,12 +12,12 @@
  * @extends ve.ui.MWExtensionInspector
  *
  * @constructor
- * @param {ve.ui.Surface} surface
+ * @param {ve.ui.SurfaceWindowSet} windowSet Window set this inspector is part of
  * @param {Object} [config] Configuration options
  */
-ve.ui.MWHieroInspector = function VeUiMWHieroInspector( surface, config ) {
+ve.ui.MWHieroInspector = function VeUiMWHieroInspector( windowSet, config ) {
 	// Parent constructor
-	ve.ui.MWExtensionInspector.call( this, surface, config );
+	ve.ui.MWExtensionInspector.call( this, windowSet, config );
 };
 
 /* Inheritance */

modules/ve-mw/ui/inspectors/ve.ui.MWLinkInspector.js
@@ -14,12 +14,12 @@
  * @extends ve.ui.LinkInspector
  *
  * @constructor
- * @param {ve.ui.Surface} surface
+ * @param {ve.ui.SurfaceWindowSet} windowSet Window set this inspector is part of
  * @param {Object} [config] Configuration options
  */
-ve.ui.MWLinkInspector = function VeUiMWLinkInspector( surface, config ) {
+ve.ui.MWLinkInspector = function VeUiMWLinkInspector( windowSet, config ) {
 	// Parent constructor
-	ve.ui.LinkInspector.call( this, surface, config );
+	ve.ui.LinkInspector.call( this, windowSet, config );
 };
 
 /* Inheritance */

modules/ve-mw/ui/inspectors/ve.ui.MWMathInspector.js
@@ -12,12 +12,12 @@
  * @extends ve.ui.MWExtensionInspector
  *
  * @constructor
- * @param {ve.ui.Surface} surface
+ * @param {ve.ui.SurfaceWindowSet} windowSet Window set this inspector is part of
  * @param {Object} [config] Configuration options
  */
-ve.ui.MWMathInspector = function VeUiMWMathInspector( surface, config ) {
+ve.ui.MWMathInspector = function VeUiMWMathInspector( windowSet, config ) {
 	// Parent constructor
-	ve.ui.MWExtensionInspector.call( this, surface, config );
+	ve.ui.MWExtensionInspector.call( this, windowSet, config );
 
 	this.onChangeHandler = ve.debounce( ve.bind( this.updatePreview, this ), 250 );
 };

modules/ve/dm/ve.dm.Surface.js
@@ -251,6 +251,16 @@ ve.dm.Surface.prototype.getDocument = function () {
 };
 
 /**
+ * Get the meta list.
+ *
+ * @method
+ * @returns {ve.dm.MetaList} Meta list of the surface
+ */
+ve.dm.Surface.prototype.getMetaList = function () {
+	return this.metaList;
+};
+
+/**
  * Get the selection.
  *
  * @method

modules/ve/ui/inspectors/ve.ui.AnnotationInspector.js
@@ -13,12 +13,12 @@
  * @extends ve.ui.SurfaceInspector
  *
  * @constructor
- * @param {ve.ui.Surface} surface
+ * @param {ve.ui.SurfaceWindowSet} windowSet Window set this inspector is part of
  * @param {Object} [config] Configuration options
  */
-ve.ui.AnnotationInspector = function VeUiAnnotationInspector( surface, config ) {
+ve.ui.AnnotationInspector = function VeUiAnnotationInspector( windowSet, config ) {
 	// Parent constructor
-	ve.ui.SurfaceInspector.call( this, surface, config );
+	ve.ui.SurfaceInspector.call( this, windowSet, config );
 
 	// Properties
 	this.initialAnnotation = null;

modules/ve/ui/inspectors/ve.ui.LanguageInspector.js
@@ -12,12 +12,12 @@
  * @extends ve.ui.AnnotationInspector
  *
  * @constructor
- * @param {ve.ui.Surface} surface
+ * @param {ve.ui.SurfaceWindowSet} windowSet Window set this inspector is part of
  * @param {Object} [config] Configuration options
  */
-ve.ui.LanguageInspector = function VeUiLanguageInspector( surface, config ) {
+ve.ui.LanguageInspector = function VeUiLanguageInspector( windowSet, config ) {
 	// Parent constructor
-	ve.ui.AnnotationInspector.call( this, surface, config );
+	ve.ui.AnnotationInspector.call( this, windowSet, config );
 
 	// Placeholder for the dm properties:
 	this.lang = '';

modules/ve/ui/inspectors/ve.ui.LinkInspector.js
@@ -12,12 +12,12 @@
  * @extends ve.ui.AnnotationInspector
  *
  * @constructor
- * @param {ve.ui.Surface} surface
+ * @param {ve.ui.SurfaceWindowSet} windowSet Window set this inspector is part of
  * @param {Object} [config] Configuration options
  */
-ve.ui.LinkInspector = function VeUiLinkInspector( surface, config ) {
+ve.ui.LinkInspector = function VeUiLinkInspector( windowSet, config ) {
 	// Parent constructor
-	ve.ui.AnnotationInspector.call( this, surface, config );
+	ve.ui.AnnotationInspector.call( this, windowSet, config );
 };
 
 /* Inheritance */

modules/ve/ui/ve.ui.SurfaceDialog.js
@@ -10,7 +10,7 @@
  *
  * @class
  * @abstract
- * @extends ve.ui.Window
+ * @extends ve.ui.Dialog
  *
  * @constructor
  * @param {ve.ui.SurfaceWindowSet} windowSet Window set this dialog is part of

modules/ve/ui/ve.ui.SurfaceInspector.js
@@ -13,7 +13,7 @@
  * @extends ve.ui.Inspector
  *
  * @constructor
- * @param {ve.ui.SurfaceWindowSet} windowSet Window set this dialog is part of
+ * @param {ve.ui.SurfaceWindowSet} windowSet Window set this inspector is part of
  * @param {Object} [config] Configuration options
  */
 ve.ui.SurfaceInspector = function VeUiSurfaceInspector( windowSet, config ) {