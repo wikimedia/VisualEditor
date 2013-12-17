2b74982e95b339daf2197dbf983152b18c27362c
Jeff Hall <jhall@wikimedia.org>
2013-12-17 13:39:11 -0800

    [Browser test] New test for multi-edit workflow
    1) Per reviewer comments, moved some of the verbosity
       from feature to steps file in order to make
       the feature file a little more readable.
    2) Using random strings for the text of each edit
       in order to make every edit unique.
    3) Removing a commented line that wasn't needed.
    
    Bug: 57654
    
    Change-Id: Ib8b15964436d9c24556fac32977a64e2d5f22044

--- modules/ve-mw/test/browser/features/support/pages/visual_editor_page.rb
+++ modules/ve-mw/test/browser/features/support/pages/visual_editor_page.rb
@@ -53,6 +53,7 @@ class VisualEditorPage
     div(:content_box, class: 've-ce-documentNode ve-ce-branchNode', frame: frame)
     span(:leftarrowclose, class: 'oo-ui-iconedElement-icon oo-ui-icon-previous', frame: frame)
     text_field(:link_textfield, index: 0, frame: frame)
+    span(:another_save_page, class: 'oo-ui-labeledElement-label', text: 'Save page', frame: frame)
     list_item(:template_list_item, text: 'S', frame: frame)
     div(:ve_link_ui, class: 'oo-ui-window-title', frame: frame)
   end
