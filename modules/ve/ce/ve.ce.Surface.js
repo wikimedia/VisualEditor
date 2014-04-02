18171bfcafb541faeb53a4d374fbaa66c708a27c
Catrope <roan.kattouw@gmail.com>
2014-04-02 22:23:45 +0000

    Revert "Improve hasFocus check to compare the anchorNode to the parent"
    This reverts commit ef6096aeca87deb47245f605ead141f10838d098.
    
    Change-Id: I9d62a8411af578562b3bcbda85d53a9465e2baf5

--- modules/ve/ce/ve.ce.Surface.js
+++ modules/ve/ce/ve.ce.Surface.js
@@ -361,28 +361,15 @@ ve.ce.Surface.prototype.focus = function () {
  * @param {jQuery.Event} e focusin or focusout event
  */
 ve.ce.Surface.prototype.onFocusChange = function () {
-	/**
-	 * Check if a node is in, or equal to, any of a list of target nodes
-	 * @param {HTMLElement} node Node to find
-	 * @param {HTMLElement[]} targetNodes List of target nodes to search in
-	 * @returns {boolean} The node is in the list of target nodes
-	 */
-	function isInNodes( node, targetNodes ) {
-		var i;
-		for ( i = targetNodes.length - 1; i >= 0; i-- ) {
-			if ( node === targetNodes[i] || $.contains( targetNodes[i], node ) ) {
-				return true;
-			}
-		}
-		return false;
-	}
-
 	// Defer to let the selection update
 	setTimeout( ve.bind( function () {
-		var hasFocus = isInNodes( rangy.getSelection( this.getElementDocument() ).anchorNode, [
+		var hasFocus = $.contains(
 				this.documentView.getDocumentNode().$element[0],
-				this.$pasteTarget[0]
-			] );
+				rangy.getSelection( this.getElementDocument() ).anchorNode
+			) || $.contains(
+				this.$pasteTarget[0],
+				rangy.getSelection( this.getElementDocument() ).anchorNode
+			);
 
 		if ( hasFocus && !this.isFocused() ) {
 			this.documentOnFocus();
