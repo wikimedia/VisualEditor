a7ddd891ec89cdd4971571dd53279e1f72c1ac2e
Roan Kattouw <roan.kattouw@gmail.com>
2014-04-07 16:05:16 -0700

    Exclude insertions in ve.dm.SurfaceFragment#isolateAndUnwrap
    In cases where the separators we insert are adjacent to the range
    we're isolating, we don't want the range to grow to include these
    separators, as that breaks the subsequent unwrap.
    
    This range growth doesn't usually happen, because the typical situation is:
    [</list><list>]<li>|<p>Foo</p>|</li>[</list><list>]
    (i.e. the list is being split but the listItem is not), but it's possible to get:
    [</list><list>]<li>|<p>Foo</p>|[</li></list><list><li>]<list><li>...
    when converting a list item that contains a nested list, in which case the
    listItem does get split.
    
    Bug: 63209
    Change-Id: Iec3fecccf5d4ad545164f36ee73b7285f29c0e55

--- modules/ve/dm/ve.dm.SurfaceFragment.js
+++ modules/ve/dm/ve.dm.SurfaceFragment.js
@@ -886,7 +886,7 @@ ve.dm.SurfaceFragment.prototype.isolateAndUnwrap = function ( isolateForType ) {
 		return this;
 	}
 	var nodes, startSplitNode, endSplitNode,
-		startOffset, endOffset,
+		startOffset, endOffset, oldExclude,
 		outerDepth = 0,
 		factory = ve.dm.nodeFactory,
 		allowedParents = factory.getSuggestedParentNodeTypes( isolateForType ),
@@ -948,6 +948,12 @@ ve.dm.SurfaceFragment.prototype.isolateAndUnwrap = function ( isolateForType ) {
 		}
 	}
 
+	// We have to exclude insertions while doing splits, because we want the range to be
+	// exactly what we're isolating, we don't want it to grow to include the separators
+	// we're inserting (which would happen if one of them is immediately adjacent to the range)
+	oldExclude = this.willExcludeInsertions();
+	this.setExcludeInsertions( true );
+
 	if ( startSplitRequired ) {
 		createSplits( startSplitNodes, true );
 	}
@@ -955,6 +961,8 @@ ve.dm.SurfaceFragment.prototype.isolateAndUnwrap = function ( isolateForType ) {
 	if ( endSplitRequired ) {
 		createSplits( endSplitNodes, false );
 	}
+
+	this.setExcludeInsertions( oldExclude );
 
 	this.unwrapNodes( outerDepth, 0 );
 
