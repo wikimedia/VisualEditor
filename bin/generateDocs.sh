30c1a3d00c036ddc49702e0ce31c36596f06b8ea
Timo Tijhof <krinklemail@gmail.com>
2014-05-05 15:22:23 +0200

    generateDocs.sh: Simplify
    * Make more like mediawiki-core's mwjsduckgen.
      - Use bash set -e to have the script abort early when any
        part of it fails (especially the jsduck command).
      - Since it will abort early, we don't need to save the exitcode
        in a variable and return it again.
      - Don't "cd" into anything from the script, simply pass jsduck
        absolute paths instead.
    
    Change-Id: I4bb25e004cac63b81428431b57513655f671376e

--- bin/generateDocs.sh
+++ bin/generateDocs.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
-cd $(cd $(dirname $0)/..; pwd)
+set -e
+
+REPO_DIR=$(cd $(dirname $0)/..; pwd)
 
 # Disable parallel processing which seems to be causing problems under Ruby 1.8
-jsduck --config .docs/config.json --processes 0
-ec=$?
-
-cd - > /dev/null
-
-# Exit with exit code of jsduck command
-exit $ec
+jsduck --config $REPO_DIR/.docs/config.json --processes 0
