barionleg/visualegitor

Use ContextWidget instead of toolbar inside context menu
First step in context rewrite, moving toward the context not just being a
toolbar, but also a description of what is currently selected, showing the
URL of a link, the file name of an image, the content of a reference, etc.

Changes:
* Add ContextWidget and ContextItemWidget classes
* Use ContextWidget instead of Toolbar within the context menu
* Retain the ContextWidget, only clear it's items (toolbar used to destroy each time)
* Simplify ToolFactory's getToolsForX methods into getToolsForFragment
* Add getSelectedModels to SurfaceFragment. which getToolsForFragment uses
* Handle additional cases where methods called on null fragments could cause errors
* Code cleanup in SurfaceFragment (whitespace and comments)
* Added methods to NodeFactory and AnnotationFactory to get the description of a model from a view
* Add initial descriptions for some CE nodes
* Make InspectorTool's onUpdateState handler use getSelectedModels
* Made desktop context's menu use a compensated size (since it's in the body)

Change-Id: Ia2076a4238288ca113103e9ffef8c06ff80a21a4

modules/ve/ce/ve.ce.AnnotationFactory.js
@@ -24,6 +24,21 @@ OO.inheritClass( ve.ce.AnnotationFactory, OO.Factory );
 /* Methods */
 
 /**
+ * Get a plain text description of an annotation model.
+ *
+ * @param {ve.dm.Node} node Node to describe
+ * @returns {string} Description of the node
+ * @throws {Error} Unknown node type
+ */
+ve.ce.AnnotationFactory.prototype.getDescription = function ( annotation ) {
+	var type = annotation.constructor.static.name;
+	if ( type in this.registry ) {
+		return this.registry[type].static.getDescription( annotation );
+	}
+	throw new Error( 'Unknown node type: ' + type );
+};
+
+/**
  * Check if an annotation needs to force continuation
  * @param {string} type Annotation type
  * @returns {boolean} Whether the annotation needs to force continuation

modules/ve/ui/widgets/ve.ui.ContextItemWidget.js
@@ -1,0 +1,55 @@
+/**
+ * Proxy for a tool, displaying information about the current context.
+ *
+ * Use with ve.ui.ContextWidget.
+ *
+ * @class
+ * @extends OO.ui.OptionWidget
+ *
+ * @constructor
+ * @param {Object} data Item data
+ * @param {Function} tool Tool item is a proxy for
+ * @param {ve.dm.Node|ve.dm.Annotation} model Node or annotation item is related to
+ * @param {Object} [config] Configuration options
+ */
+ve.ui.ContextItemWidget = function OoUiContextItemWidget( data, tool, model, config ) {
+	// Config intialization
+	config = config || {};
+
+	// Parent constructor
+	ve.ui.ContextItemWidget.super.call( this, data, config );
+
+	// Properties
+	this.tool = tool;
+	this.model = model;
+
+	// Initialization
+	this.$element.addClass( 've-ui-contextItemWidget' );
+	this.setIcon( this.tool.static.icon );
+	this.setLabel( this.getDescription() );
+};
+
+/* Setup */
+
+OO.inheritClass( ve.ui.ContextItemWidget, OO.ui.OptionWidget );
+
+/* Methods */
+
+ve.ui.ContextItemWidget.prototype.getDescription = function () {
+	var description;
+
+	if ( this.model instanceof ve.dm.Annotation ) {
+		description = ve.ce.annotationFactory.getDescription( this.model );
+	} else if ( this.model instanceof ve.dm.Node ) {
+		description = ve.ce.nodeFactory.getDescription( this.model );
+	}
+	if ( !description ) {
+		description = this.tool.static.title;
+	}
+
+	return description;
+};
+
+ve.ui.ContextItemWidget.prototype.getCommand = function () {
+	return ve.ui.commandRegistry.lookup( this.tool.static.commandName );
+};

modules/ve/ui/widgets/ve.ui.ContextWidget.js
@@ -1,0 +1,37 @@
+/**
+ * List of context items, displaying information about the current context.
+ *
+ * Use with ve.ui.ContextItemWidget.
+ *
+ * @class
+ * @extends OO.ui.SelectWidget
+ *
+ * @constructor
+ * @param {Object} [config] Configuration options
+ */
+ve.ui.ContextWidget = function OoUiContextWidget( config ) {
+	// Config intialization
+	config = config || {};
+
+	// Parent constructor
+	ve.ui.ContextWidget.super.call( this, config );
+
+	this.connect( this, { 'choose': 'onChooseItem' } );
+
+	// Initialization
+	this.$element.addClass( 've-ui-contextWidget' );
+};
+
+/* Setup */
+
+OO.inheritClass( ve.ui.ContextWidget, OO.ui.SelectWidget );
+
+/* Methods */
+
+/**
+ * Handle choose item events.
+ */
+ve.ui.ContextWidget.prototype.onChooseItem = function () {
+	// Auto-deselect
+	this.selectItem( null );
+};

wikimedia/VisualEditor

modules/ve/ce/ve.ce.AnnotationFactory.js
@@ -24,6 +24,21 @@ OO.inheritClass( ve.ce.AnnotationFactory, OO.Factory );
 /* Methods */
 
 /**
+ * Get a plain text description of an annotation model.
+ *
+ * @param {ve.dm.Annotation} annotation Annotation to describe
+ * @returns {string} Description of the annotation
+ * @throws {Error} Unknown annotation type
+ */
+ve.ce.AnnotationFactory.prototype.getDescription = function ( annotation ) {
+	var type = annotation.constructor.static.name;
+	if ( type in this.registry ) {
+		return this.registry[type].static.getDescription( annotation );
+	}
+	throw new Error( 'Unknown annotation type: ' + type );
+};
+
+/**
  * Check if an annotation needs to force continuation
  * @param {string} type Annotation type
  * @returns {boolean} Whether the annotation needs to force continuation

modules/ve/ui/widgets/ve.ui.ContextItemWidget.js
@@ -1,0 +1,65 @@
+/**
+ * Proxy for a tool, displaying information about the current context.
+ *
+ * Use with ve.ui.ContextWidget.
+ *
+ * @class
+ * @extends OO.ui.OptionWidget
+ *
+ * @constructor
+ * @param {Object} data Item data
+ * @param {Function} tool Tool item is a proxy for
+ * @param {ve.dm.Node|ve.dm.Annotation} model Node or annotation item is related to
+ * @param {Object} [config] Configuration options
+ */
+ve.ui.ContextItemWidget = function VeUiContextItemWidget( data, tool, model, config ) {
+	// Config intialization
+	config = config || {};
+
+	// Parent constructor
+	ve.ui.ContextItemWidget.super.call( this, data, config );
+
+	// Properties
+	this.tool = tool;
+	this.model = model;
+
+	// Initialization
+	this.$element.addClass( 've-ui-contextItemWidget' );
+	this.setIcon( this.tool.static.icon );
+	this.setLabel( this.getDescription() );
+};
+
+/* Setup */
+
+OO.inheritClass( ve.ui.ContextItemWidget, OO.ui.OptionWidget );
+
+/* Methods */
+
+/**
+ * Get a description of the model.
+ *
+ * @return {string} Description of model
+ */
+ve.ui.ContextItemWidget.prototype.getDescription = function () {
+	var description;
+
+	if ( this.model instanceof ve.dm.Annotation ) {
+		description = ve.ce.annotationFactory.getDescription( this.model );
+	} else if ( this.model instanceof ve.dm.Node ) {
+		description = ve.ce.nodeFactory.getDescription( this.model );
+	}
+	if ( !description ) {
+		description = this.tool.static.title;
+	}
+
+	return description;
+};
+
+/**
+ * Get the command for this item.
+ *
+ * @return {ve.ui.Command} Command
+ */
+ve.ui.ContextItemWidget.prototype.getCommand = function () {
+	return ve.ui.commandRegistry.lookup( this.tool.static.commandName );
+};

modules/ve/ui/widgets/ve.ui.ContextWidget.js
@@ -1,0 +1,37 @@
+/**
+ * List of context items, displaying information about the current context.
+ *
+ * Use with ve.ui.ContextItemWidget.
+ *
+ * @class
+ * @extends OO.ui.SelectWidget
+ *
+ * @constructor
+ * @param {Object} [config] Configuration options
+ */
+ve.ui.ContextWidget = function VeUiContextWidget( config ) {
+	// Config intialization
+	config = config || {};
+
+	// Parent constructor
+	ve.ui.ContextWidget.super.call( this, config );
+
+	this.connect( this, { 'choose': 'onChooseItem' } );
+
+	// Initialization
+	this.$element.addClass( 've-ui-contextWidget' );
+};
+
+/* Setup */
+
+OO.inheritClass( ve.ui.ContextWidget, OO.ui.SelectWidget );
+
+/* Methods */
+
+/**
+ * Handle choose item events.
+ */
+ve.ui.ContextWidget.prototype.onChooseItem = function () {
+	// Auto-deselect
+	this.selectItem( null );
+};