barionleg/visualegitor

Improved layout of language input widget
* Put 'Find language' in a layout
* Switch out text of button with autonym, give it a fixed
  width and ellipsis overflow for this purpose.
* Given button an indicator to show that it launches a
  select widget.

Change-Id: Icd1eae3a3006b1516f410c4000461ec66b33bcb1

modules/ve/ui/widgets/ve.ui.LanguageInputWidget.js
@@ -21,14 +21,21 @@ ve.ui.LanguageInputWidget = function VeUiLanguageInputWidget( config ) {
 	// Parent constructor
 	OO.ui.Widget.call( this, config );
 
-	var languageField, directionField, surface, searchDialog, changeButton;
+	var findLangaugeField, languageCodeField, directionField, surface, searchDialog;
 
 	// Properties
 	this.annotation = null;
 
-	this.languageTextInput = new OO.ui.TextInputWidget( {
+	this.findLanguageButton = new OO.ui.ButtonWidget( {
 		'$': this.$,
-		'classes': [ 've-ui-languageInputWidget-languageTextInput' ]
+		'classes': [ 've-ui-languageInputWidget-findLanguageButton' ],
+		'label': ve.msg( 'visualeditor-languageinspector-widget-changelang' ),
+		'indicator': 'down'
+	} );
+
+	this.languageCodeTextInput = new OO.ui.TextInputWidget( {
+		'$': this.$,
+		'classes': [ 've-ui-languageInputWidget-languageCodeTextInput' ]
 	} );
 
 	this.directionSelect = new OO.ui.ButtonSelectWidget( {
@@ -41,7 +48,13 @@ ve.ui.LanguageInputWidget = function VeUiLanguageInputWidget( config ) {
 	] );
 
 	// Initialization
-	languageField = new OO.ui.FieldLayout( this.languageTextInput, {
+	findLangaugeField = new OO.ui.FieldLayout( this.findLanguageButton, {
+			'$': this.$,
+			'align': 'left',
+			'label': ve.msg( 'visualeditor-languageinspector-widget-label-language' )
+		}
+	);
+	languageCodeField = new OO.ui.FieldLayout( this.languageCodeTextInput, {
 			'$': this.$,
 			'align': 'left',
 			'label': ve.msg( 'visualeditor-languageinspector-widget-label-langcode' )
@@ -58,12 +71,6 @@ ve.ui.LanguageInputWidget = function VeUiLanguageInputWidget( config ) {
 	// overlay and open windows.
 	surface = new ve.ui.DesktopSurface( [], { '$': this.$ } );
 	searchDialog = surface.getDialogs().getWindow( 'languageSearch' );
-	changeButton = new OO.ui.ButtonWidget( {
-		'$': this.$,
-		'classes': [ 've-ui-languageInputWidget-changeButton' ],
-		'label': ve.msg( 'visualeditor-languageinspector-widget-changelang' ),
-		'flags': ['primary']
-	} );
 
 	// Skip full Surface initialize and just attach the global overlay
 	$( 'body' ).append( surface.$globalOverlay );
@@ -72,12 +79,12 @@ ve.ui.LanguageInputWidget = function VeUiLanguageInputWidget( config ) {
 	// TODO: Consider using getAutonym to display a nicer language name label somewhere
 	this.$element
 		.addClass( 've-ui-langInputWidget' )
-		.append( changeButton.$element, languageField.$element, directionField.$element );
+		.append( findLangaugeField.$element, languageCodeField.$element, directionField.$element );
 
 	// Events
-	this.languageTextInput.connect( this, { 'change': 'onChange' } );
+	this.findLanguageButton.connect( searchDialog, { 'click': 'open' } );
+	this.languageCodeTextInput.connect( this, { 'change': 'onChange' } );
 	this.directionSelect.connect( this, { 'select': 'onChange' } );
-	changeButton.connect( searchDialog, { 'click': 'open' } );
 	searchDialog.on( 'close', ve.bind( function ( data ) {
 		if ( data.action === 'apply' ) {
 			this.setAnnotationFromValues( data.lang, data.dir );
@@ -107,7 +114,7 @@ ve.ui.LanguageInputWidget.prototype.onChange = function () {
 	}
 
 	var selectedItem = this.directionSelect.getSelectedItem();
-	this.setAnnotationFromValues( this.languageTextInput.getValue(), selectedItem ? selectedItem.getData() : null );
+	this.setAnnotationFromValues( this.languageCodeTextInput.getValue(), selectedItem ? selectedItem.getData() : null );
 };
 
 /**
@@ -127,12 +134,17 @@ ve.ui.LanguageInputWidget.prototype.setAnnotation = function ( annotation ) {
 	// Set state flag while programmatically changing input widget values
 	this.updating = true;
 	if ( annotation ) {
-		this.languageTextInput.setValue( annotation.getAttribute( 'lang' ) );
+		this.languageCodeTextInput.setValue( annotation.getAttribute( 'lang' ) );
+		this.findLanguageButton.setLabel(
+			$.uls.data.getAutonym( annotation.getAttribute( 'lang' ).toLowerCase() ) ||
+			ve.msg( 'visualeditor-languageinspector-widget-changelang' )
+		);
 		this.directionSelect.selectItem(
 			this.directionSelect.getItemFromData( annotation.getAttribute( 'dir' ) || null )
 		);
 	} else {
-		this.languageTextInput.setValue( '' );
+		this.languageCodeTextInput.setValue( '' );
+		this.findLanguageButton.setLabel( ve.msg( 'visualeditor-languageinspector-widget-changelang' ) );
 		this.directionSelect.selectItem( this.directionSelect.getItemFromData( null ) );
 	}
 	this.updating = false;

wikimedia/VisualEditor

modules/ve/ui/widgets/ve.ui.LanguageInputWidget.js
@@ -21,14 +21,21 @@ ve.ui.LanguageInputWidget = function VeUiLanguageInputWidget( config ) {
 	// Parent constructor
 	OO.ui.Widget.call( this, config );
 
-	var languageField, directionField, surface, searchDialog, changeButton;
+	var findLanguageField, languageCodeField, directionField, surface, searchDialog;
 
 	// Properties
 	this.annotation = null;
 
-	this.languageTextInput = new OO.ui.TextInputWidget( {
+	this.findLanguageButton = new OO.ui.ButtonWidget( {
 		'$': this.$,
-		'classes': [ 've-ui-languageInputWidget-languageTextInput' ]
+		'classes': [ 've-ui-languageInputWidget-findLanguageButton' ],
+		'label': ve.msg( 'visualeditor-languageinspector-widget-changelang' ),
+		'indicator': 'next'
+	} );
+
+	this.languageCodeTextInput = new OO.ui.TextInputWidget( {
+		'$': this.$,
+		'classes': [ 've-ui-languageInputWidget-languageCodeTextInput' ]
 	} );
 
 	this.directionSelect = new OO.ui.ButtonSelectWidget( {
@@ -41,7 +48,13 @@ ve.ui.LanguageInputWidget = function VeUiLanguageInputWidget( config ) {
 	] );
 
 	// Initialization
-	languageField = new OO.ui.FieldLayout( this.languageTextInput, {
+	findLanguageField = new OO.ui.FieldLayout( this.findLanguageButton, {
+			'$': this.$,
+			'align': 'left',
+			'label': ve.msg( 'visualeditor-languageinspector-widget-label-language' )
+		}
+	);
+	languageCodeField = new OO.ui.FieldLayout( this.languageCodeTextInput, {
 			'$': this.$,
 			'align': 'left',
 			'label': ve.msg( 'visualeditor-languageinspector-widget-label-langcode' )
@@ -58,12 +71,6 @@ ve.ui.LanguageInputWidget = function VeUiLanguageInputWidget( config ) {
 	// overlay and open windows.
 	surface = new ve.ui.DesktopSurface( [], { '$': this.$ } );
 	searchDialog = surface.getDialogs().getWindow( 'languageSearch' );
-	changeButton = new OO.ui.ButtonWidget( {
-		'$': this.$,
-		'classes': [ 've-ui-languageInputWidget-changeButton' ],
-		'label': ve.msg( 'visualeditor-languageinspector-widget-changelang' ),
-		'flags': ['primary']
-	} );
 
 	// Skip full Surface initialize and just attach the global overlay
 	$( 'body' ).append( surface.$globalOverlay );
@@ -72,12 +79,12 @@ ve.ui.LanguageInputWidget = function VeUiLanguageInputWidget( config ) {
 	// TODO: Consider using getAutonym to display a nicer language name label somewhere
 	this.$element
 		.addClass( 've-ui-langInputWidget' )
-		.append( changeButton.$element, languageField.$element, directionField.$element );
+		.append( findLanguageField.$element, languageCodeField.$element, directionField.$element );
 
 	// Events
-	this.languageTextInput.connect( this, { 'change': 'onChange' } );
+	this.findLanguageButton.connect( searchDialog, { 'click': 'open' } );
+	this.languageCodeTextInput.connect( this, { 'change': 'onChange' } );
 	this.directionSelect.connect( this, { 'select': 'onChange' } );
-	changeButton.connect( searchDialog, { 'click': 'open' } );
 	searchDialog.on( 'teardown', ve.bind( function ( data ) {
 		if ( data.action === 'apply' ) {
 			this.setAnnotationFromValues( data.lang, data.dir );
@@ -107,7 +114,7 @@ ve.ui.LanguageInputWidget.prototype.onChange = function () {
 	}
 
 	var selectedItem = this.directionSelect.getSelectedItem();
-	this.setAnnotationFromValues( this.languageTextInput.getValue(), selectedItem ? selectedItem.getData() : null );
+	this.setAnnotationFromValues( this.languageCodeTextInput.getValue(), selectedItem ? selectedItem.getData() : null );
 };
 
 /**
@@ -127,12 +134,17 @@ ve.ui.LanguageInputWidget.prototype.setAnnotation = function ( annotation ) {
 	// Set state flag while programmatically changing input widget values
 	this.updating = true;
 	if ( annotation ) {
-		this.languageTextInput.setValue( annotation.getAttribute( 'lang' ) );
+		this.languageCodeTextInput.setValue( annotation.getAttribute( 'lang' ) );
+		this.findLanguageButton.setLabel(
+			$.uls.data.getAutonym( annotation.getAttribute( 'lang' ).toLowerCase() ) ||
+			ve.msg( 'visualeditor-languageinspector-widget-changelang' )
+		);
 		this.directionSelect.selectItem(
 			this.directionSelect.getItemFromData( annotation.getAttribute( 'dir' ) || null )
 		);
 	} else {
-		this.languageTextInput.setValue( '' );
+		this.languageCodeTextInput.setValue( '' );
+		this.findLanguageButton.setLabel( ve.msg( 'visualeditor-languageinspector-widget-changelang' ) );
 		this.directionSelect.selectItem( this.directionSelect.getItemFromData( null ) );
 	}
 	this.updating = false;