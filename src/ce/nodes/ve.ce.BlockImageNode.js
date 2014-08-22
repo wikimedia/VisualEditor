barionleg/visualegitor

Basic block image (figure/figcaption) support
Change-Id: Iddb3a5f57fd0ab965db00c2b148ab4f5e89b923d

src/ce/nodes/ve.ce.BlockImageNode.js
@@ -1,0 +1,101 @@
+/*!
+ * VisualEditor ContentEditable ImageNode class.
+ *
+ * @copyright 2011-2014 VisualEditor Team and others; see AUTHORS.txt
+ * @license The MIT License (MIT); see LICENSE.txt
+ */
+
+/**
+ * ContentEditable block image node.
+ *
+ * @class
+ * @extends ve.ce.BranchNode
+ * @mixins ve.ce.FocusableNode
+ * @mixins ve.ce.ResizableNode
+ *
+ * @constructor
+ * @param {ve.dm.BlockImageNode} model Model to observe
+ * @param {Object} [config] Configuration options
+ */
+ve.ce.BlockImageNode = function VeCeImageNode( model, config ) {
+	config = ve.extendObject( {
+		minDimensions: { width: 1, height: 1 }
+	}, config );
+
+	// Parent constructor
+	ve.ce.BranchNode.call( this, model, config );
+
+	// Build DOM
+	this.$image = this.$( '<img>' )
+		.attr( 'src', this.getResolvedAttribute( 'src' ) )
+		.prependTo( this.$element );
+
+	// Mixin constructors
+	ve.ce.FocusableNode.call( this );
+	ve.ce.ResizableNode.call( this, this.$image, config );
+
+	// Events
+	this.$image.on( 'load', ve.bind( this.onLoad, this ) );
+	this.model.connect( this, { attributeChange: 'onAttributeChange' } );
+
+	// Initialization
+	this.$element.addClass( 've-ce-imageNode' );
+	this.$image
+		.attr( {
+			alt: this.model.getAttribute( 'alt' ),
+			src: this.getResolvedAttribute( 'src' )
+		} )
+		.css( {
+			width: this.model.getAttribute( 'width' ),
+			height: this.model.getAttribute( 'height' )
+		} );
+};
+
+/* Inheritance */
+
+OO.inheritClass( ve.ce.BlockImageNode, ve.ce.BranchNode );
+
+OO.mixinClass( ve.ce.BlockImageNode, ve.ce.FocusableNode );
+OO.mixinClass( ve.ce.BlockImageNode, ve.ce.ResizableNode );
+
+/* Static Properties */
+
+ve.ce.BlockImageNode.static.name = 'blockImage';
+
+ve.ce.BlockImageNode.static.tagName = 'figure';
+
+/* Methods */
+
+/**
+ * Update the rendering of the 'src', 'width' and 'height' attributes when they change in the model.
+ *
+ * @method
+ * @param {string} key Attribute key
+ * @param {string} from Old value
+ * @param {string} to New value
+ */
+ve.ce.BlockImageNode.prototype.onAttributeChange = function ( key, from, to ) {
+	if ( key === 'src' ) {
+		this.$image.attr( 'src', this.getResolvedAttribute( 'src' ) );
+	}
+	if ( key === 'width' || key === 'height' ) {
+		this.$image.css( key, to !== null ? to : '' );
+	}
+};
+
+/**
+ * Handle the image load
+ *
+ * @method
+ * @param {jQuery.Event} e Load event
+ */
+ve.ce.BlockImageNode.prototype.onLoad = function () {
+	this.setOriginalDimensions( {
+		width: this.$image.prop( 'naturalWidth' ),
+		height: this.$image.prop( 'naturalHeight' )
+	} );
+};
+
+/* Registration */
+
+ve.ce.nodeFactory.register( ve.ce.BlockImageNode );

src/dm/nodes/ve.dm.BlockImageNode.js
@@ -1,0 +1,146 @@
+/*!
+ * VisualEditor DataModel BlockImageNode class.
+ *
+ * @copyright 2011-2014 VisualEditor Team and others; see AUTHORS.txt
+ * @license The MIT License (MIT); see LICENSE.txt
+ */
+
+/**
+ * DataModel block image node.
+ *
+ * @class
+ * @extends ve.dm.BranchNode
+ * @mixins ve.dm.ResizableNode
+ *
+ * @constructor
+ * @param {Object} [element] Reference to element in linear model
+ * @param {ve.dm.Node[]} [children]
+ */
+ve.dm.BlockImageNode = function VeDmBlockImageNode() {
+	// Parent constructor
+	ve.dm.BranchNode.apply( this, arguments );
+
+	// Mixin constructor
+	ve.dm.ResizableNode.call( this );
+};
+
+/* Inheritance */
+
+OO.inheritClass( ve.dm.BlockImageNode, ve.dm.BranchNode );
+
+OO.mixinClass( ve.dm.BlockImageNode, ve.dm.ResizableNode );
+
+/* Static Properties */
+
+ve.dm.BlockImageNode.static.name = 'blockImage';
+
+ve.dm.BlockImageNode.static.storeHtmlAttributes = {
+	'blacklist': [ 'typeof', 'class', 'src', 'resource', 'width', 'height', 'href', 'rel' ]
+};
+
+ve.dm.BlockImageNode.static.handlesOwnChildren = true;
+
+ve.dm.BlockImageNode.static.childNodeTypes = [ 'imageCaption' ];
+
+ve.dm.BlockImageNode.static.matchTagNames = [ 'figure' ];
+
+//ve.dm.BlockImageNode.static.blacklistedAnnotationTypes = [ 'link' ];
+
+ve.dm.BlockImageNode.static.toDataElement = function ( domElements, converter ) {
+	var dataElement,
+		$figure = $( domElements[0] ),
+		$img = $figure.children( 'img' ).eq( 0 ),
+		$caption = $figure.children( 'figcaption' ).eq( 0 ),
+		attributes = {
+			src: $img.attr( 'src' )
+		},
+		width = $img.attr( 'width' ),
+		height = $img.attr( 'height' ),
+		altText = $img.attr( 'alt' );
+
+	if ( altText !== undefined ) {
+		attributes.alt = altText;
+	}
+
+	attributes.width = width !== undefined && width !== '' ? Number( width ) : null;
+	attributes.height = height !== undefined && height !== '' ? Number( height ) : null;
+
+	dataElement = { 'type': this.name, 'attributes': attributes };
+
+	if ( $caption.length === 0 ) {
+		return [
+			dataElement,
+			{ 'type': 'imageCaption' },
+			{ 'type': 'imageCaption' },
+			{ 'type': '/' + this.name }
+		];
+	} else {
+		return [ dataElement ].
+			concat( converter.getDataFromDomClean( $caption[0], { 'type': 'imageCaption' } ) ).
+			concat( [ { 'type': '/' + this.name } ] );
+	}
+};
+
+// TODO: Consider using jQuery instead of pure JS.
+// TODO: At this moment node is not resizable but when it will be then adding defaultSize class
+// should be more conditional.
+ve.dm.BlockImageNode.static.toDomElements = function ( data, doc, converter ) {
+	var dataElement = data[0],
+		width = dataElement.attributes.width,
+		height = dataElement.attributes.height,
+		figure = doc.createElement( 'figure' ),
+		img = doc.createElement( 'img' ),
+		wrapper = doc.createElement( 'div' ),
+		captionData = data.slice( 1, -1 );
+
+	img.setAttribute( 'src', dataElement.attributes.src );
+	img.setAttribute( 'width', width );
+	img.setAttribute( 'height', height );
+	if ( dataElement.attributes.alt !== undefined ) {
+		img.setAttribute( 'alt', dataElement.attributes.alt );
+	}
+	figure.appendChild( img );
+
+	// If length of captionData is smaller or equal to 2 it means that there is no caption or that
+	// it is empty - in both cases we are going to skip appending <figcaption>.
+	if ( captionData.length > 2 ) {
+		converter.getDomSubtreeFromData( data.slice( 1, -1 ), wrapper );
+		while ( wrapper.firstChild ) {
+			figure.appendChild( wrapper.firstChild );
+		}
+	}
+	return [ figure ];
+};
+
+/* Methods */
+
+/**
+ * Get the caption node of the image.
+ *
+ * @method
+ * @returns {ve.dm.ImageCaptionNode|null} Caption node, if present
+ */
+ve.dm.BlockImageNode.prototype.getCaptionNode = function () {
+	var node = this.children[0];
+	return node instanceof ve.dm.ImageCaptionNode ? node : null;
+};
+
+/**
+ * @inheritdoc
+ */
+ve.dm.BlockImageNode.prototype.createScalable = function () {
+	return new ve.dm.Scalable( {
+		currentDimensions: {
+			width: this.getAttribute( 'width' ),
+			height: this.getAttribute( 'height' )
+		},
+		minDimensions: {
+			width: 1,
+			height: 1
+		}
+	} );
+};
+
+/* Registration */
+
+ve.dm.modelRegistry.register( ve.dm.BlockImageNode );

wikimedia/VisualEditor

src/ce/nodes/ve.ce.BlockImageNode.js
@@ -1,0 +1,101 @@
+/*!
+ * VisualEditor ContentEditable block image node class.
+ *
+ * @copyright 2011-2014 VisualEditor Team and others; see AUTHORS.txt
+ * @license The MIT License (MIT); see LICENSE.txt
+ */
+
+/**
+ * ContentEditable block image node.
+ *
+ * @class
+ * @extends ve.ce.BranchNode
+ * @mixins ve.ce.FocusableNode
+ * @mixins ve.ce.ResizableNode
+ *
+ * @constructor
+ * @param {ve.dm.BlockImageNode} model Model to observe
+ * @param {Object} [config] Configuration options
+ */
+ve.ce.BlockImageNode = function VeCeBlockImageNode( model, config ) {
+	config = ve.extendObject( {
+		minDimensions: { width: 1, height: 1 }
+	}, config );
+
+	// Parent constructor
+	ve.ce.BranchNode.call( this, model, config );
+
+	// Build DOM
+	this.$image = this.$( '<img>' )
+		.attr( 'src', this.getResolvedAttribute( 'src' ) )
+		.prependTo( this.$element );
+
+	// Mixin constructors
+	ve.ce.FocusableNode.call( this );
+	ve.ce.ResizableNode.call( this, this.$image, config );
+
+	// Events
+	this.$image.on( 'load', ve.bind( this.onLoad, this ) );
+	this.model.connect( this, { attributeChange: 'onAttributeChange' } );
+
+	// Initialization
+	this.$element.addClass( 've-ce-imageNode' );
+	this.$image
+		.attr( {
+			alt: this.model.getAttribute( 'alt' ),
+			src: this.getResolvedAttribute( 'src' )
+		} )
+		.css( {
+			width: this.model.getAttribute( 'width' ),
+			height: this.model.getAttribute( 'height' )
+		} );
+};
+
+/* Inheritance */
+
+OO.inheritClass( ve.ce.BlockImageNode, ve.ce.BranchNode );
+
+OO.mixinClass( ve.ce.BlockImageNode, ve.ce.FocusableNode );
+OO.mixinClass( ve.ce.BlockImageNode, ve.ce.ResizableNode );
+
+/* Static Properties */
+
+ve.ce.BlockImageNode.static.name = 'blockImage';
+
+ve.ce.BlockImageNode.static.tagName = 'figure';
+
+/* Methods */
+
+/**
+ * Update the rendering of the 'src', 'width' and 'height' attributes when they change in the model.
+ *
+ * @method
+ * @param {string} key Attribute key
+ * @param {string} from Old value
+ * @param {string} to New value
+ */
+ve.ce.BlockImageNode.prototype.onAttributeChange = function ( key, from, to ) {
+	if ( key === 'src' ) {
+		this.$image.attr( 'src', this.getResolvedAttribute( 'src' ) );
+	}
+	if ( key === 'width' || key === 'height' ) {
+		this.$image.css( key, to !== null ? to : '' );
+	}
+};
+
+/**
+ * Handle the image load
+ *
+ * @method
+ * @param {jQuery.Event} e Load event
+ */
+ve.ce.BlockImageNode.prototype.onLoad = function () {
+	this.setOriginalDimensions( {
+		width: this.$image.prop( 'naturalWidth' ),
+		height: this.$image.prop( 'naturalHeight' )
+	} );
+};
+
+/* Registration */
+
+ve.ce.nodeFactory.register( ve.ce.BlockImageNode );

src/dm/nodes/ve.dm.BlockImageNode.js
@@ -1,0 +1,146 @@
+/*!
+ * VisualEditor DataModel BlockImageNode class.
+ *
+ * @copyright 2011-2014 VisualEditor Team and others; see AUTHORS.txt
+ * @license The MIT License (MIT); see LICENSE.txt
+ */
+
+/**
+ * DataModel block image node.
+ *
+ * @class
+ * @extends ve.dm.BranchNode
+ * @mixins ve.dm.ResizableNode
+ *
+ * @constructor
+ * @param {Object} [element] Reference to element in linear model
+ * @param {ve.dm.Node[]} [children]
+ */
+ve.dm.BlockImageNode = function VeDmBlockImageNode() {
+	// Parent constructor
+	ve.dm.BranchNode.apply( this, arguments );
+
+	// Mixin constructor
+	ve.dm.ResizableNode.call( this );
+};
+
+/* Inheritance */
+
+OO.inheritClass( ve.dm.BlockImageNode, ve.dm.BranchNode );
+
+OO.mixinClass( ve.dm.BlockImageNode, ve.dm.ResizableNode );
+
+/* Static Properties */
+
+ve.dm.BlockImageNode.static.name = 'blockImage';
+
+ve.dm.BlockImageNode.static.storeHtmlAttributes = {
+	blacklist: [ 'typeof', 'class', 'src', 'resource', 'width', 'height', 'href', 'rel' ]
+};
+
+ve.dm.BlockImageNode.static.handlesOwnChildren = true;
+
+ve.dm.BlockImageNode.static.childNodeTypes = [ 'imageCaption' ];
+
+ve.dm.BlockImageNode.static.matchTagNames = [ 'figure' ];
+
+//ve.dm.BlockImageNode.static.blacklistedAnnotationTypes = [ 'link' ];
+
+ve.dm.BlockImageNode.static.toDataElement = function ( domElements, converter ) {
+	var dataElement,
+		$figure = $( domElements[0] ),
+		$img = $figure.children( 'img' ).eq( 0 ),
+		$caption = $figure.children( 'figcaption' ).eq( 0 ),
+		attributes = {
+			src: $img.attr( 'src' )
+		},
+		width = $img.attr( 'width' ),
+		height = $img.attr( 'height' ),
+		altText = $img.attr( 'alt' );
+
+	if ( altText !== undefined ) {
+		attributes.alt = altText;
+	}
+
+	attributes.width = width !== undefined && width !== '' ? Number( width ) : null;
+	attributes.height = height !== undefined && height !== '' ? Number( height ) : null;
+
+	dataElement = { type: this.name, attributes: attributes };
+
+	if ( $caption.length === 0 ) {
+		return [
+			dataElement,
+			{ type: 'imageCaption' },
+			{ type: 'imageCaption' },
+			{ type: '/' + this.name }
+		];
+	} else {
+		return [ dataElement ].
+			concat( converter.getDataFromDomClean( $caption[0], { type: 'imageCaption' } ) ).
+			concat( [ { type: '/' + this.name } ] );
+	}
+};
+
+// TODO: Consider using jQuery instead of pure JS.
+// TODO: At this moment node is not resizable but when it will be then adding defaultSize class
+// should be more conditional.
+ve.dm.BlockImageNode.static.toDomElements = function ( data, doc, converter ) {
+	var dataElement = data[0],
+		width = dataElement.attributes.width,
+		height = dataElement.attributes.height,
+		figure = doc.createElement( 'figure' ),
+		img = doc.createElement( 'img' ),
+		wrapper = doc.createElement( 'div' ),
+		captionData = data.slice( 1, -1 );
+
+	img.setAttribute( 'src', dataElement.attributes.src );
+	img.setAttribute( 'width', width );
+	img.setAttribute( 'height', height );
+	if ( dataElement.attributes.alt !== undefined ) {
+		img.setAttribute( 'alt', dataElement.attributes.alt );
+	}
+	figure.appendChild( img );
+
+	// If length of captionData is smaller or equal to 2 it means that there is no caption or that
+	// it is empty - in both cases we are going to skip appending <figcaption>.
+	if ( captionData.length > 2 ) {
+		converter.getDomSubtreeFromData( data.slice( 1, -1 ), wrapper );
+		while ( wrapper.firstChild ) {
+			figure.appendChild( wrapper.firstChild );
+		}
+	}
+	return [ figure ];
+};
+
+/* Methods */
+
+/**
+ * Get the caption node of the image.
+ *
+ * @method
+ * @returns {ve.dm.BlockImageCaptionNode|null} Caption node, if present
+ */
+ve.dm.BlockImageNode.prototype.getCaptionNode = function () {
+	var node = this.children[0];
+	return node instanceof ve.dm.BlockImageCaptionNode ? node : null;
+};
+
+/**
+ * @inheritdoc
+ */
+ve.dm.BlockImageNode.prototype.createScalable = function () {
+	return new ve.dm.Scalable( {
+		currentDimensions: {
+			width: this.getAttribute( 'width' ),
+			height: this.getAttribute( 'height' )
+		},
+		minDimensions: {
+			width: 1,
+			height: 1
+		}
+	} );
+};
+
+/* Registration */
+
+ve.dm.modelRegistry.register( ve.dm.BlockImageNode );