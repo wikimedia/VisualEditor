wikimedia/VisualEditor

commit 355be3edf4cef93e8f17c18ed0d13a99b84504aa

Prevent getOffset from return an offset inside a content node
Browsers will always find a way to place the cursor at a strange
offset, but we should take care to not return a DM offset where
insertions can't take place.

Bug: 66691
Change-Id: I16baa0c67325d859ffe756f8d0e190758f2b9190

modules/ve/ce/ve.ce.js
@@ -252,9 +252,9 @@ ve.ce.getOffset = function ( domNode, domOffset ) {
 
 	if ( $.contains( node, startNode ) ) {
 		// node is an ancestor of startNode
-		// Add 1 to take the opening into account
-		offset += view.getModel().isWrapped() ? 1 : 0;
-		if ( view.getModel().canContainContent() ) {
+		if ( !view.getModel().isContent() ) {
+			// Add 1 to take the opening into account
+			offset += view.getModel().isWrapped() ? 1 : 0;
 			offset += lengthSum;
 		}
 		// else: we're inside an alienated node: throw away all the text node lengths,

modules/ve/test/ce/ve.ce.test.js
@@ -37,8 +37,10 @@ QUnit.test( 'getOffset', function ( assert ) {
 			{
 				'msg': 'Empty paragraph',
 				'html': '<p></p>',
-				// CE HTML summary;
+				// CE HTML summary:
 				// <p><span [inlineSlug]>&#xFEFF;</span></p>
+				// Linmod:
+				// [<p>, </p>]
 				'expected': [
 					0,
 					1, 1, 1, 1, 1, 1,
@@ -48,6 +50,8 @@ QUnit.test( 'getOffset', function ( assert ) {
 			{
 				'msg': 'Annotations',
 				'html': '<p><i><b>Foo</b></i></p>',
+				// Linmod:
+				// [<p>, F, o, o, </p>]
 				'expected': [
 					0,
 					1, 1, 1, 1,
@@ -60,6 +64,8 @@ QUnit.test( 'getOffset', function ( assert ) {
 			{
 				'msg': 'Multiple siblings',
 				'html': '<p><b><i>Foo</i><s><u>Bar</u><span>Baz</span></s></b></p>',
+				// Linmod:
+				// [<p>, F, o, o, B, a, r, B, a, z, </p>]
 				'expected': [
 					0,
 					1, 1, 1, 1,
@@ -80,13 +86,14 @@ QUnit.test( 'getOffset', function ( assert ) {
 				'html': '<p>Foo<b><span rel="ve:Alien">Bar</span></b>Baz</p>',
 				// CE HTML summary;
 				// <p>Foo<b><span [focusableNode]><span [alien]>Bar</span></span></b>Baz</p>
+				// Linmod:
+				// [<p>, F, o, o, <alineinline>, </alineinline>, B, a, z, </p>]
 				'expected': [
 					0,
 					1, 1,
 					2,
 					3,
-					4, 4, 4,
-					5, 5, 5, 5, 5, 5, 5, 5,
+					4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
 					6, 6, 6,
 					7,
 					8,
@@ -103,6 +110,8 @@ QUnit.test( 'getOffset', function ( assert ) {
 				//  <p>Foo</p>
 				// </td></tr></tbody></table>
 				// <div [slugWrapper]><p [blockSlug]></p></div>
+				// Linmod:
+				// [<table>, <tbody>, <tr>, <td>, <p>, F, o, o, </p>, </td>, </tr>, </tbody>, </table>]
 				'expected': [
 					0, 0, 0, 0, 0, 0, 0, 0,
 					1,
@@ -127,12 +136,12 @@ QUnit.test( 'getOffset', function ( assert ) {
 				// <p><span [inlineSlug]>&#xFEFF;</span><span [focusableNode]><span [alien]>Foo</span></span>
 				// <span [inlineSlug]>&#xFEFF;</span><span [focusableNode]><span [alien]>Bar</span></span>
 				// <span [inlineSlug]>&#xFEFF;</span><br></br><span [inlineSlug]>&#xFEFF;</span></p>
+				// Linmod:
+				// [<p>, <alineinline>, </alineinline>, <alineinline>, </alineinline>, <break>, </break>, </p>]
 				'expected': [
 					0,
-					1, 1, 1, 1, 1, 1,
-					2, 2, 2, 2, 2, 2, 2, 2,
-					3, 3, 3, 3, 3, 3,
-					4, 4, 4, 4, 4, 4, 4, 4,
+					1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+					3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
 					5, 5, 5, 5, 5, 5,
 					6,
 					7, 7, 7, 7, 7, 7,

commit 72f9aff4f1d8c1170c1aab509bbd006aaad0a2cb

Followup 355be3ed: fix getOffset() behavior inside block aliens
If you gave getOffset() a DOM offset inside a block alien, it
would correctly return the DM offset inside of that block alien.
This was broken in 355be3ed which caused getOffset() to count
text node characters inside of block aliens, which led to some
very strange results.

Fix this by making the lengthSum addition conditional on
canContainContent again, as it was before 355be3ed. Kept the
isWrapped addition conditional on !isContent, that part of
355be3ed was correct.

This was causing bugs in cases where the user was able to get
the cursor inside of a block transclusion, which IE allows.

Change-Id: I5cfb1df45ed9ff1497cb179b0f5c2f442cf1e106

src/ce/ve.ce.js
@@ -261,6 +261,8 @@ ve.ce.getOffset = function ( domNode, domOffset ) {
 		if ( !view.getModel().isContent() ) {
 			// Add 1 to take the opening into account
 			offset += view.getModel().isWrapped() ? 1 : 0;
+		}
+		if ( view.getModel().canContainContent() ) {
 			offset += lengthSum;
 		}
 		// else: we're inside an alienated node: throw away all the text node lengths,

tests/ce/ve.ce.test.js
@@ -130,6 +130,27 @@ QUnit.test( 'getOffset', function ( assert ) {
 				]
 			},
 			{
+				msg: 'Block alien',
+				html: '<p>Foo</p><div rel="ve:Alien">Bar</div><p>Baz</p>',
+				// Linmod:
+				// [<p>, F, o, o, </p>, <alienBlock>, </alienBlock>, <p>, B, a, z, </p>]
+				expected: [
+					0,
+					1, 1,
+					2,
+					3,
+					4, 4,
+					5,
+					6, 6, 6, 6, 6, 6,
+					7,
+					8, 8,
+					9,
+					10,
+					11, 11,
+					12
+				]
+			},
+			{
 				msg: 'Table with block slugs',
 				html: '<table><tr><td>Foo</td></tr></table>',
 				// CE HTML summary;