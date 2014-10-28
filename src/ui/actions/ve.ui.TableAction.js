barionleg/visualegitor

Merge cells tool
Change-Id: If0ee115468c13412c52e5dba122105cdaca28e3e

src/ui/actions/ve.ui.TableAction.js
@@ -32,7 +32,7 @@ ve.ui.TableAction.static.name = 'table';
  * @static
  * @property
  */
-ve.ui.TableAction.static.methods = [ 'create', 'insert', 'delete', 'toggleCellHeader' ];
+ve.ui.TableAction.static.methods = [ 'create', 'insert', 'delete', 'toggleCellHeader', 'mergeCells' ];
 
 /* Methods */
 
@@ -174,6 +174,56 @@ ve.ui.TableAction.prototype.toggleCellHeader = function () {
 				surfaceModel.getDocument(), ranges[i].start, { style: style }
 			)
 		);
+	}
+	surfaceModel.change( txs );
+};
+
+/**
+ * Merge multiple cells into one, or split a merged cell.
+ */
+ve.ui.TableAction.prototype.mergeCells = function () {
+	var i, cells,
+		txs = [],
+		surfaceModel = this.surface.getModel(),
+		selection = surfaceModel.getSelection();
+
+	if ( !( selection instanceof ve.dm.TableSelection ) ) {
+		return;
+	}
+
+	if ( selection.isSingleCell() ) {
+		// Split
+		cells = selection.getMatrixCells( true );
+		txs.push(
+			ve.dm.Transaction.newFromAttributeChanges(
+				surfaceModel.getDocument(), cells[0].node.getOuterRange().start,
+				{ colspan: 1, rowspan: 1 }
+			)
+		);
+		for ( i = cells.length - 1; i >= 1; i-- ) {
+			txs.push(
+				this.replacePlaceholder( selection.getTableNode().getMatrix(), cells[i] )
+			);
+		}
+	} else {
+		// Merge
+		cells = selection.getMatrixCells();
+		txs.push(
+			ve.dm.Transaction.newFromAttributeChanges(
+				surfaceModel.getDocument(), cells[0].node.getOuterRange().start,
+				{
+					colspan: 1 + selection.endCol - selection.startCol,
+					rowspan: 1 + selection.endRow - selection.startRow
+				}
+			)
+		);
+		for ( i = cells.length - 1; i >= 1; i-- ) {
+			txs.push(
+				ve.dm.Transaction.newFromRemoval(
+					surfaceModel.getDocument(), cells[i].node.getOuterRange()
+				)
+			);
+		}
 	}
 	surfaceModel.change( txs );
 };

wikimedia/VisualEditor

src/ui/actions/ve.ui.TableAction.js
@@ -32,7 +32,7 @@ ve.ui.TableAction.static.name = 'table';
  * @static
  * @property
  */
-ve.ui.TableAction.static.methods = [ 'create', 'insert', 'delete', 'changeCellStyle' ];
+ve.ui.TableAction.static.methods = [ 'create', 'insert', 'delete', 'changeCellStyle', 'mergeCells' ];
 
 /* Methods */
 
@@ -165,6 +165,56 @@ ve.ui.TableAction.prototype.changeCellStyle = function ( style ) {
 				surfaceModel.getDocument(), ranges[i].start, { style: style }
 			)
 		);
+	}
+	surfaceModel.change( txs );
+};
+
+/**
+ * Merge multiple cells into one, or split a merged cell.
+ */
+ve.ui.TableAction.prototype.mergeCells = function () {
+	var i, cells,
+		txs = [],
+		surfaceModel = this.surface.getModel(),
+		selection = surfaceModel.getSelection();
+
+	if ( !( selection instanceof ve.dm.TableSelection ) ) {
+		return;
+	}
+
+	if ( selection.isSingleCell() ) {
+		// Split
+		cells = selection.getMatrixCells( true );
+		txs.push(
+			ve.dm.Transaction.newFromAttributeChanges(
+				surfaceModel.getDocument(), cells[0].node.getOuterRange().start,
+				{ colspan: 1, rowspan: 1 }
+			)
+		);
+		for ( i = cells.length - 1; i >= 1; i-- ) {
+			txs.push(
+				this.replacePlaceholder( selection.getTableNode().getMatrix(), cells[i] )
+			);
+		}
+	} else {
+		// Merge
+		cells = selection.getMatrixCells();
+		txs.push(
+			ve.dm.Transaction.newFromAttributeChanges(
+				surfaceModel.getDocument(), cells[0].node.getOuterRange().start,
+				{
+					colspan: 1 + selection.endCol - selection.startCol,
+					rowspan: 1 + selection.endRow - selection.startRow
+				}
+			)
+		);
+		for ( i = cells.length - 1; i >= 1; i-- ) {
+			txs.push(
+				ve.dm.Transaction.newFromRemoval(
+					surfaceModel.getDocument(), cells[i].node.getOuterRange()
+				)
+			);
+		}
 	}
 	surfaceModel.change( txs );
 };