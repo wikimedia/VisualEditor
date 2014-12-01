barionleg/visualegitor

Find and replace
Change-Id: I8dc406ffe8455dedf35eef02a1909de2d79adeb0

src/ui/ve.ui.CommandRegistry.js
@@ -129,6 +129,11 @@ ve.ui.commandRegistry.register(
 );
 ve.ui.commandRegistry.register(
 	new ve.ui.Command(
+		'findAndReplace', 'window', 'open', { args: ['findAndReplace'] }
+	)
+);
+ve.ui.commandRegistry.register(
+	new ve.ui.Command(
 		'code', 'annotation', 'toggle',
 		{ args: ['textStyle/code'], supportedSelections: ['linear', 'table'] }
 	)

src/ui/ve.ui.Surface.js
@@ -53,6 +53,12 @@ ve.ui.Surface = function VeUiSurface( dataOrDoc, config ) {
 	this.filibuster = null;
 
 	this.toolbarHeight = 0;
+	this.toolbarWindows = new ve.ui.WindowManager( {
+		$: this.$,
+		factory: ve.ui.windowFactory,
+		modal: false,
+		isolate: true
+	} );
 
 	// Initialization
 	this.$menus.append( this.context.$element );

src/ui/ve.ui.Toolbar.js
@@ -50,6 +50,10 @@ ve.ui.Toolbar = function VeUiToolbar( surface, options ) {
 	// Events
 	this.surface.getModel().connect( this, { contextChange: 'onContextChange' } );
 	this.surface.connect( this, { addCommand: 'onSurfaceAddCommand' } );
+	this.surface.toolbarWindows.connect( this, {
+		opening: 'onToolbarWindowOpeningOrClosing',
+		closing: 'onToolbarWindowOpeningOrClosing'
+	} );
 };
 
 /* Inheritance */
@@ -99,6 +103,27 @@ ve.ui.Toolbar.prototype.onWindowResize = function () {
 			right: this.elementOffset.right
 		} );
 	}
+};
+
+/**
+ * Handle windows opening or closing in the toolbar window manager.
+ *
+ * @param {OO.ui.Window} win
+ * @param {jQuery.Promise} openingOrClosing
+ * @param {Object} data
+ */
+ve.ui.Toolbar.prototype.onToolbarWindowOpeningOrClosing = function ( win, openingOrClosing ) {
+	var toolbar = this;
+	openingOrClosing.then( function () {
+		// Wait for window transition
+		setTimeout( function () {
+			if ( toolbar.floating ) {
+				// Re-calculate height
+				toolbar.unfloat();
+				toolbar.float();
+			}
+		}, 250 );
+	} );
 };
 
 /**

wikimedia/VisualEditor

Bug: T50368

src/ui/ve.ui.CommandRegistry.js
@@ -141,6 +141,11 @@ ve.ui.commandRegistry.register(
 );
 ve.ui.commandRegistry.register(
 	new ve.ui.Command(
+		'findAndReplace', 'window', 'toggle', { args: ['findAndReplace'] }
+	)
+);
+ve.ui.commandRegistry.register(
+	new ve.ui.Command(
 		'code', 'annotation', 'toggle',
 		{ args: ['textStyle/code'], supportedSelections: ['linear', 'table'] }
 	)

src/ui/ve.ui.Surface.js
@@ -57,6 +57,12 @@ ve.ui.Surface = function VeUiSurface( dataOrDoc, config ) {
 	this.filibuster = null;
 
 	this.toolbarHeight = 0;
+	this.toolbarDialogs = new ve.ui.ToolbarDialogWindowManager( {
+		$: this.$,
+		factory: ve.ui.windowFactory,
+		modal: false,
+		isolate: true
+	} );
 
 	// Initialization
 	this.setupCommands( config.excludeCommands );

src/ui/ve.ui.Toolbar.js
@@ -49,6 +49,10 @@ ve.ui.Toolbar = function VeUiToolbar( surface, options ) {
 		.addClass( 've-ui-dir-block-' + this.contextDirection.block );
 	// Events
 	this.surface.getModel().connect( this, { contextChange: 'onContextChange' } );
+	this.surface.toolbarDialogs.connect( this, {
+		opening: 'onToolbarWindowOpeningOrClosing',
+		closing: 'onToolbarWindowOpeningOrClosing'
+	} );
 };
 
 /* Inheritance */
@@ -116,6 +120,27 @@ ve.ui.Toolbar.prototype.onWindowResize = function () {
 			right: this.elementOffset.right
 		} );
 	}
+};
+
+/**
+ * Handle windows opening or closing in the toolbar window manager.
+ *
+ * @param {OO.ui.Window} win
+ * @param {jQuery.Promise} openingOrClosing
+ * @param {Object} data
+ */
+ve.ui.Toolbar.prototype.onToolbarWindowOpeningOrClosing = function ( win, openingOrClosing ) {
+	var toolbar = this;
+	openingOrClosing.then( function () {
+		// Wait for window transition
+		setTimeout( function () {
+			if ( toolbar.floating ) {
+				// Re-calculate height
+				toolbar.unfloat();
+				toolbar.float();
+			}
+		}, 250 );
+	} );
 };
 
 /**