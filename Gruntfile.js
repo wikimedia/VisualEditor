bee267f3f67ff4edba16f679613877a071c20369
Esanders <esanders@wikimedia.org>
2014-12-18 19:50:20 +0000

    Revert "Follow-up 2075a46: Fix RTL image build by embedding after flipping"
    This fix actually overwrites the RTL stylesheet completely.
    Need a fix that embeds and RTL flips assets in one pass.
    
    This reverts commit 7ce4a63608ba841d509f095badbe1b90eee225d5.
    
    Change-Id: Ic8de624de95cd4159515a69a6c564176067216e2

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,6 +44,12 @@ module.exports = function ( grunt ) {
 		clean: {
 			dist: [ 'dist/*', 'test-coverage/*' ]
 		},
+		cssUrlEmbed: {
+			dist: {
+				dest: 'dist/visualEditor.css',
+				src: coreBuildFiles.styles
+			}
+		},
 		concat: {
 			options: {
 				banner: grunt.file.read( 'build/banner.txt' )
@@ -54,23 +60,13 @@ module.exports = function ( grunt ) {
 			},
 			css: {
 				dest: 'dist/visualEditor.css',
-				src: coreBuildFiles.styles
+				src: ['dist/visualEditor.css']
 			}
 		},
 		cssjanus: {
 			dist: {
 				src: 'dist/visualEditor.css',
 				dest: 'dist/visualEditor.rtl.css'
-			}
-		},
-		cssUrlEmbed: {
-			dist: {
-				dest: 'dist/visualEditor.css',
-				src: coreBuildFiles.styles
-			},
-			distRtl: {
-				dest: 'dist/visualEditor.rtl.css',
-				src: coreBuildFiles.styles
 			}
 		},
 		copy: {
@@ -209,7 +205,7 @@ module.exports = function ( grunt ) {
 		}
 	} );
 
-	grunt.registerTask( 'build', [ 'clean', 'concat', 'cssjanus', 'cssUrlEmbed', 'copy', 'buildloader' ] );
+	grunt.registerTask( 'build', [ 'clean', 'cssUrlEmbed', 'concat', 'cssjanus', 'copy', 'buildloader' ] );
 	grunt.registerTask( 'lint', [ 'jshint', 'jscs', 'csslint', 'banana' ] );
 	grunt.registerTask( 'unit', [ 'karma:main' ] );
 	grunt.registerTask( 'test', [ 'git-build', 'build', 'lint', 'unit' ] );
