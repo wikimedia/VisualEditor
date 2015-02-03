wikimedia/VisualEditor

commit a9266178180f40818e09e1850cbe2c673abe76d9

ce.GeneratedContentNode: Import DOM elements into target document early
Copy DOM elements into the target document before processing them,
rather than after. This means the processing is done not in the
input document, but in the view document. The latter is likely to be
the main document, and so we avoid running into
https://github.com/jquery/sizzle/issues/311 causing a lot of state
thrashing in Sizzle.

Bug: T87416
Change-Id: I3fbc83ebd5c07e4040b8f22a4eff9f0e1b1d7601

src/ce/nodes/ve.ce.GeneratedContentNode.js
@@ -107,11 +107,13 @@ ve.ce.GeneratedContentNode.prototype.getRenderedDomElements = function ( domElem
 		this.setAttribute( attr, this[attr] );
 	}
 
-	// Copy domElements so we can modify the elements
+	// Clone the elements into the target document
+	$rendering = $( ve.copyDomElements( domElements, doc ) );
+
 	// Filter out link and style tags for bug 50043
 	// Previously filtered out meta tags, but restore these as they
 	// can be made visible.
-	$rendering = this.$( domElements ).not( 'link, style' );
+	$rendering = $rendering.not( 'link, style' );
 	// Also remove link and style tags nested inside other tags
 	$rendering.find( 'link, style' ).remove();
 
@@ -134,8 +136,7 @@ ve.ce.GeneratedContentNode.prototype.getRenderedDomElements = function ( domElem
 			.each( resolveAttribute );
 	}
 
-	// Clone the elements into the target document
-	return ve.copyDomElements( $rendering.toArray(), doc );
+	return $rendering.toArray();
 };
 
 /**

commit cd3e3939fd5af08814d0f261b557e96f1437386e

Followup a92661781: fix attribute resolution
a92661781 broke resolution of href and src attributes in
ce.GeneratedContentNode. Those attributes need to be resolved
in the original document, but a92661781 made them be
resolved in the view document by accident.

Change-Id: Ia09f00e3f10c19ec38b89ea1e72958b20094fc72

src/ce/nodes/ve.ce.GeneratedContentNode.js
@@ -104,7 +104,10 @@ ve.ce.GeneratedContentNode.prototype.getRenderedDomElements = function ( domElem
 	 * @private
 	 */
 	function resolveAttribute() {
-		this.setAttribute( attr, this[attr] );
+		var origDoc = domElements[0].ownerDocument,
+			nodeInOrigDoc = origDoc.createElement( this.nodeName );
+		nodeInOrigDoc.setAttribute( attr, this.getAttribute( attr ) );
+		this.setAttribute( attr, nodeInOrigDoc[attr] );
 	}
 
 	// Clone the elements into the target document