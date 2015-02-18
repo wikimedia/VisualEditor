barionleg/visualegitor

Replace ve.dm.Model#matchesAttributeSpec
Attribute matcher specifications can take any form from the list below:

* Object with an array 'whitelist' property, containing any mix of booleans,
  regular expressions, and strings.
* Object with an array 'blacklist' property (same composition as above).
* Object with both 'whitelist' and 'blacklist' properties (ditto).
* Array (ditto).
* Regular expression.
* Boolean.

This flexibility is purchased at the cost of a lot of branching in a very hot
code path (ve.dm.Model#matchesAttributeSpec gets called for every property of
every element).

This patch allows attribute specifications to be either a boolean value or a
function which takes an attribute name and returns a boolean value. This allows
for dynamic and complex specifications without incurring costs for the common
cases.

Change-Id: Ia0e0fe4fc2385dab67c816148cfc362a39413dac

src/dm/nodes/ve.dm.BlockImageNode.js
@@ -37,8 +37,8 @@ OO.mixinClass( ve.dm.BlockImageNode, ve.dm.AlignableNode );
 
 ve.dm.BlockImageNode.static.name = 'blockImage';
 
-ve.dm.BlockImageNode.static.storeHtmlAttributes = {
-	blacklist: [ 'src', 'width', 'height', 'href' ]
+ve.dm.BlockImageNode.static.storeHtmlAttributes = function ( attribute ) {
+	return ve.indexOf( attribute, [ 'src', 'width', 'height', 'href' ] ) === -1;
 };
 
 ve.dm.BlockImageNode.static.handlesOwnChildren = true;

src/dm/nodes/ve.dm.TableCellNode.js
@@ -39,8 +39,8 @@ ve.dm.TableCellNode.static.defaultAttributes = { style: 'data' };
 ve.dm.TableCellNode.static.matchTagNames = [ 'td', 'th' ];
 
 // Blacklisting 'colspan' and 'rowspan' as they are managed explicitly
-ve.dm.TableCellNode.static.storeHtmlAttributes = {
-	blacklist: ['colspan', 'rowspan']
+ve.dm.TableCellNode.static.storeHtmlAttributes = function ( attribute ) {
+	return attribute !== 'colspan' && attribute !== 'rowspan';
 };
 
 /* Static Methods */

src/dm/ve.dm.Converter.js
@@ -143,21 +143,21 @@ ve.dm.Converter.openAndCloseAnnotations = function ( currentSet, targetSet, open
  * @param {Object[]} [attributeList] Existing attribute list to populate; used for recursion
  * @returns {Object[]|undefined} Attribute list, or undefined if empty
  */
-ve.dm.Converter.buildHtmlAttributeList = function ( domElements, spec, deep, attributeList ) {
+ve.dm.Converter.buildHtmlAttributeList = function ( domElements, filter, deep, attributeList ) {
 	var i, ilen, j, jlen, domAttributes, childList, attrName,
 		empty = true;
 	attributeList = attributeList || [];
 	for ( i = 0, ilen = domElements.length; i < ilen; i++ ) {
+		attributeList.push( { values: {}, computed: {} } );
+		if ( !filter ) {
+			continue;
+		}
 		domAttributes = domElements[i].attributes || [];
-		attributeList[i] = { values: {} };
 		for ( j = 0, jlen = domAttributes.length; j < jlen; j++ ) {
 			attrName = domAttributes[j].name;
-			if ( ve.dm.Model.matchesAttributeSpec( attrName, spec ) ) {
+			if ( filter === true || filter( attrName ) ) {
 				attributeList[i].values[attrName] = domAttributes[j].value;
 				if ( ve.indexOf( attrName, this.computedAttributes ) !== -1 ) {
-					if ( !attributeList[i].computed ) {
-						attributeList[i].computed = {};
-					}
 					attributeList[i].computed[attrName] = domElements[i][attrName];
 				}
 				empty = false;
@@ -169,7 +169,7 @@ ve.dm.Converter.buildHtmlAttributeList = function ( domElements, spec, deep, att
 				// Use .children rather than .childNodes so we don't mess around with things that
 				// can't have attributes anyway. Unfortunately, non-element nodes have .children
 				// set to undefined so we have to coerce it to an array in that case.
-				domElements[i].children || [], spec, deep, attributeList[i].children
+				domElements[i].children || [], filter, deep, attributeList[i].children
 			);
 			if ( childList ) {
 				empty = false;
@@ -190,16 +190,16 @@ ve.dm.Converter.buildHtmlAttributeList = function ( domElements, spec, deep, att
  * @static
  * @param {Object[]} attributeList Attribute list, see buildHtmlAttributeList()
  * @param {HTMLElement[]} domElements Array of DOM elements to render onto
- * @param {boolean|string|RegExp|Array|Object} [spec=true] Attribute specification, see ve.dm.Model
+ * @param {boolean|Function} [filter=true] Attribute filter
  * @param {boolean} [computed=false] If true, use the computed values of attributes where available
  * @param {boolean} [overwrite=false] If true, overwrite attributes that are already set
  */
-ve.dm.Converter.renderHtmlAttributeList = function ( attributeList, domElements, spec, computed, overwrite ) {
+ve.dm.Converter.renderHtmlAttributeList = function ( attributeList, domElements, filter, computed, overwrite ) {
 	var i, ilen, key, values, value;
-	if ( spec === undefined ) {
-		spec = true;
+	if ( filter === undefined ) {
+		filter = true;
 	}
-	if ( spec === false ) {
+	if ( filter === false ) {
 		return;
 	}
 	for ( i = 0, ilen = attributeList.length; i < ilen; i++ ) {
@@ -208,7 +208,7 @@ ve.dm.Converter.renderHtmlAttributeList = function ( attributeList, domElements,
 		}
 		values = attributeList[i].values;
 		for ( key in values ) {
-			if ( ve.dm.Model.matchesAttributeSpec( key, spec ) ) {
+			if ( filter === true || filter( key ) ) {
 				value = computed && attributeList[i].computed && attributeList[i].computed[key] || values[key];
 				if ( value === undefined ) {
 					domElements[i].removeAttribute( key );
@@ -219,7 +219,7 @@ ve.dm.Converter.renderHtmlAttributeList = function ( attributeList, domElements,
 		}
 		if ( attributeList[i].children ) {
 			ve.dm.Converter.renderHtmlAttributeList(
-				attributeList[i].children, domElements[i].children, spec, computed, overwrite
+				attributeList[i].children, domElements[i].children, filter, computed, overwrite
 			);
 		}
 	}
@@ -783,7 +783,7 @@ ve.dm.Converter.prototype.getDataFromDomSubtree = function ( domElement, wrapper
 						!this.nodeFactory.doesNodeHandleOwnChildren( childDataElements[0].type )
 					) {
 						htmlAttributes = ve.dm.Converter.buildHtmlAttributeList(
-							childNodes, modelClass.static.storeHtmlAttributes
+							childNodes, modelClass.static.storeHtmlAttributes, false
 						);
 						if ( htmlAttributes ) {
 							childDataElements[0].htmlAttributes = htmlAttributes;

wikimedia/VisualEditor

src/dm/nodes/ve.dm.BlockImageNode.js
@@ -37,8 +37,8 @@ OO.mixinClass( ve.dm.BlockImageNode, ve.dm.AlignableNode );
 
 ve.dm.BlockImageNode.static.name = 'blockImage';
 
-ve.dm.BlockImageNode.static.preserveHtmlAttributes = {
-	blacklist: [ 'src', 'width', 'height', 'href' ]
+ve.dm.BlockImageNode.static.preserveHtmlAttributes = function ( attribute ) {
+	return ve.indexOf( attribute, [ 'src', 'width', 'height', 'href' ] ) === -1;
 };
 
 ve.dm.BlockImageNode.static.handlesOwnChildren = true;

src/dm/nodes/ve.dm.TableCellNode.js
@@ -39,8 +39,8 @@ ve.dm.TableCellNode.static.defaultAttributes = { style: 'data' };
 ve.dm.TableCellNode.static.matchTagNames = [ 'td', 'th' ];
 
 // Blacklisting 'colspan' and 'rowspan' as they are managed explicitly
-ve.dm.TableCellNode.static.preserveHtmlAttributes = {
-	blacklist: ['colspan', 'rowspan']
+ve.dm.TableCellNode.static.preserveHtmlAttributes = function ( attribute ) {
+	return attribute !== 'colspan' && attribute !== 'rowspan';
 };
 
 /* Static Methods */

src/dm/ve.dm.Converter.js
@@ -134,16 +134,16 @@ ve.dm.Converter.openAndCloseAnnotations = function ( currentSet, targetSet, open
  * @static
  * @param {HTMLElement[]} originalDomElements Array of DOM elements to render from
  * @param {HTMLElement[]} targetDomElements Array of DOM elements to render onto
- * @param {boolean|string|RegExp|Array|Object} [spec=true] Attribute specification, see ve.dm.Model
+ * @param {boolean|Function} [filter=true] Attribute filter
  * @param {boolean} [computed=false] If true, use the computed values of attributes where available
  * @param {boolean} [deep=false] Recurse into child nodes
  */
-ve.dm.Converter.renderHtmlAttributeList = function ( originalDomElements, targetDomElements, spec, computed, deep ) {
+ve.dm.Converter.renderHtmlAttributeList = function ( originalDomElements, targetDomElements, filter, computed, deep ) {
 	var i, ilen, j, jlen, attrs, value;
-	if ( spec === undefined ) {
-		spec = true;
+	if ( filter === undefined ) {
+		filter = true;
 	}
-	if ( spec === false ) {
+	if ( filter === false ) {
 		return;
 	}
 
@@ -158,7 +158,7 @@ ve.dm.Converter.renderHtmlAttributeList = function ( originalDomElements, target
 		for ( j = 0, jlen = attrs.length; j < jlen; j++ ) {
 			if (
 				!targetDomElements[i].hasAttribute( attrs[j].name ) &&
-				( spec === true || ve.dm.Model.matchesAttributeSpec( attrs[j].name, spec ) )
+				( filter === true || filter( attrs[j].name ) )
 			) {
 				if ( computed && ve.dm.Converter.computedAttributes.indexOf( attrs[j].name ) !== -1 ) {
 					value = originalDomElements[i][attrs[j].name];
@@ -168,7 +168,7 @@ ve.dm.Converter.renderHtmlAttributeList = function ( originalDomElements, target
 				targetDomElements[i].setAttribute( attrs[j].name, value );
 			}
 
-			if ( spec === true || ve.dm.Model.matchesAttributeSpec( attrs[j].name, spec ) ) {
+			if ( filter === true || filter( attrs[j].name ) ) {
 				value = computed && ve.dm.Converter.computedAttributes.indexOf( attrs[j].name ) !== -1 ?
 					originalDomElements[i][attrs[j].name] :
 					attrs[j].value;
@@ -180,7 +180,7 @@ ve.dm.Converter.renderHtmlAttributeList = function ( originalDomElements, target
 			ve.dm.Converter.renderHtmlAttributeList(
 				originalDomElements[i].children,
 				targetDomElements[i].children,
-				spec,
+				filter,
 				computed,
 				true
 			);