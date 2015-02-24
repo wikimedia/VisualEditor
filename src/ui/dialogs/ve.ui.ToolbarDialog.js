a0d9caa9d79062b24f888bd2aea62233c2c4f37c
Ed Sanders <esanders@wikimedia.org>
2015-02-24 17:25:43 +0000

    Make toolbar dialogs disableable
    Change-Id: If61a0fd9aab7dbead9f1e0f6ab67f75da606d047

--- src/ui/dialogs/ve.ui.ToolbarDialog.js
+++ src/ui/dialogs/ve.ui.ToolbarDialog.js
@@ -19,6 +19,10 @@ ve.ui.ToolbarDialog = function VeUiToolbarDialog( config ) {
 	// Parent constructor
 	ve.ui.ToolbarDialog.super.call( this, config );
 
+	// Properties
+	this.disabled = false;
+	this.$shield = this.$( '<div>' ).addClass( 've-ui-toolbarDialog-shield' );
+
 	// Pre-initialization
 	// This class needs to exist before setup to constrain the height
 	// of the dialog when it first loads.
@@ -44,8 +48,25 @@ ve.ui.ToolbarDialog.prototype.initialize = function () {
 	// Parent method
 	ve.ui.ToolbarDialog.super.prototype.initialize.call( this );
 
+	this.$body.append( this.$shield );
 	this.$content.addClass( 've-ui-toolbarDialog-content' );
 	if ( this.constructor.static.padded ) {
 		this.$element.addClass( 've-ui-toolbarDialog-padded' );
+	}
+};
+
+/**
+ * Set the disabled state of the toolbar dialog
+ *
+ * @param {boolean} disabled Disable the dialog
+ */
+ve.ui.ToolbarDialog.prototype.setDisabled = function ( disabled ) {
+	this.$content.addClass( 've-ui-toolbarDialog-content' );
+	if ( disabled !== this.disabled ) {
+		this.disabled = disabled;
+		this.$body
+			// Make sure sheild is last child
+			.append( this.$shield )
+			.toggleClass( 've-ui-toolbarDialog-disabled', this.disabled );
 	}
 };
