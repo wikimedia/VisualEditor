56c59371fb48f9bea86de615ae66c393b89800ab
Ed Sanders <esanders@wikimedia.org>
2015-02-27 14:24:31 +0000

    Provide isAlignable checks for alignable nodes
    Change-Id: I45afff63c5aa577972e96379c1e48ef53a120823

--- src/dm/ve.dm.AlignableNode.js
+++ src/dm/ve.dm.AlignableNode.js
@@ -24,6 +24,8 @@ OO.inheritClass( ve.dm.AlignableNode, ve.dm.ClassAttributeNode );
 
 /* Static properties */
 
+ve.dm.AlignableNode.static.isAlignable = true;
+
 ve.dm.AlignableNode.static.classAttributes = {
 	've-align-left': { align: 'left' },
 	've-align-right': { align: 'right' },
