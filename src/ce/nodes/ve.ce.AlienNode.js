5380b8c75b526ad3edb7da0779eaa9625844b95f
Roan Kattouw <roan.kattouw@gmail.com>
2015-03-25 14:07:47 -0700

    Follow-up 80a07cf5cb: don't use DM DOM nodes directly in CE DOM
    Not only are they in the wrong document, this causes wikitext like
    [[File:Foo.png|thumb|class=ve-ce-leafNode ve-ce-focusableNode]]
    to be saved.
    
    Change-Id: Ic6e45a5b3028fb0e85dbc95262c08a3cd8a3d9c4

--- src/ce/nodes/ve.ce.AlienNode.js
+++ src/ce/nodes/ve.ce.AlienNode.js
@@ -21,7 +21,7 @@ ve.ce.AlienNode = function VeCeAlienNode() {
 	ve.ce.AlienNode.super.apply( this, arguments );
 
 	// DOM changes
-	this.$element = $( this.model.getOriginalDomElements() );
+	this.$element = $( ve.copyDomElements( this.model.getOriginalDomElements(), document ) );
 
 	// Mixin constructors
 	ve.ce.FocusableNode.call( this, this.$element, {
