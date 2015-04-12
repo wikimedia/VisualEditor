12ef3103d84aee3d4b56cc0ce59e8995df449f29
Ed Sanders <esanders@wikimedia.org>
2015-04-12 17:38:22 +0100

    Move @example demo to ve.init.sa.Platform
    Platform initialize is required to load i18n messages.
    
    Change-Id: Iaf52b7ca8e39a609853f6c99bc40f2fc15b17021

--- src/init/sa/ve.init.sa.Target.js
+++ src/init/sa/ve.init.sa.Target.js
@@ -7,15 +7,6 @@
 /**
  * Initialization Standalone target.
  *
- *     @example
- *     var target = new ve.init.sa.Target();
- *     target.addSurface(
- *         ve.dm.converter.getModelFromDom(
- *             ve.createDocumentFromHtml( '<p>Hello, World!</p>' )
- *         )
- *     );
- *     $( 'body' ).append( target.$element );
- *
  * @class
  * @extends ve.init.Target
  *
