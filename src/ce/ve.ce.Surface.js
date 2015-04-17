da6ae78c99683cffe834efeb166b9b0d2d3acb2d
Ed Sanders <esanders@wikimedia.org>
2015-04-17 10:11:03 +0100

    ve.ce.Surface: Restrict native selection rangeCount to 1
    FF lets you create multi range selections which we don't support,
    so trim excess ranges if they are created so the view matches
    the model.
    
    Bug: T96310
    Change-Id: I55c795b89974385ac67810cdd2b16f15fc538ba6

--- src/ce/ve.ce.Surface.js
+++ src/ce/ve.ce.Surface.js
@@ -2539,6 +2539,16 @@ ve.ce.Surface.prototype.onSurfaceObserverRangeChange = function ( oldRange, newR
 		this.decRenderLock();
 	}
 	this.checkUnicorns( false );
+	// Firefox lets you create multiple selections within a single paragraph
+	// which our model doesn't support, so detect and prevent these. This cannot
+	// create problems with IME candidates: only an explicit mouse action can
+	// create a multiple selection, and we remove it immediately, so there can
+	// never be a multiple selection while the user is typing text; therefore the
+	// selection change will never commit IME candidates prematurely."
+	while ( this.nativeSelection.rangeCount > 1 ) {
+		// The current range is the last range, so remove ranges from the front
+		this.nativeSelection.removeRange( this.nativeSelection.getRangeAt( 0 ) );
+	}
 };
 
 /**
