f81eefa3d5ce28ae39331c2a214560b3fd6f9624
Ed Sanders <esanders@wikimedia.org>
2015-05-08 09:08:48 +0100

    [BREAKING CHANGE] Rename LinkInspector to LinkAnnotationInspector
    Matches naming convention in MW. 
    
    Bonus: Remove CSS file containing one unused rule.
    
    Change-Id: Ied550f050b87a28fac193e54da41bc18ec1b3d71

--- src/ui/inspectors/ve.ui.LinkAnnotationInspector.js
+++ src/ui/inspectors/ve.ui.LinkAnnotationInspector.js
@@ -1,5 +1,5 @@
 /*!
- * VisualEditor UserInterface LinkInspector class.
+ * VisualEditor UserInterface LinkAnnotationInspector class.
  *
  * @copyright 2011-2015 VisualEditor Team and others; see http://ve.mit-license.org
  */
@@ -13,24 +13,24 @@
  * @constructor
  * @param {Object} [config] Configuration options
  */
-ve.ui.LinkInspector = function VeUiLinkInspector( config ) {
+ve.ui.LinkAnnotationInspector = function VeUiLinkAnnotationInspector( config ) {
 	// Parent constructor
 	ve.ui.AnnotationInspector.call( this, config );
 };
 
 /* Inheritance */
 
-OO.inheritClass( ve.ui.LinkInspector, ve.ui.AnnotationInspector );
+OO.inheritClass( ve.ui.LinkAnnotationInspector, ve.ui.AnnotationInspector );
 
 /* Static properties */
 
-ve.ui.LinkInspector.static.name = 'link';
+ve.ui.LinkAnnotationInspector.static.name = 'link';
 
-ve.ui.LinkInspector.static.title = OO.ui.deferMsg( 'visualeditor-linkinspector-title' );
+ve.ui.LinkAnnotationInspector.static.title = OO.ui.deferMsg( 'visualeditor-linkinspector-title' );
 
-ve.ui.LinkInspector.static.modelClasses = [ ve.dm.LinkAnnotation ];
+ve.ui.LinkAnnotationInspector.static.modelClasses = [ ve.dm.LinkAnnotation ];
 
-ve.ui.LinkInspector.static.actions = ve.ui.LinkInspector.super.static.actions.concat( [
+ve.ui.LinkAnnotationInspector.static.actions = ve.ui.LinkAnnotationInspector.super.static.actions.concat( [
 	{
 		action: 'open',
 		label: OO.ui.deferMsg( 'visualeditor-linkinspector-open' ),
@@ -45,14 +45,14 @@ ve.ui.LinkInspector.static.actions = ve.ui.LinkInspector.super.static.actions.co
  *
  * @param {ve.dm.LinkAnnotation} annotation New link annotation value
  */
-ve.ui.LinkInspector.prototype.onAnnotationInputChange = function () {
+ve.ui.LinkAnnotationInspector.prototype.onAnnotationInputChange = function () {
 	this.updateActions();
 };
 
 /**
  * Update the actions based on the annotation state
  */
-ve.ui.LinkInspector.prototype.updateActions = function () {
+ve.ui.LinkAnnotationInspector.prototype.updateActions = function () {
 	var annotation = this.annotationInput.getAnnotation(),
 		href = this.annotationInput.getHref();
 
@@ -68,28 +68,28 @@ ve.ui.LinkInspector.prototype.updateActions = function () {
 /**
  * @inheritdoc
  */
-ve.ui.LinkInspector.prototype.shouldRemoveAnnotation = function () {
+ve.ui.LinkAnnotationInspector.prototype.shouldRemoveAnnotation = function () {
 	return !this.annotationInput.getAnnotation();
 };
 
 /**
  * @inheritdoc
  */
-ve.ui.LinkInspector.prototype.getInsertionText = function () {
+ve.ui.LinkAnnotationInspector.prototype.getInsertionText = function () {
 	return this.annotationInput.getHref();
 };
 
 /**
  * @inheritdoc
  */
-ve.ui.LinkInspector.prototype.getAnnotation = function () {
+ve.ui.LinkAnnotationInspector.prototype.getAnnotation = function () {
 	return this.annotationInput.getAnnotation();
 };
 
 /**
  * @inheritdoc
  */
-ve.ui.LinkInspector.prototype.getAnnotationFromFragment = function ( fragment ) {
+ve.ui.LinkAnnotationInspector.prototype.getAnnotationFromFragment = function ( fragment ) {
 	var text = fragment.getText();
 
 	return text ? new ve.dm.LinkAnnotation( {
@@ -101,9 +101,9 @@ ve.ui.LinkInspector.prototype.getAnnotationFromFragment = function ( fragment ) 
 /**
  * @inheritdoc
  */
-ve.ui.LinkInspector.prototype.initialize = function () {
+ve.ui.LinkAnnotationInspector.prototype.initialize = function () {
 	// Parent method
-	ve.ui.LinkInspector.super.prototype.initialize.call( this );
+	ve.ui.LinkAnnotationInspector.super.prototype.initialize.call( this );
 
 	// Properties
 	this.annotationInput = this.createAnnotationInput();
@@ -120,15 +120,15 @@ ve.ui.LinkInspector.prototype.initialize = function () {
  *
  * @return {ve.ui.LinkAnnotationWidget} Link annotation widget
  */
-ve.ui.LinkInspector.prototype.createAnnotationInput = function () {
+ve.ui.LinkAnnotationInspector.prototype.createAnnotationInput = function () {
 	return new ve.ui.LinkAnnotationWidget();
 };
 
 /**
  * @inheritdoc
  */
-ve.ui.LinkInspector.prototype.getSetupProcess = function ( data ) {
-	return ve.ui.LinkInspector.super.prototype.getSetupProcess.call( this, data )
+ve.ui.LinkAnnotationInspector.prototype.getSetupProcess = function ( data ) {
+	return ve.ui.LinkAnnotationInspector.super.prototype.getSetupProcess.call( this, data )
 		.next( function () {
 			// Disable surface until animation is complete; will be reenabled in ready()
 			this.getFragment().getSurface().disable();
@@ -139,8 +139,8 @@ ve.ui.LinkInspector.prototype.getSetupProcess = function ( data ) {
 /**
  * @inheritdoc
  */
-ve.ui.LinkInspector.prototype.getReadyProcess = function ( data ) {
-	return ve.ui.LinkInspector.super.prototype.getReadyProcess.call( this, data )
+ve.ui.LinkAnnotationInspector.prototype.getReadyProcess = function ( data ) {
+	return ve.ui.LinkAnnotationInspector.super.prototype.getReadyProcess.call( this, data )
 		.next( function () {
 			this.annotationInput.text.focus().select();
 			this.getFragment().getSurface().enable();
@@ -156,8 +156,8 @@ ve.ui.LinkInspector.prototype.getReadyProcess = function ( data ) {
 /**
  * @inheritdoc
  */
-ve.ui.LinkInspector.prototype.getHoldProcess = function ( data ) {
-	return ve.ui.LinkInspector.super.prototype.getHoldProcess.call( this, data )
+ve.ui.LinkAnnotationInspector.prototype.getHoldProcess = function ( data ) {
+	return ve.ui.LinkAnnotationInspector.super.prototype.getHoldProcess.call( this, data )
 		.next( function () {
 			this.annotationInput.text.blur();
 		}, this );
@@ -166,8 +166,8 @@ ve.ui.LinkInspector.prototype.getHoldProcess = function ( data ) {
 /**
  * @inheritdoc
  */
-ve.ui.LinkInspector.prototype.getTeardownProcess = function ( data ) {
-	return ve.ui.LinkInspector.super.prototype.getTeardownProcess.call( this, data )
+ve.ui.LinkAnnotationInspector.prototype.getTeardownProcess = function ( data ) {
+	return ve.ui.LinkAnnotationInspector.super.prototype.getTeardownProcess.call( this, data )
 		.next( function () {
 			this.annotationInput.setAnnotation( null );
 		}, this );
@@ -175,4 +175,4 @@ ve.ui.LinkInspector.prototype.getTeardownProcess = function ( data ) {
 
 /* Registration */
 
-ve.ui.windowFactory.register( ve.ui.LinkInspector );
+ve.ui.windowFactory.register( ve.ui.LinkAnnotationInspector );
