barionleg/visualegitor

Separate SA target into Mobile and Desktop
Change-Id: I480ec0d1912d2a89662408f2814968de8eb3fc88

src/init/sa/ve.init.sa.DesktopTarget.js
@@ -1,0 +1,43 @@
+/*!
+ * VisualEditor Standalone Initialization Mobile Target class.
+ *
+ * @copyright 2011-2015 VisualEditor Team and others; see http://ve.mit-license.org
+ */
+
+/**
+ * Initialization standalone mobile target.
+ *
+ * @class
+ * @extends ve.init.sa.Target
+ *
+ * @constructor
+ * @param {Object} [config] Configuration options
+ * @cfg {Object} [toolbarConfig] Configuration options for the toolbar
+ */
+ve.init.sa.DesktopTarget = function VeInitSaTarget( config ) {
+	// Parent constructor
+	ve.init.sa.DesktopTarget.super.call( this, config );
+
+	this.$element.addClass( 've-init-sa-desktopTarget' );
+};
+
+/* Inheritance */
+
+OO.inheritClass( ve.init.sa.DesktopTarget, ve.init.sa.Target );
+
+/* Methods */
+
+/**
+ * @inheritdoc
+ */
+ve.init.sa.DesktopTarget.prototype.setupToolbar = function ( surface ) {
+	if ( !this.getToolbar().initialized ) {
+		this.actions = new ve.ui.TargetToolbar( this );
+		this.getToolbar().$actions.append( this.actions.$element );
+	}
+
+	this.actions.setup( this.constructor.static.actionGroups, surface );
+
+	// Parent method
+	ve.init.sa.DesktopTarget.super.prototype.setupToolbar.call( this, surface );
+};

src/init/sa/ve.init.sa.MobileTarget.js
@@ -1,0 +1,109 @@
+/*!
+ * VisualEditor Standalone Initialization Mobile Target class.
+ *
+ * @copyright 2011-2015 VisualEditor Team and others; see http://ve.mit-license.org
+ */
+
+/**
+ * Initialization standalone mobile target.
+ *
+ * @class
+ * @extends ve.init.sa.Target
+ *
+ * @constructor
+ * @param {Object} [config] Configuration options
+ * @cfg {Object} [toolbarConfig] Configuration options for the toolbar
+ */
+ve.init.sa.MobileTarget = function VeInitSaTarget( config ) {
+	// Parent constructor
+	ve.init.sa.MobileTarget.super.call( this, config );
+
+	this.$element.addClass( 've-init-sa-mobileTarget' );
+};
+
+/* Inheritance */
+
+OO.inheritClass( ve.init.sa.MobileTarget, ve.init.sa.Target );
+
+/* Static Properties */
+
+ve.init.sa.MobileTarget.static.toolbarGroups = [
+	// History
+	{
+		header: OO.ui.deferMsg( 'visualeditor-toolbar-history' ),
+		include: [ 'undo' ]
+	},
+	// Style
+	{
+		classes: [ 've-test-toolbar-style' ],
+		type: 'list',
+		icon: 'textStyle',
+		indicator: 'down',
+		title: OO.ui.deferMsg( 'visualeditor-toolbar-style-tooltip' ),
+		include: [ { group: 'textStyle' }, 'language', 'clear' ],
+		forceExpand: [ 'bold', 'italic', 'clear' ],
+		promote: [ 'bold', 'italic' ],
+		demote: [ 'strikethrough', 'code', 'underline', 'language', 'clear' ]
+	},
+	// Link
+	{
+		header: OO.ui.deferMsg( 'visualeditor-linkinspector-title' ),
+		include: [ 'link' ]
+	},
+	// Structure
+	{
+		header: OO.ui.deferMsg( 'visualeditor-toolbar-structure' ),
+		type: 'list',
+		icon: 'listBullet',
+		indicator: 'down',
+		include: [ { group: 'structure' } ],
+		demote: [ 'outdent', 'indent' ]
+	},
+	// Insert
+	{
+		header: OO.ui.deferMsg( 'visualeditor-toolbar-insert' ),
+		type: 'list',
+		icon: 'insert',
+		label: '',
+		title: OO.ui.deferMsg( 'visualeditor-toolbar-insert' ),
+		indicator: 'down',
+		include: '*'
+	},
+	// Table
+	{
+		header: OO.ui.deferMsg( 'visualeditor-toolbar-table' ),
+		type: 'list',
+		icon: 'table',
+		indicator: 'down',
+		include: [ { group: 'table' } ],
+		demote: [ 'deleteTable' ]
+	}
+];
+
+/* Methods */
+
+/**
+ * @inheritdoc
+ */
+ve.init.sa.Target.prototype.createSurface = function ( dmDoc, config ) {
+	config = ve.extendObject( {
+		includeCommands: this.constructor.static.includeCommands,
+		excludeCommands: OO.simpleArrayUnion(
+			this.constructor.static.excludeCommands,
+			this.constructor.static.documentCommands,
+			this.constructor.static.targetCommands
+		),
+		importRules: this.constructor.static.importRules
+	}, config );
+	return new ve.ui.MobileSurface( dmDoc, config );
+};
+
+/**
+ * @inheritdoc
+ */
+ve.init.sa.MobileTarget.prototype.setupToolbar = function ( surface ) {
+	// Parent method
+	ve.init.sa.MobileTarget.super.prototype.setupToolbar.call( this, surface );
+
+	this.getToolbar().$bar.append( surface.context.$element );
+};

wikimedia/VisualEditor

src/init/sa/ve.init.sa.DesktopTarget.js
@@ -1,0 +1,43 @@
+/*!
+ * VisualEditor Standalone Initialization Desktop Target class.
+ *
+ * @copyright 2011-2015 VisualEditor Team and others; see http://ve.mit-license.org
+ */
+
+/**
+ * Initialization standalone desktop target.
+ *
+ * @class
+ * @extends ve.init.sa.Target
+ *
+ * @constructor
+ * @param {Object} [config] Configuration options
+ * @cfg {Object} [toolbarConfig] Configuration options for the toolbar
+ */
+ve.init.sa.DesktopTarget = function VeInitSaDesktopTarget( config ) {
+	// Parent constructor
+	ve.init.sa.DesktopTarget.super.call( this, config );
+
+	this.$element.addClass( 've-init-sa-desktopTarget' );
+};
+
+/* Inheritance */
+
+OO.inheritClass( ve.init.sa.DesktopTarget, ve.init.sa.Target );
+
+/* Methods */
+
+/**
+ * @inheritdoc
+ */
+ve.init.sa.DesktopTarget.prototype.setupToolbar = function ( surface ) {
+	if ( !this.actions ) {
+		this.actions = new ve.ui.TargetToolbar( this );
+		this.getToolbar().$actions.append( this.actions.$element );
+	}
+
+	this.actions.setup( this.constructor.static.actionGroups, surface );
+
+	// Parent method
+	ve.init.sa.DesktopTarget.super.prototype.setupToolbar.call( this, surface );
+};

src/init/sa/ve.init.sa.MobileTarget.js
@@ -1,0 +1,109 @@
+/*!
+ * VisualEditor Standalone Initialization Mobile Target class.
+ *
+ * @copyright 2011-2015 VisualEditor Team and others; see http://ve.mit-license.org
+ */
+
+/**
+ * Initialization standalone mobile target.
+ *
+ * @class
+ * @extends ve.init.sa.Target
+ *
+ * @constructor
+ * @param {Object} [config] Configuration options
+ * @cfg {Object} [toolbarConfig] Configuration options for the toolbar
+ */
+ve.init.sa.MobileTarget = function VeInitSaMobileTarget( config ) {
+	// Parent constructor
+	ve.init.sa.MobileTarget.super.call( this, config );
+
+	this.$element.addClass( 've-init-sa-mobileTarget' );
+};
+
+/* Inheritance */
+
+OO.inheritClass( ve.init.sa.MobileTarget, ve.init.sa.Target );
+
+/* Static Properties */
+
+ve.init.sa.MobileTarget.static.toolbarGroups = [
+	// History
+	{
+		header: OO.ui.deferMsg( 'visualeditor-toolbar-history' ),
+		include: [ 'undo' ]
+	},
+	// Style
+	{
+		classes: [ 've-test-toolbar-style' ],
+		type: 'list',
+		icon: 'textStyle',
+		indicator: 'down',
+		title: OO.ui.deferMsg( 'visualeditor-toolbar-style-tooltip' ),
+		include: [ { group: 'textStyle' }, 'language', 'clear' ],
+		forceExpand: [ 'bold', 'italic', 'clear' ],
+		promote: [ 'bold', 'italic' ],
+		demote: [ 'strikethrough', 'code', 'underline', 'language', 'clear' ]
+	},
+	// Link
+	{
+		header: OO.ui.deferMsg( 'visualeditor-linkinspector-title' ),
+		include: [ 'link' ]
+	},
+	// Structure
+	{
+		header: OO.ui.deferMsg( 'visualeditor-toolbar-structure' ),
+		type: 'list',
+		icon: 'listBullet',
+		indicator: 'down',
+		include: [ { group: 'structure' } ],
+		demote: [ 'outdent', 'indent' ]
+	},
+	// Insert
+	{
+		header: OO.ui.deferMsg( 'visualeditor-toolbar-insert' ),
+		type: 'list',
+		icon: 'insert',
+		label: '',
+		title: OO.ui.deferMsg( 'visualeditor-toolbar-insert' ),
+		indicator: 'down',
+		include: '*'
+	},
+	// Table
+	{
+		header: OO.ui.deferMsg( 'visualeditor-toolbar-table' ),
+		type: 'list',
+		icon: 'table',
+		indicator: 'down',
+		include: [ { group: 'table' } ],
+		demote: [ 'deleteTable' ]
+	}
+];
+
+/* Methods */
+
+/**
+ * @inheritdoc
+ */
+ve.init.sa.MobileTarget.prototype.createSurface = function ( dmDoc, config ) {
+	config = ve.extendObject( {
+		includeCommands: this.constructor.static.includeCommands,
+		excludeCommands: OO.simpleArrayUnion(
+			this.constructor.static.excludeCommands,
+			this.constructor.static.documentCommands,
+			this.constructor.static.targetCommands
+		),
+		importRules: this.constructor.static.importRules
+	}, config );
+	return new ve.ui.MobileSurface( dmDoc, config );
+};
+
+/**
+ * @inheritdoc
+ */
+ve.init.sa.MobileTarget.prototype.setupToolbar = function ( surface ) {
+	// Parent method
+	ve.init.sa.MobileTarget.super.prototype.setupToolbar.call( this, surface );
+
+	this.getToolbar().$bar.append( surface.context.$element );
+};