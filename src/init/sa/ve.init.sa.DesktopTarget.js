6e5d335bffd2bfcbe5420030407f19c1aa6c301e
Roan Kattouw <roan.kattouw@gmail.com>
2015-09-07 11:11:32 -0700

    sa.DesktopTarget: Clean up handling of actions toolbar
    * Actually initialize and destroy actions toolbar
    * Don't pre-empt parent's getToolbar() call in setupToolbar();
      attach actions toolbar after parent call instead
    * Remove unused this.actions property from sa.Target
    
    Change-Id: Ib138cc7a4a3cc5bef92b7762d1228acedcac6eb8

--- src/init/sa/ve.init.sa.DesktopTarget.js
+++ src/init/sa/ve.init.sa.DesktopTarget.js
@@ -33,11 +33,26 @@ OO.inheritClass( ve.init.sa.DesktopTarget, ve.init.sa.Target );
 ve.init.sa.DesktopTarget.prototype.setupToolbar = function ( surface ) {
 	if ( !this.actions ) {
 		this.actions = new ve.ui.TargetToolbar( this );
-		this.getToolbar().$actions.append( this.actions.$element );
 	}
-
-	this.actions.setup( this.constructor.static.actionGroups, surface );
 
 	// Parent method
 	ve.init.sa.DesktopTarget.super.prototype.setupToolbar.call( this, surface );
+
+	this.actions.setup( this.constructor.static.actionGroups, surface );
+	this.getToolbar().$actions.append( this.actions.$element );
+	this.actions.initialize();
+
+};
+
+/**
+ * @inheritdoc
+ */
+ve.init.sa.DesktopTarget.prototype.destroy = function () {
+	if ( this.actions ) {
+		this.actions.destroy();
+		this.actions = null;
+	}
+
+	// Parent method
+	ve.init.sa.DesktopTarget.super.prototype.destroy.apply( this, arguments );
 };
