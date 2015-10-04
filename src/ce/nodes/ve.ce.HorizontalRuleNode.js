7c95c6a85e736c47949a46f848271a15c85634db
Ed Sanders <esanders@wikimedia.org>
2015-10-04 15:04:59 +0100

    Wrap <hr>s so their margins are clickable
    Otherwise you have a click target that is ~2px high.
    
    Bug: T52770
    Change-Id: Iaae21ecb295f90a81800d4fd8947e309a9dc5790

---
+++ src/ce/nodes/ve.ce.HorizontalRuleNode.js
@@ -0,0 +1,47 @@
+/*!
+ * VisualEditor ContentEditable HorizontalRuleNode class.
+ *
+ * @copyright 2011-2015 VisualEditor Team and others; see http://ve.mit-license.org
+ */
+
+/**
+ * ContentEditable horizontal rule node.
+ *
+ * @class
+ * @extends ve.ce.LeafNode
+ * @mixins ve.ce.FocusableNode
+ *
+ * @constructor
+ * @param {ve.dm.HorizontalRuleNode} model Model to observe
+ * @param {Object} [config] Configuration options
+ */
+ve.ce.HorizontalRuleNode = function VeCeHorizontalRuleNode() {
+	// Parent constructor
+	ve.ce.HorizontalRuleNode.super.apply( this, arguments );
+
+	// Wrap the <hr> in a div so the margins become focusable
+	// and the user has a click target of more than 2px
+	this.$element = $( '<div>' ).append( this.$element );
+
+	// Mixin constructors
+	ve.ce.FocusableNode.call( this );
+
+	// DOM changes
+	this.$element.addClass( 've-ce-horizontalRuleNode' );
+};
+
+/* Inheritance */
+
+OO.inheritClass( ve.ce.HorizontalRuleNode, ve.ce.LeafNode );
+
+OO.mixinClass( ve.ce.HorizontalRuleNode, ve.ce.FocusableNode );
+
+/* Static Properties */
+
+ve.ce.HorizontalRuleNode.static.name = 'horizontalRule';
+
+ve.ce.HorizontalRuleNode.static.tagName = 'hr';
+
+/* Registration */
+
+ve.ce.nodeFactory.register( ve.ce.HorizontalRuleNode );
---
+++ src/dm/nodes/ve.dm.HorizontalRuleNode.js
@@ -0,0 +1,39 @@
+/*!
+ * VisualEditor DataModel HorizontalRuleNode class.
+ *
+ * @copyright 2011-2015 VisualEditor Team and others; see http://ve.mit-license.org
+ */
+
+/**
+ * DataModel horizontal rule node.
+ *
+ * @class
+ * @extends ve.dm.LeafNode
+ * @mixins ve.dm.FocusableNode
+ *
+ * @constructor
+ * @param {Object} [element] Reference to element in linear model
+ */
+ve.dm.HorizontalRuleNode = function VeDmHorizontalRuleNode() {
+	// Parent constructor
+	ve.dm.HorizontalRuleNode.super.apply( this, arguments );
+
+	// Mixin constructors
+	ve.dm.FocusableNode.call( this );
+};
+
+/* Inheritance */
+
+OO.inheritClass( ve.dm.HorizontalRuleNode, ve.dm.LeafNode );
+
+OO.mixinClass( ve.dm.HorizontalRuleNode, ve.dm.FocusableNode );
+
+/* Static Properties */
+
+ve.dm.HorizontalRuleNode.static.name = 'horizontalRule';
+
+ve.dm.HorizontalRuleNode.static.matchTagNames = [ 'hr' ];
+
+/* Registration */
+
+ve.dm.modelRegistry.register( ve.dm.HorizontalRuleNode );
