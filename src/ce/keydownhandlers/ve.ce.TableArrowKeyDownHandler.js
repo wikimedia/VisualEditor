24de6b22423221f899b55210dfced3b0f4317d4e
Ed Sanders <esanders@wikimedia.org>
2015-10-27 09:33:31 +0000

    Introduce key down handlers to separate logic from ce surface
    Bug: T78696
    Bug: T116748
    Change-Id: If0b1a855ac4454ad6087ec8c3f1e82e8fac618e4

---
+++ src/ce/keydownhandlers/ve.ce.TableArrowKeyDownHandler.js
@@ -0,0 +1,98 @@
+/*!
+ * VisualEditor ContentEditable table arrow key down handler
+ *
+ * @copyright 2011-2015 VisualEditor Team and others; see http://ve.mit-license.org
+ */
+
+/**
+ * Arrow key down handler for table selections.
+ *
+ * @class
+ * @extends ve.ce.KeyDownHandler
+ *
+ * @constructor
+ */
+ve.ce.TableArrowKeyDownHandler = function VeCeTableArrowKeyDownHandler() {
+	// Parent constructor
+	ve.ui.TableArrowKeyDownHandler.super.apply( this, arguments );
+};
+
+/* Inheritance */
+
+OO.inheritClass( ve.ce.TableArrowKeyDownHandler, ve.ce.KeyDownHandler );
+
+/* Static properties */
+
+ve.ce.TableArrowKeyDownHandler.static.name = 'tableArrow';
+
+ve.ce.TableArrowKeyDownHandler.static.keys = [
+	OO.ui.Keys.UP, OO.ui.Keys.DOWN, OO.ui.Keys.LEFT, OO.ui.Keys.RIGHT,
+	OO.ui.Keys.END, OO.ui.Keys.HOME, OO.ui.Keys.PAGEUP, OO.ui.Keys.PAGEDOWN
+];
+
+ve.ce.TableArrowKeyDownHandler.static.supportedSelections = [ 'table' ];
+
+/* Static methods */
+
+/**
+ * @inheritdoc
+ */
+ve.ce.TableArrowKeyDownHandler.static.execute = function ( surface, e ) {
+	var tableNode, newSelection,
+		checkDir = false,
+		selection = surface.getModel().getSelection(),
+		colOffset = 0,
+		rowOffset = 0;
+
+	switch ( e.keyCode ) {
+		case OO.ui.Keys.LEFT:
+			colOffset = -1;
+			checkDir = true;
+			break;
+		case OO.ui.Keys.RIGHT:
+			colOffset = 1;
+			checkDir = true;
+			break;
+		case OO.ui.Keys.UP:
+			rowOffset = -1;
+			break;
+		case OO.ui.Keys.DOWN:
+			rowOffset = 1;
+			break;
+		case OO.ui.Keys.HOME:
+			colOffset = -Infinity;
+			break;
+		case OO.ui.Keys.END:
+			colOffset = Infinity;
+			break;
+		case OO.ui.Keys.PAGEUP:
+			rowOffset = -Infinity;
+			break;
+		case OO.ui.Keys.PAGEDOWN:
+			rowOffset = Infinity;
+			break;
+	}
+
+	e.preventDefault();
+
+	if ( colOffset && checkDir ) {
+		tableNode = surface.documentView.getBranchNodeFromOffset( selection.tableRange.start + 1 );
+		if ( tableNode.$element.css( 'direction' ) !== 'ltr' ) {
+			colOffset *= -1;
+		}
+	}
+	if ( !e.shiftKey && !selection.isSingleCell() ) {
+		selection = selection.collapseToFrom();
+	}
+	newSelection = selection.newFromAdjustment(
+		e.shiftKey ? 0 : colOffset,
+		e.shiftKey ? 0 : rowOffset,
+		colOffset,
+		rowOffset
+	);
+	surface.getModel().setSelection( newSelection );
+};
+
+/* Registration */
+
+ve.ce.keyDownHandlerFactory.register( ve.ce.TableArrowKeyDownHandler );
---
+++ src/ce/keydownhandlers/ve.ce.TableDeleteKeyDownHandler.js
@@ -0,0 +1,69 @@
+/*!
+ * VisualEditor ContentEditable table delete key down handler
+ *
+ * @copyright 2011-2015 VisualEditor Team and others; see http://ve.mit-license.org
+ */
+
+/**
+ * Delete key down handler for table selections.
+ *
+ * @class
+ * @extends ve.ce.KeyDownHandler
+ *
+ * @constructor
+ */
+ve.ce.TableDeleteKeyDownHandler = function VeCeTableDeleteKeyDownHandler() {
+	// Parent constructor
+	ve.ui.TableDeleteKeyDownHandler.super.apply( this, arguments );
+};
+
+/* Inheritance */
+
+OO.inheritClass( ve.ce.TableDeleteKeyDownHandler, ve.ce.KeyDownHandler );
+
+/* Static properties */
+
+ve.ce.TableDeleteKeyDownHandler.static.name = 'tableDelete';
+
+ve.ce.TableDeleteKeyDownHandler.static.keys = [ OO.ui.Keys.BACKSPACE, OO.ui.Keys.DELETE ];
+
+ve.ce.TableDeleteKeyDownHandler.static.supportedSelections = [ 'table' ];
+
+/* Static methods */
+
+/**
+ * @inheritdoc
+ *
+ * Handle delete and backspace key down events with a table selection.
+ *
+ * Performs a strip-delete removing all the cell contents but not altering the structure.
+ */
+ve.ce.TableDeleteKeyDownHandler.static.execute = function ( surface, e ) {
+	var i, l,
+		surfaceModel = surface.getModel(),
+		fragments = [],
+		cells = surfaceModel.getSelection().getMatrixCells();
+
+	if ( e ) {
+		e.preventDefault();
+	}
+
+	for ( i = 0, l = cells.length; i < l; i++ ) {
+		if ( cells[ i ].node.isCellEditable() ) {
+			// Create auto-updating fragments from ranges
+			fragments.push( surfaceModel.getLinearFragment( cells[ i ].node.getRange(), true ) );
+		}
+	}
+
+	for ( i = 0, l = fragments.length; i < l; i++ ) {
+		// Replace contents with empty wrapper paragraphs
+		fragments[ i ].insertContent( [
+			{ type: 'paragraph', internal: { generated: 'wrapper' } },
+			{ type: '/paragraph' }
+		] );
+	}
+};
+
+/* Registration */
+
+ve.ce.keyDownHandlerFactory.register( ve.ce.TableDeleteKeyDownHandler );
