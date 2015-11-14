7ecde6c052d059cf08b906145f8f17cf5b89e30b
David Lynch <dlynch@wikimedia.org>
2015-11-13 16:30:29 -0800

    Add ve.ui.AnnotationContextItem
    Some context items appear on annotations. For them, pressing "delete"
    is ambiguous, because it might mean to delete the annotation and it
    might mean to delete the annotation-plus-content. So, create a new
    ContextItem abstract class which can be inherited from, to enable
    a "clear annotations" button in the context popup.
    
    Implement it on Link and Language contextitems.
    
    Remove the 'remove' action fron AnnotationInspector, since it's now
    superfluous.
    
    (Also, we currently restrict isDeletable to only be true for aliens.)
    
    Bug: T98272
    Change-Id: I3b1b553d2e148e2a50df897b8bc5b29412bdbca7

---
+++ src/ui/contextitems/ve.ui.AnnotationContextItem.js
@@ -0,0 +1,82 @@
+/*!
+ * VisualEditor AnnotationContextItem class.
+ *
+ * @copyright 2011-2015 VisualEditor Team and others; see http://ve.mit-license.org
+ */
+
+/**
+ * Context item for an annotation.
+ *
+ * @class
+ * @abstract
+ * @extends ve.ui.LinearContextItem
+ *
+ * @param {ve.ui.Context} context Context item is in
+ * @param {ve.dm.Model} model Model item is related to
+ * @param {Object} config Configuration options
+ */
+ve.ui.AnnotationContextItem = function VeUiAnnotationontextItem( context, model, config ) {
+	// Parent constructor
+	ve.ui.AnnotationContextItem.super.call( this, context, model, config );
+
+	// Initialization
+	this.$element.addClass( 've-ui-annotationContextItem' );
+
+	if ( !this.context.isMobile() ) {
+		this.clearButton = new OO.ui.ButtonWidget( {
+			title: this.constructor.static.clearMsg,
+			icon: this.constructor.static.clearIcon,
+			flags: [ 'destructive' ]
+		} );
+	} else {
+		this.clearButton = new OO.ui.ButtonWidget( {
+			framed: false,
+			icon: this.constructor.static.clearIcon,
+			flags: [ 'destructive' ]
+		} );
+	}
+	if ( this.isClearable() ) {
+		this.actionButtons.addItems( [ this.clearButton ], 0 );
+	}
+	this.clearButton.connect( this, { click: 'onClearButtonClick' } );
+};
+
+/* Inheritance */
+
+OO.inheritClass( ve.ui.AnnotationContextItem, ve.ui.LinearContextItem );
+
+/* Static Properties */
+
+ve.ui.AnnotationContextItem.static.clearable = true;
+ve.ui.AnnotationContextItem.static.clearIcon = 'cancel';
+ve.ui.AnnotationContextItem.static.clearMsg = OO.ui.deferMsg( 'visualeditor-clearbutton-tooltip' );
+
+/* Methods */
+
+/**
+ * Check if item is clearable.
+ *
+ * @return {boolean} Item is clearable
+ */
+ve.ui.AnnotationContextItem.prototype.isClearable = function () {
+	return this.constructor.static.clearable;
+};
+
+/**
+ * Handle clear button click events.
+ *
+ * @localdoc Removes any modelClasses annotations from the current fragment
+ *
+ * @protected
+ */
+ve.ui.AnnotationContextItem.prototype.onClearButtonClick = function () {
+	var i, len,
+		modelClasses = this.constructor.static.modelClasses,
+		fragment = this.getFragment(),
+		annotations = fragment.getAnnotations( true ).filter( function ( annotation ) {
+			return ve.isInstanceOfAny( annotation, modelClasses );
+		} ).get();
+	for ( i = 0, len = annotations.length; i < len; i++ ) {
+		fragment.expandLinearSelection( 'annotation', annotations[ i ] ).annotateContent( 'clear', annotations[ i ] );
+	}
+};
