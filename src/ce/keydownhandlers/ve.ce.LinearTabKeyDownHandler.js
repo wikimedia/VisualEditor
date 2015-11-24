ee3c7877b5eca7d25f80c6f597d49825761c09d8
David Lynch <dlynch@wikimedia.org>
2015-11-24 10:47:16 -0600

    When the cursor is inside a table cell, tab moves between cells
    For consistency, make tab when a table cell is the entire selection
    be treated as a right arrow as well.
    
    Break out the table-cell selection movement from the TableArrow
    KeyDownHandler so that we can just reuse it here.
    
    Bug: T119545
    Change-Id: I8ffd4f2b26e9783bc79a9cf7c022f004b22ec441

---
+++ src/ce/keydownhandlers/ve.ce.LinearTabKeyDownHandler.js
@@ -0,0 +1,62 @@
+/*!
+ * VisualEditor ContentEditable linear escape key down handler
+ *
+ * @copyright 2011-2015 VisualEditor Team and others; see http://ve.mit-license.org
+ */
+
+/**
+ * Tab key down handler for linear selections.
+ *
+ * @class
+ * @extends ve.ce.KeyDownHandler
+ *
+ * @constructor
+ */
+ve.ce.LinearTabKeyDownHandler = function VeCeLinearTabKeyDownHandler() {
+	// Parent constructor
+	ve.ui.LinearTabKeyDownHandler.super.apply( this, arguments );
+};
+
+/* Inheritance */
+
+OO.inheritClass( ve.ce.LinearTabKeyDownHandler, ve.ce.TableArrowKeyDownHandler );
+
+/* Static properties */
+
+ve.ce.LinearTabKeyDownHandler.static.name = 'linearTab';
+
+ve.ce.LinearTabKeyDownHandler.static.keys = [ OO.ui.Keys.TAB ];
+
+ve.ce.LinearTabKeyDownHandler.static.supportedSelections = [ 'linear' ];
+
+/* Static methods */
+
+/**
+ * @inheritdoc
+ *
+ * Handle escape key down events with a linear selection while table editing.
+ */
+ve.ce.LinearTabKeyDownHandler.static.execute = function ( surface, e ) {
+	var tableNode = surface.getActiveTableNode();
+	if ( tableNode ) {
+		e.preventDefault();
+		e.stopPropagation();
+		tableNode.setEditing( false );
+		// if this was a merged cell, we're going to have unexpected behavior when the selection moves,
+		// so preemptively collapse to the top-left point of the merged cell.
+		surface.getModel().setSelection( surface.getModel().getSelection().collapseToStart() );
+		ve.ce.LinearTabKeyDownHandler.static.moveTableSelection(
+			surface,
+			0, // rows
+			e.shiftKey ? -1 : 1, // columns
+			true, // respect rtl/ltr
+			false // don't expand the current selection
+		);
+		return true;
+	}
+	return false;
+};
+
+/* Registration */
+
+ve.ce.keyDownHandlerFactory.register( ve.ce.LinearTabKeyDownHandler );
