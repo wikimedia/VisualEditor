c45cfebc56ef32a0ec53954913d2b818b54ef1c5
Ed Sanders <esanders@wikimedia.org>
2016-05-18 17:10:20 +0100

    Add <BDI> support
    Bug: T135620
    Change-Id: Id93747ee674ca7efd75d93ae576f82fc7e2e92fe

---
+++ src/ce/annotations/ve.ce.BidiAnnotation.js
@@ -0,0 +1,37 @@
+/*!
+ * VisualEditor ContentEditable BidiAnnotation class.
+ *
+ * @copyright 2011-2016 VisualEditor Team and others; see http://ve.mit-license.org
+ */
+
+/**
+ * ContentEditable bi-di annotation.
+ *
+ * @class
+ * @extends ve.ce.TextStyleAnnotation
+ * @constructor
+ * @param {ve.dm.BidiAnnotation} model Model to observe
+ * @param {ve.ce.ContentBranchNode} [parentNode] Node rendering this annotation
+ * @param {Object} [config] Configuration options
+ */
+ve.ce.BidiAnnotation = function VeCeBidiAnnotation() {
+	// Parent constructor
+	ve.ce.BidiAnnotation.super.apply( this, arguments );
+
+	// DOM changes
+	this.$element.addClass( 've-ce-bidiAnnotation' );
+};
+
+/* Inheritance */
+
+OO.inheritClass( ve.ce.BidiAnnotation, ve.ce.TextStyleAnnotation );
+
+/* Static Properties */
+
+ve.ce.BidiAnnotation.static.name = 'textStyle/bidi';
+
+ve.ce.BidiAnnotation.static.tagName = 'bdi';
+
+/* Registration */
+
+ve.ce.annotationFactory.register( ve.ce.BidiAnnotation );
---
+++ src/dm/annotations/ve.dm.BidiAnnotation.js
@@ -0,0 +1,34 @@
+/*!
+ * VisualEditor DataModel BidiAnnotation class.
+ *
+ * @copyright 2011-2016 VisualEditor Team and others; see http://ve.mit-license.org
+ */
+
+/**
+ * DataModel bi-di annotation.
+ *
+ * Represents `<bdi>` tags.
+ *
+ * @class
+ * @extends ve.dm.TextStyleAnnotation
+ * @constructor
+ * @param {Object} element
+ */
+ve.dm.BidiAnnotation = function VeDmBidiAnnotation() {
+	// Parent constructor
+	ve.dm.BidiAnnotation.super.apply( this, arguments );
+};
+
+/* Inheritance */
+
+OO.inheritClass( ve.dm.BidiAnnotation, ve.dm.TextStyleAnnotation );
+
+/* Static Properties */
+
+ve.dm.BidiAnnotation.static.name = 'textStyle/bidi';
+
+ve.dm.BidiAnnotation.static.matchTagNames = [ 'bdi' ];
+
+/* Registration */
+
+ve.dm.modelRegistry.register( ve.dm.BidiAnnotation );
