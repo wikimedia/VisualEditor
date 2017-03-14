barionleg/visualegitor

DiffElement: Allow a node change to be an attribute change *and* a linear diff
Change-Id: I0cb2f28f12cda08593ea9d0511d6893ec28b6d16

src/ui/elements/ve.ui.DiffElement.js
@@ -327,7 +327,8 @@ ve.ui.DiffElement.prototype.getChangedNodeElements = function ( oldNodeIndex, mo
 			subTreeRootNodeData = subTreeDiffInfo.linearDiff;
 			annotatedData = this.annotateNode( subTreeRootNodeData );
 			ve.batchSplice( nodeData, subTreeRootNodeRangeStart + 1, subTreeRootNode.node.length, annotatedData );
-		} else {
+		}
+		if ( subTreeDiffInfo.attributeChange ) {
 			// If there is no content change, just add change class
 			nodeData[ subTreeRootNodeRangeStart ] = this.addAttributesToNode(
 				nodeData[ subTreeRootNodeRangeStart ], this.newDoc, { 'data-diff-action': 'change' }

wikimedia/VisualEditor

src/ui/elements/ve.ui.DiffElement.js
@@ -370,7 +370,8 @@ ve.ui.DiffElement.prototype.getChangedNodeElements = function ( oldNodeIndex, mo
 			nodeDiffData = diffInfo.linearDiff;
 			annotatedData = this.annotateNode( nodeDiffData );
 			ve.batchSplice( nodeData, nodeRangeStart + 1, node.node.length, annotatedData );
-		} else {
+		}
+		if ( diffInfo.attributeChange ) {
 			// If there is no content change, just add change class
 			nodeData[ nodeRangeStart ] = this.addAttributesToNode(
 				nodeData[ nodeRangeStart ], this.newDoc, { 'data-diff-action': 'structural-change' }