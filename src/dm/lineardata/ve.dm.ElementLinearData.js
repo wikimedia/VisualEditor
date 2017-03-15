wikimedia/VisualEditor

commit 14b5fbc3979e9064d22ad7225f256a967658394f

[BREAKING CHANGE] Move originalDomElements to the IV store and use MD5
Doing so ensures that the linear model contains only data that
can be quickly serialized with JSON.stringify.

Using MD5 hashes removes the need for complex remappings when
documents are merged.

Bug: T142943
Change-Id: I8a71c1a40ec35108d0a9a388da6f75632f8dc53c

src/dm/lineardata/ve.dm.ElementLinearData.js
@@ -937,6 +937,7 @@ ve.dm.ElementLinearData.prototype.getWordRange = function ( offset ) {
  */
 ve.dm.ElementLinearData.prototype.getUsedStoreValues = function ( range ) {
 	var i, index, indexes, j,
+		store = this.getStore(),
 		valueStore = {};
 
 	range = range || new ve.Range( 0, this.data.length );
@@ -949,34 +950,14 @@ ve.dm.ElementLinearData.prototype.getUsedStoreValues = function ( range ) {
 		while ( j-- ) {
 			index = indexes[ j ];
 			if ( !Object.prototype.hasOwnProperty.call( valueStore, index ) ) {
-				valueStore[ index ] = this.getStore().value( index );
+				valueStore[ index ] = store.value( index );
 			}
+		}
+		if ( this.data[ i ].originalDomElementsIndex !== undefined ) {
+			valueStore[ this.data[ i ].originalDomElementsIndex ] = store.value( this.data[ i ].originalDomElementsIndex );
 		}
 	}
 	return valueStore;
-};
-
-/**
- * Remap the store indexes used in this linear data.
- *
- * Remaps annotations and calls remapStoreIndexes() on each node.
- *
- * @method
- * @param {Object} mapping Mapping from store indexes to store indexes
- */
-ve.dm.ElementLinearData.prototype.remapStoreIndexes = function ( mapping ) {
-	var i, ilen, j, jlen, indexes, nodeClass;
-	for ( i = 0, ilen = this.data.length; i < ilen; i++ ) {
-		indexes = this.getAnnotationIndexesFromOffset( i, true );
-		for ( j = 0, jlen = indexes.length; j < jlen; j++ ) {
-			indexes[ j ] = mapping[ indexes[ j ] ];
-		}
-		this.setAnnotationIndexesAtOffset( i, indexes );
-		if ( this.isOpenElementData( i ) ) {
-			nodeClass = ve.dm.nodeFactory.lookup( this.getType( i ) );
-			nodeClass.static.remapStoreIndexes( this.data[ i ], mapping );
-		}
-	}
 };
 
 /**
@@ -1032,7 +1013,7 @@ ve.dm.ElementLinearData.prototype.remapInternalListKeys = function ( internalLis
  */
 ve.dm.ElementLinearData.prototype.sanitize = function ( rules ) {
 	var i, len, annotations, emptySet, setToRemove, type,
-		canContainContent, contentElement, isOpen, nodeClass, ann, oldHash,
+		canContainContent, contentElement, isOpen, nodeClass, ann,
 		store = this.getStore(),
 		allAnnotations = this.getAnnotationsFromRange( new ve.Range( 0, this.getLength() ), true );
 
@@ -1043,10 +1024,8 @@ ve.dm.ElementLinearData.prototype.sanitize = function ( rules ) {
 			// Remove originalDomElements from annotations
 			for ( i = 0, len = allAnnotations.getLength(); i < len; i++ ) {
 				ann = allAnnotations.get( i );
-				if ( ann.element.originalDomElements ) {
-					oldHash = OO.getHash( ann );
-					delete allAnnotations.get( i ).element.originalDomElements;
-					store.replaceHash( oldHash, ann );
+				if ( ann.element.originalDomElementsIndex !== undefined ) {
+					delete allAnnotations.get( i ).element.originalDomElementsIndex;
 				}
 			}
 		}
@@ -1168,7 +1147,7 @@ ve.dm.ElementLinearData.prototype.sanitize = function ( rules ) {
 			}
 			if ( rules.removeOriginalDomElements ) {
 				// Remove originalDomElements from nodes
-				delete this.getData( i ).originalDomElements;
+				delete this.getData( i ).originalDomElementsIndex;
 			}
 		}
 	}
@@ -1182,12 +1161,13 @@ ve.dm.ElementLinearData.prototype.sanitize = function ( rules ) {
  * @param {boolean} preserveGenerated Preserve internal.generated properties of elements
  */
 ve.dm.ElementLinearData.prototype.cloneElements = function ( preserveGenerated ) {
-	var i, len, nodeClass;
+	var i, len, nodeClass,
+		store = this.getStore();
 	for ( i = 0, len = this.getLength(); i < len; i++ ) {
 		if ( this.isOpenElementData( i ) ) {
 			nodeClass = ve.dm.nodeFactory.lookup( this.getType( i ) );
 			if ( nodeClass ) {
-				this.setData( i, nodeClass.static.cloneElement( this.getData( i ), preserveGenerated ) );
+				this.setData( i, nodeClass.static.cloneElement( this.getData( i ), store, preserveGenerated ) );
 			}
 		}
 	}

src/dm/ve.dm.IndexValueStore.js
@@ -4,129 +4,119 @@
  * @copyright 2011-2016 VisualEditor Team and others; see http://ve.mit-license.org
  */
 
+/* global SparkMD5 */
+
 /**
- * Index-value store
+ * Hash-keyed value store
+ *
+ * Values are objects, strings or Arrays, and are hashed using an algorithm with low collision
+ * probability: values with the same hash can be assumed equal.
+ *
+ * Two stores can be merged even if they have differently computed hashes, so long as two values
+ * will (with high probability) have the same hash only if equal. In this case, equivalent
+ * values can have two different hashes.
+ *
+ * TODO: rename the class to reflect that it is no longer an index-value store
  *
  * @class
  * @constructor
  */
 ve.dm.IndexValueStore = function VeDmIndexValueStore() {
-	// maps hashes to indexes
+	// maps values to hashes
 	this.hashStore = {};
-	// maps indexes to values
-	this.valueStore = [];
 };
 
 /* Methods */
 
 /**
- * Get the index of a value in the store.
- *
- * If the hash is not found the value is added to the store.
+ * Insert a value into the store
  *
  * @method
- * @param {Object|string|Array} value Value to lookup or store
- * @param {string} [hash] Value hash. Uses OO.getHash( value ) if not provided.
- * @param {boolean} [overwrite=false] Overwrite the value in the store if the hash is already in use
- * @return {number} The index of the value in the store
+ * @param {Object|string|Array} value Value to store
+ * @param {string} [stringified] Stringified version of value; default OO.getHash( value )
+ * @param {boolean} [overwrite=false] Overwrite the value in the store if the hash is found
+ * @return {string} Hash value with low collision probability
  */
-ve.dm.IndexValueStore.prototype.index = function ( value, hash, overwrite ) {
-	var index;
-	if ( typeof hash !== 'string' ) {
-		hash = OO.getHash( value );
-	}
-	index = this.indexOfHash( hash );
-	if ( index === null || overwrite ) {
-		if ( index === null ) {
-			index = this.valueStore.length;
-		}
+ve.dm.IndexValueStore.prototype.index = function ( value, stringified, overwrite ) {
+	var hash = this.indexOfValue( value, stringified );
+	if ( !this.hashStore[ hash ] || overwrite ) {
 		if ( Array.isArray( value ) ) {
-			this.valueStore[ index ] = ve.copy( value );
+			this.hashStore[ hash ] = ve.copy( value );
 		} else if ( typeof value === 'object' ) {
-			this.valueStore[ index ] = ve.cloneObject( value );
+			this.hashStore[ hash ] = ve.cloneObject( value );
 		} else {
-			this.valueStore[ index ] = value;
+			this.hashStore[ hash ] = value;
 		}
-		this.hashStore[ hash ] = index;
 	}
-	return index;
+
+	return hash;
 };
 
 /**
- * Get the index of a hash in the store.
- *
- * Returns null if the hash is not found.
+ * Get the hash of a value without inserting it in the store
  *
  * @method
- * @param {Object|string|Array} hash Value hash.
- * @return {number|null} The index of the value in the store, or undefined if it is not found
+ * @param {Object|string|Array} value Value to hash
+ * @param {string} [stringified] Stringified version of value; default OO.getHash( value )
+ * @return {string} Hash value with low collision probability
  */
-ve.dm.IndexValueStore.prototype.indexOfHash = function ( hash ) {
-	return hash in this.hashStore ? this.hashStore[ hash ] : null;
+ve.dm.IndexValueStore.prototype.indexOfValue = function ( value, stringified ) {
+	if ( typeof stringified !== 'string' ) {
+		stringified = OO.getHash( value );
+	}
+
+	// We don't need cryptographically strong hashes, just low collision probability. Given
+	// effectively random hash distribution, for n values hashed into a space of m hash
+	// strings, the probability of a collision is roughly n^2 / (2m). We use 16 hex digits
+	// of MD5 i.e. 2^64 possible hash strings, so given 2^16 stored values the collision
+	// probability is about 2^-33 =~ 0.0000000001 , i.e. negligible.
+	//
+	// Prefix with a letter to prevent all numeric hashes, and to constrain the space of
+	// possible object property values.
+	return 'h' + SparkMD5.hash( stringified ).slice( 0, 16 );
 };
 
 /**
- * Get the indexes of values in the store
+ * Get the hashes of values in the store
  *
  * Same as index but with arrays.
  *
  * @method
  * @param {Object[]} values Values to lookup or store
- * @return {Array} The indexes of the values in the store
+ * @return {string[]} The hashes of the values in the store
  */
 ve.dm.IndexValueStore.prototype.indexes = function ( values ) {
-	var i, length, indexes = [];
+	var i, length, hashes = [];
 	for ( i = 0, length = values.length; i < length; i++ ) {
-		indexes.push( this.index( values[ i ] ) );
+		hashes.push( this.index( values[ i ] ) );
 	}
-	return indexes;
+	return hashes;
 };
 
 /**
- * Get the value at a particular index
+ * Get the value stored for a particular hash
  *
  * @method
- * @param {number} index Index to lookup
- * @return {Object|undefined} Value at this index, or undefined if out of bounds
+ * @param {string} hash Hash to look up
+ * @return {Object|undefined} Value stored for this hash if present, else undefined
  */
-ve.dm.IndexValueStore.prototype.value = function ( index ) {
-	return this.valueStore[ index ];
+ve.dm.IndexValueStore.prototype.value = function ( hash ) {
+	return this.hashStore[ hash ];
 };
 
 /**
- * Replace a value's stored hash, e.g. if the value has changed and you want to discard the old one.
- *
- * @param {string} oldHash The value's previously stored hash
- * @param {Object|string|Array} value New value
- * @throws {Error} Old hash not found
- */
-ve.dm.IndexValueStore.prototype.replaceHash = function ( oldHash, value ) {
-	var newHash = OO.getHash( value ),
-		index = this.hashStore[ oldHash ];
-
-	if ( index === undefined ) {
-		throw new Error( 'Old hash not found: ' + oldHash );
-	}
-
-	delete this.hashStore[ oldHash ];
-
-	this.hashStore[ newHash ] = index;
-	this.valueStore[ index ] = value;
-};
-
-/**
- * Get the values at a set of indexes
+ * Get the values stored for a list of hashes
  *
  * Same as value but with arrays.
  *
  * @method
- * @param {number[]} indexes Indices to lookup
- * @return {Array} Values at these indexes, or undefined if out of bounds
+ * @param {string[]} hashes Hashes to lookup
+ * @return {Array} Values for these hashes (undefined for any not present)
  */
-ve.dm.IndexValueStore.prototype.values = function ( indexes ) {
+ve.dm.IndexValueStore.prototype.values = function ( hashes ) {
 	var i, length, values = [];
-	for ( i = 0, length = indexes.length; i < length; i++ ) {
-		values.push( this.value( indexes[ i ] ) );
+	for ( i = 0, length = hashes.length; i < length; i++ ) {
+		values.push( this.value( hashes[ i ] ) );
 	}
 	return values;
 };
@@ -141,10 +131,9 @@ ve.dm.IndexValueStore.prototype.values = function ( indexes ) {
  * @return {ve.dm.IndexValueStore} New store with the same contents as this one
  */
 ve.dm.IndexValueStore.prototype.clone = function () {
-	var key, clone = new this.constructor();
-	clone.valueStore = this.valueStore.slice();
-	for ( key in this.hashStore ) {
-		clone.hashStore[ key ] = this.hashStore[ key ];
+	var hash, clone = new this.constructor();
+	for ( hash in this.hashStore ) {
+		clone.hashStore[ hash ] = this.hashStore[ hash ];
 	}
 	return clone;
 };
@@ -152,23 +141,20 @@ ve.dm.IndexValueStore.prototype.clone = function () {
 /**
  * Merge another store into this store.
  *
- * Objects that are in other but not in this are added to this, possibly with a different index.
- * Objects present in both stores may have different indexes in each store. An object is returned
- * mapping each index in other to the corresponding index in this.
+ * It is allowed for the two stores to have differently computed hashes, so long as two values
+ * will (with high probability) have the same hash only if equal. In this case, equivalent
+ * values can have two different hashes.
  *
  * Objects added to the store are added by reference, not cloned like in .index()
  *
  * @param {ve.dm.IndexValueStore} other Store to merge into this one
- * @return {Object} Object in which the keys are indexes in other and the values are the corresponding keys in this
  */
 ve.dm.IndexValueStore.prototype.merge = function ( other ) {
-	var key, index, mapping = {};
-	for ( key in other.hashStore ) {
-		if ( !Object.prototype.hasOwnProperty.call( this.hashStore, key ) ) {
-			index = this.valueStore.push( other.valueStore[ other.hashStore[ key ] ] ) - 1;
-			this.hashStore[ key ] = index;
+	var hash;
+
+	for ( hash in other.hashStore ) {
+		if ( !Object.prototype.hasOwnProperty.call( this.hashStore, hash ) ) {
+			this.hashStore[ hash ] = other.hashStore[ hash ];
 		}
-		mapping[ other.hashStore[ key ] ] = this.hashStore[ key ];
 	}
-	return mapping;
 };

tests/dm/lineardata/ve.dm.ElementLinearData.test.js
@@ -1536,7 +1536,7 @@ QUnit.test( 'getWordRange', function ( assert ) {
 } );
 
 QUnit.test( 'sanitize', function ( assert ) {
-	var i, model, data, actualStore,
+	var i, model, data, actualStore, key,
 		count = 0,
 		bold = { type: 'textStyle/bold', attributes: { nodeName: 'b' } },
 		cases = [
@@ -1544,12 +1544,12 @@ QUnit.test( 'sanitize', function ( assert ) {
 				html: '<p style="text-shadow: 0 0 1px #000;">F<b style="color:blue;">o</b>o</p>',
 				data: [
 					{ type: 'paragraph' },
-					'F', [ 'o', [ 0 ] ], 'o',
+					'F', [ 'o', [ 'h5dd47c3facf7b6a8' ] ], 'o',
 					{ type: '/paragraph' },
 					{ type: 'internalList' },
 					{ type: '/internalList' }
 				],
-				store: [ bold ],
+				store: { h5dd47c3facf7b6a8: bold },
 				rules: { removeOriginalDomElements: true },
 				msg: 'Original DOM elements removed'
 			},
@@ -1675,14 +1675,14 @@ QUnit.test( 'sanitize', function ( assert ) {
 				data: [
 					{ type: 'paragraph' },
 					'F', 'o', 'o', ' ', 'B', 'a', 'r', ' ',
-					[ 'B', [ 0 ] ],
-					[ 'a', [ 0 ] ],
-					[ 'z', [ 0 ] ],
-					[ ' ', [ 0 ] ],
-					[ 'Q', [ 0 ] ],
-					[ 'u', [ 0 ] ],
-					[ 'u', [ 0 ] ],
-					[ 'x', [ 0 ] ],
+					[ 'B', [ ve.dm.example.annIndex( 'b', 'Baz \nQuux' ) ] ],
+					[ 'a', [ ve.dm.example.annIndex( 'b', 'Baz \nQuux' ) ] ],
+					[ 'z', [ ve.dm.example.annIndex( 'b', 'Baz \nQuux' ) ] ],
+					[ ' ', [ ve.dm.example.annIndex( 'b', 'Baz \nQuux' ) ] ],
+					[ 'Q', [ ve.dm.example.annIndex( 'b', 'Baz \nQuux' ) ] ],
+					[ 'u', [ ve.dm.example.annIndex( 'b', 'Baz \nQuux' ) ] ],
+					[ 'u', [ ve.dm.example.annIndex( 'b', 'Baz \nQuux' ) ] ],
+					[ 'x', [ ve.dm.example.annIndex( 'b', 'Baz \nQuux' ) ] ],
 					{ type: '/paragraph' },
 					{ type: 'internalList' },
 					{ type: '/internalList' }
@@ -1734,10 +1734,10 @@ QUnit.test( 'sanitize', function ( assert ) {
 		data.sanitize( cases[ i ].rules || {} );
 		assert.equalLinearData( data.data, cases[ i ].data, cases[ i ].msg + ': data' );
 		if ( cases[ i ].store ) {
-			/*jshint loopfunc:true */
-			actualStore = data.getStore().valueStore.map( function ( ann ) {
-				return ann.element;
-			} );
+			actualStore = {};
+			for ( key in cases[ i ].store ) {
+				actualStore[ key ] = data.getStore().value( key ).element;
+			}
 			assert.deepEqualWithDomElements( actualStore, cases[ i ].store, cases[ i ].msg + ': store' );
 		}
 	}
@@ -1865,30 +1865,30 @@ QUnit.test( 'getUsedStoreValues', function ( assert ) {
 			{
 				msg: 'no range (whole document) contains everything',
 				expected: {
-					0: bold,
-					1: italic
+					h49981eab0f8056ff: bold,
+					hefd27ef3bf2041dd: italic
 				}
 			},
 			{
 				msg: '2-4 contains bold and italic',
 				range: new ve.Range( 2, 4 ),
 				expected: {
-					0: bold,
-					1: italic
+					h49981eab0f8056ff: bold,
+					hefd27ef3bf2041dd: italic
 				}
 			},
 			{
 				msg: '2-3 contains bold',
 				range: new ve.Range( 2, 3 ),
 				expected: {
-					0: bold
+					h49981eab0f8056ff: bold
 				}
 			},
 			{
 				msg: '3-4 contains italic',
 				range: new ve.Range( 3, 4 ),
 				expected: {
-					1: italic
+					hefd27ef3bf2041dd: italic
 				}
 			},
 			{
@@ -1898,140 +1898,19 @@ QUnit.test( 'getUsedStoreValues', function ( assert ) {
 			}
 		];
 
+	function getElement( ann ) {
+		return ann.element;
+	}
+
 	QUnit.expect( cases.length );
 	for ( i = 0; i < cases.length; i++ ) {
 		assert.deepEqual(
-			elementData.getUsedStoreValues( cases[ i ].range ),
-			cases[ i ].expected,
+			ve.copy( elementData.getUsedStoreValues( cases[ i ].range ), getElement ),
+			ve.copy( cases[ i ].expected, getElement ),
 			cases[ i ].msg
 		);
 	}
 
-} );
-
-QUnit.test( 'remapStoreIndexes', function ( assert ) {
-	var i, data,
-		cases = [
-			{
-				before: [
-					[ 'F', [ 0 ] ],
-					[ 'o', [ 1 ] ],
-					[ 'o', [ 2 ] ]
-				],
-				mapping: {
-					0: 1,
-					1: 2,
-					2: 3
-				},
-				after: [
-					[ 'F', [ 1 ] ],
-					[ 'o', [ 2 ] ],
-					[ 'o', [ 3 ] ]
-				],
-				msg: 'Annotated text: increment indexes'
-			},
-			{
-				before: [
-					[ 'F', [ 0 ] ],
-					[ 'o', [ 1 ] ],
-					[ 'o', [ 2 ] ]
-				],
-				mapping: {
-					0: 1,
-					1: 0,
-					2: 2
-				},
-				after: [
-					[ 'F', [ 1 ] ],
-					[ 'o', [ 0 ] ],
-					[ 'o', [ 2 ] ]
-				],
-				msg: 'Annotated text: swap 0 and 1'
-			},
-			{
-				before: [
-					[ 'F', [ 0, 1 ] ],
-					[ 'o', [ 1, 2 ] ],
-					[ 'o', [ 2, 3 ] ]
-				],
-				mapping: {
-					0: 3,
-					1: 2,
-					2: 1,
-					3: 0
-				},
-				after: [
-					[ 'F', [ 3, 2 ] ],
-					[ 'o', [ 2, 1 ] ],
-					[ 'o', [ 1, 0 ] ]
-				],
-				msg: 'Annotated text: multiple annotations mapped, order preserved'
-			},
-			{
-				before: [
-					{ type: 'alienInline', annotations: [ 0 ] },
-					{ type: '/alienInline' }
-				],
-				mapping: {
-					0: 1,
-					1: 0
-				},
-				after: [
-					{ type: 'alienInline', annotations: [ 1 ] },
-					{ type: '/alienInline' }
-				],
-				msg: 'Annotated node'
-			},
-			{
-				before: [
-					{ type: 'paragraph' },
-					'F',
-					[ 'o', [ 2, 1, 3 ] ],
-					[ 'o', [ 4 ] ],
-					{ type: 'alienInline', annotations: [ 5, 0 ] },
-					{ type: '/alienInline' },
-					[ 'B', [ 5, 0 ] ],
-					[ 'a', [ 7 ] ],
-					'r',
-					{ type: 'alienInline', annotations: [ 6 ] },
-					{ type: '/alienInline' },
-					{ type: '/paragraph' }
-				],
-				mapping: {
-					0: 1,
-					1: 4,
-					2: 2,
-					3: 8,
-					4: 5,
-					5: 7,
-					6: 3,
-					7: 6,
-					8: 0
-				},
-				after: [
-					{ type: 'paragraph' },
-					'F',
-					[ 'o', [ 2, 4, 8 ] ],
-					[ 'o', [ 5 ] ],
-					{ type: 'alienInline', annotations: [ 7, 1 ] },
-					{ type: '/alienInline' },
-					[ 'B', [ 7, 1 ] ],
-					[ 'a', [ 6 ] ],
-					'r',
-					{ type: 'alienInline', annotations: [ 3 ] },
-					{ type: '/alienInline' },
-					{ type: '/paragraph' }
-				],
-				msg: 'Paragraph with mix of unannotated text, annotated text and annotated nodes'
-			}
-		];
-
-	QUnit.expect( cases.length );
-	for ( i = 0; i < cases.length; i++ ) {
-		data = new ve.dm.ElementLinearData( new ve.dm.IndexValueStore(), cases[ i ].before );
-		data.remapStoreIndexes( cases[ i ].mapping );
-		assert.deepEqual( data.data, cases[ i ].after, cases[ i ].msg );
-	}
 } );
 
 QUnit.test( 'compareElements', function ( assert ) {

demos/ve/desktop.html
@@ -151,6 +151,9 @@
 		<!-- rangefix -->
 		<script src="../../lib/rangefix/rangefix.js"></script>
 
+		<!-- spark-md5 -->
+		<script src="../../lib/spark-md5/spark-md5.js"></script>
+
 		<!-- dom-classlist-shim -->
 		<script src="../../lib/classList/classList.js"></script>
 

demos/ve/mobile.html
@@ -152,6 +152,9 @@
 		<!-- rangefix -->
 		<script src="../../lib/rangefix/rangefix.js"></script>
 
+		<!-- spark-md5 -->
+		<script src="../../lib/spark-md5/spark-md5.js"></script>
+
 		<!-- dom-classlist-shim -->
 		<script src="../../lib/classList/classList.js"></script>
 

lib/spark-md5/LICENSE
@@ -1,0 +1,13 @@
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+                    Version 2, December 2004
+
+ Copyright (C) 2015 André Cruz <amdfcruz@gmail.com>
+
+ Everyone is permitted to copy and distribute verbatim or modified
+ copies of this license document, and changing it is allowed as long
+ as the name is changed.
+
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+  0. You just DO WHAT THE FUCK YOU WANT TO.

lib/spark-md5/LICENSE2
@@ -1,0 +1,7 @@
+Copyright (c) 2015 André Cruz <amdfcruz@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.

commit cc3e6193001d7a314e5702329a2c5e0980e8c256

ElementLinearData: when removing original dom elements, remap hashes
Failure to do this breaks later assumptions that VE makes when action on
annotations, specifically that annotation objects fetched from the store will
always hash-by-value back into it. This manifested as not being able to toggle
annotations converted from pasted content.

This has been broken since 14b5fbc3 / T142943, which switched
originalDomElements to using the IV store.

Bug: T154123
Change-Id: I6a22de2d6b39fa45d57d1dbcedc5cc8aac6b893d

src/dm/lineardata/ve.dm.ElementLinearData.js
@@ -1064,6 +1064,31 @@ ve.dm.ElementLinearData.prototype.remapInternalListKeys = function ( internalLis
 };
 
 /**
+ * Remap an annotation index when it changes
+ *
+ * @param  {string} oldIndex Old index to replace
+ * @param  {string} newIndex New index to replace it with
+ */
+ve.dm.ElementLinearData.prototype.remapAnnotationIndex = function ( oldIndex, newIndex ) {
+	var i, ilen, attrIndex;
+	for ( i = 0, ilen = this.data.length; i < ilen; i++ ) {
+		if ( this.data[ i ] === undefined || typeof this.data[ i ] === 'string' ) {
+			// common case, cheap, avoid the isArray check
+			continue;
+		} else if ( Array.isArray( this.data[ i ] ) ) {
+			attrIndex = this.data[ i ][ 1 ].indexOf( oldIndex );
+			if ( attrIndex !== -1 ) {
+				if ( this.data[ i ][ 1 ].indexOf( newIndex ) === -1 ) {
+					this.data[ i ][ 1 ].splice( attrIndex, 1, newIndex );
+				} else {
+					this.data[ i ][ 1 ].splice( attrIndex, 1, newIndex );
+				}
+			}
+		}
+	}
+};
+
+/**
  * Sanitize data according to a set of rules.
  *
  * @param {Object} rules Sanitization rules
@@ -1077,7 +1102,7 @@ ve.dm.ElementLinearData.prototype.remapInternalListKeys = function ( internalLis
  * @param {boolean} [rules.keepEmptyContentBranches] Preserve empty content branch nodes
  */
 ve.dm.ElementLinearData.prototype.sanitize = function ( rules ) {
-	var i, len, annotations, emptySet, setToRemove, type,
+	var i, len, annotations, emptySet, setToRemove, type, oldHash, newHash,
 		canContainContent, contentElement, isOpen, nodeClass, ann,
 		elementStack = [],
 		store = this.getStore(),
@@ -1091,7 +1116,23 @@ ve.dm.ElementLinearData.prototype.sanitize = function ( rules ) {
 			for ( i = 0, len = allAnnotations.getLength(); i < len; i++ ) {
 				ann = allAnnotations.get( i );
 				if ( ann.element.originalDomElementsIndex !== undefined ) {
+					// This changes the hash of the value, so we have to
+					// update that. If we don't do this, other assumptions
+					// that values fetched from the store are actually in the
+					// store will fail.
+					oldHash = store.indexOfValue( ann );
 					delete allAnnotations.get( i ).element.originalDomElementsIndex;
+					newHash = store.replaceHash( oldHash, ann );
+					this.remapAnnotationIndex( oldHash, newHash );
+					if ( allAnnotations.storeIndexes.indexOf( newHash ) !== -1 ) {
+						// New annotation-value was already in the set, which
+						// just reduces the effective-length of the set.
+						allAnnotations.storeIndexes.splice( i, 1 );
+						i--;
+						len--;
+					} else {
+						allAnnotations.storeIndexes.splice( i, 1, newHash );
+					}
 				}
 			}
 		}

src/dm/ve.dm.IndexValueStore.js
@@ -108,6 +108,32 @@ ve.dm.IndexValueStore.prototype.index = function ( value, stringified ) {
 	return hash;
 };
 
+ /**
+ * Replace a value's stored hash, e.g. if the value has changed and you want to discard the old one.
+ *
+ * @param {string} oldHash The value's previously stored hash
+ * @param {Object|string|Array} value New value
+ * @throws {Error} Old hash not found
+ * @return {string} New hash
+ */
+ve.dm.IndexValueStore.prototype.replaceHash = function ( oldHash, value ) {
+	var newHash = this.indexOfValue( value ),
+		index = this.hashStore[ oldHash ];
+
+	if ( index === undefined ) {
+		throw new Error( 'Old hash not found: ' + oldHash );
+	}
+
+	delete this.hashStore[ oldHash ];
+
+	if ( this.hashStore[ newHash ] === undefined ) {
+		this.hashStore[ newHash ] = value;
+		this.hashes.splice( this.hashes.indexOf( oldHash ), 1, newHash );
+	}
+
+	return newHash;
+};
+
 /**
  * Get the hash of a value without inserting it in the store
  *

tests/dm/lineardata/ve.dm.ElementLinearData.test.js
@@ -1545,12 +1545,12 @@ QUnit.test( 'sanitize', function ( assert ) {
 				html: '<p style="text-shadow: 0 0 1px #000;">F<b style="color:blue;">o</b>o</p>',
 				data: [
 					{ type: 'paragraph' },
-					'F', [ 'o', [ 'h5dd47c3facf7b6a8' ] ], 'o',
+					'F', [ 'o', [ 'h49981eab0f8056ff' ] ], 'o',
 					{ type: '/paragraph' },
 					{ type: 'internalList' },
 					{ type: '/internalList' }
 				],
-				store: { h5dd47c3facf7b6a8: bold },
+				store: { h49981eab0f8056ff: bold },
 				rules: { removeOriginalDomElements: true },
 				msg: 'Original DOM elements removed'
 			},