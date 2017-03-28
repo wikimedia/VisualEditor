495cd7d654397285841e168f3bc9ddff13969b29
Esanders <esanders@wikimedia.org>
2017-03-28 11:40:30 +0000

    Re-apply "Remove $returnFocusTo hack now null is supported upstream"
    $returnFocusTo is now actually supported upstream
    
    This reverts commit 056366b22e979a4a6bd5ef175c9010a3f805f7d7.
    
    Change-Id: Id52d7d4245b5c332782dc166f54deb33bc36fd4c

--- src/ui/widgets/ve.ui.LanguageInputWidget.js
+++ src/ui/widgets/ve.ui.LanguageInputWidget.js
@@ -122,10 +122,8 @@ OO.inheritClass( ve.ui.LanguageInputWidget, OO.ui.Widget );
  * Handle find language button click events.
  */
 ve.ui.LanguageInputWidget.prototype.onFindLanguageButtonClick = function () {
-	var widget = this,
-		// HACK: Allow $returnFocusTo to take null upstream
-		$noFocus = [ { focus: function () {} } ];
-	this.dialogs.openWindow( 'languageSearch', { availableLanguages: this.availableLanguages, $returnFocusTo: $noFocus } )
+	var widget = this;
+	this.dialogs.openWindow( 'languageSearch', { availableLanguages: this.availableLanguages, $returnFocusTo: null } )
 		.then( function ( opened ) {
 			opened.then( function ( closing ) {
 				closing.then( function ( data ) {
