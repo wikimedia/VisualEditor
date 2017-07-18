ca5fddf9bd827dce5af716d8a530bf5fe53aa0de
James D. Forrester <jforrester@wikimedia.org>
2017-07-18 12:17:26 -0700

    tests: Move QUnit fiddles to VE-core-only file
    Change-Id: I3f214fb1d5d78a82b367cdb6b896a1020b00685e

---
+++ tests/ve.qunit.local.js
@@ -0,0 +1,35 @@
+/*!
+ * VisualEditor core-only plugin for QUnit.
+ *
+ * @copyright 2011-2017 VisualEditor Team and others; see http://ve.mit-license.org
+ */
+
+// Extend QUnit.module to provide a fixture element. This used to be in tests/index.html, but
+// dynamic test runners like Karma build their own web page.
+( function ( QUnit ) {
+	var orgModule = QUnit.module;
+
+	QUnit.dump.maxDepth = 10;
+
+	QUnit.module = function ( name, localEnv ) {
+		localEnv = localEnv || {};
+		orgModule( name, {
+			beforeEach: function () {
+				this.fixture = document.createElement( 'div' );
+				this.fixture.id = 'qunit-fixture';
+				document.body.appendChild( this.fixture );
+
+				if ( localEnv.beforeEach ) {
+					localEnv.beforeEach.call( this );
+				}
+			},
+			afterEach: function () {
+				if ( localEnv.afterEach ) {
+					localEnv.afterEach.call( this );
+				}
+
+				this.fixture.parentNode.removeChild( this.fixture );
+			}
+		} );
+	};
+}( QUnit ) );
