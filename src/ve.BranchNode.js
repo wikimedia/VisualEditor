barionleg/visualegitor

Notification for DM node attached/detached
Change-Id: I2d42f0e3345c49417d2ec8c9d1b291f10175d938

src/ve.BranchNode.js
@@ -92,7 +92,7 @@ ve.BranchNode.prototype.setRoot = function ( root ) {
 		return;
 	}
 	if ( oldRoot ) {
-		// Null the root, then recurse into children, then emit root/unroot.
+		// Null the root, then recurse into children, then emit unroot.
 		// That way, at emit time, all this node's ancestors and descendants have
 		// null root.
 		this.root = null;
@@ -103,7 +103,7 @@ ve.BranchNode.prototype.setRoot = function ( root ) {
 	}
 	this.root = root;
 	if ( root ) {
-		// We've set the new root, so recurse into children, then emit root/unroot.
+		// We've set the new root, so recurse into children, then emit root.
 		// That way, at emit time, all this node's ancestors and descendants have
 		// the new root.
 		for ( i = 0, len = this.children.length; i < len; i++ ) {
@@ -121,14 +121,31 @@ ve.BranchNode.prototype.setRoot = function ( root ) {
  * @param {ve.Document} doc Document this node is a part of
  */
 ve.BranchNode.prototype.setDocument = function ( doc ) {
-	var i;
+	var i, len,
+		oldDoc = this.doc;
 	if ( doc === this.doc ) {
 		// Nothing to do, don't recurse into all descendants
 		return;
 	}
+	if ( oldDoc ) {
+		// Null the doc, then recurse into children, then notify the doc.
+		// That way, at notify time, all this node's ancestors and descendants have
+		// null doc.
+		this.doc = null;
+		for ( i = 0, len = this.children.length; i < len; i++ ) {
+			this.children[ i ].setDocument( null );
+		}
+		oldDoc.onNodeDetached( this );
+	}
 	this.doc = doc;
-	for ( i = 0; i < this.children.length; i++ ) {
-		this.children[ i ].setDocument( doc );
+	if ( doc ) {
+		// We've set the new doc, so recurse into children, then notify the doc.
+		// That way, at notify time, all this node's ancestors and descendants have
+		// the new root.
+		for ( i = 0, len = this.children.length; i < len; i++ ) {
+			this.children[ i ].setDocument( doc );
+		}
+		doc.onNodeAttached( this );
 	}
 };
 

src/ve.Document.js
@@ -26,6 +26,26 @@ ve.Document = function VeDocument( documentNode ) {
 
 OO.mixinClass( ve.Document, OO.EventEmitter );
 
+/* Events */
+
+/**
+ * A node has been attached with ve.Node#setDocument . Its descendents are guaranteed
+ * to be attached too (and the event is emitted for descendents first, and for siblings
+ * in their order in the children list)
+ *
+ * @event nodeAttached
+ * @param {ve.Node} node The node that has been attached
+ */
+
+/**
+ * A node has been detached with ve.Node#setDocument . Its descendents are guaranteed
+ * to be detached too (and the event is emitted for descendents first, and for siblings
+ * in their order in the children list)
+ *
+ * @event nodeDetached
+ * @param {ve.Node} node The node that has been detached
+ */
+
 /* Methods */
 
 /**
@@ -562,4 +582,28 @@ ve.Document.prototype.getCoveredSiblingGroups = function ( range ) {
 ve.Document.prototype.rangeInsideOneLeafNode = function ( range ) {
 	var selected = this.selectNodes( range, 'leaves' );
 	return selected.length === 1 && selected[ 0 ].nodeRange.containsRange( range ) && selected[ 0 ].indexInNode === undefined;
+};
+
+/**
+ * Callback when a node is attached with ve.Node#setDocument
+ *
+ * The node and all its children are guaranteed to be attached
+ *
+ * @method
+ * @param {ve.Node} node The node attached
+ */
+ve.Document.prototype.onNodeAttached = function ( node ) {
+	this.emit( 'nodeAttached', node );
+};
+
+/**
+ * Callback when a node is attached with ve.Node#setDocument
+ *
+ * The node and all its children are guaranteed to be attached
+ *
+ * @method
+ * @param {ve.Node} node The node detached
+ */
+ve.Document.prototype.onNodeDetached = function ( node ) {
+	this.emit( 'nodeDetached', node );
 };

src/ve.Node.js
@@ -351,10 +351,21 @@ ve.Node.prototype.getDocument = function () {
  * This method is overridden by nodes with children.
  *
  * @method
- * @param {ve.Document} doc Document this node is a part of
+ * @param {ve.Document|null} doc Document this node is a part of
  */
 ve.Node.prototype.setDocument = function ( doc ) {
+	var oldDoc = this.doc;
+	if ( doc === oldDoc ) {
+		return;
+	}
+	if ( oldDoc ) {
+		this.doc = null;
+		oldDoc.onNodeDetached( this );
+	}
 	this.doc = doc;
+	if ( doc ) {
+		doc.onNodeAttached( this );
+	}
 };
 
 /**

wikimedia/VisualEditor

Bug: T162761

src/ve.BranchNode.js
@@ -92,7 +92,7 @@ ve.BranchNode.prototype.setRoot = function ( root ) {
 		return;
 	}
 	if ( oldRoot ) {
-		// Null the root, then recurse into children, then emit root/unroot.
+		// Null the root, then recurse into children, then emit unroot.
 		// That way, at emit time, all this node's ancestors and descendants have
 		// null root.
 		this.root = null;
@@ -103,7 +103,7 @@ ve.BranchNode.prototype.setRoot = function ( root ) {
 	}
 	this.root = root;
 	if ( root ) {
-		// We've set the new root, so recurse into children, then emit root/unroot.
+		// We've set the new root, so recurse into children, then emit root.
 		// That way, at emit time, all this node's ancestors and descendants have
 		// the new root.
 		for ( i = 0, len = this.children.length; i < len; i++ ) {
@@ -121,14 +121,31 @@ ve.BranchNode.prototype.setRoot = function ( root ) {
  * @param {ve.Document} doc Document this node is a part of
  */
 ve.BranchNode.prototype.setDocument = function ( doc ) {
-	var i;
+	var i, len,
+		oldDoc = this.doc;
 	if ( doc === this.doc ) {
 		// Nothing to do, don't recurse into all descendants
 		return;
 	}
+	if ( oldDoc ) {
+		// Null the doc, then recurse into children, then notify the doc.
+		// That way, at notify time, all this node's ancestors and descendants have
+		// null doc.
+		this.doc = null;
+		for ( i = 0, len = this.children.length; i < len; i++ ) {
+			this.children[ i ].setDocument( null );
+		}
+		oldDoc.nodeDetached( this );
+	}
 	this.doc = doc;
-	for ( i = 0; i < this.children.length; i++ ) {
-		this.children[ i ].setDocument( doc );
+	if ( doc ) {
+		// We've set the new doc, so recurse into children, then notify the doc.
+		// That way, at notify time, all this node's ancestors and descendants have
+		// the new root.
+		for ( i = 0, len = this.children.length; i < len; i++ ) {
+			this.children[ i ].setDocument( doc );
+		}
+		doc.nodeAttached( this );
 	}
 };
 

src/ve.Document.js
@@ -26,6 +26,26 @@ ve.Document = function VeDocument( documentNode ) {
 
 OO.mixinClass( ve.Document, OO.EventEmitter );
 
+/* Events */
+
+/**
+ * A node has been attached with ve.Node#setDocument . Its descendants are guaranteed
+ * to be attached too (and the event is emitted for descendants first, and for siblings
+ * in their order in the children list)
+ *
+ * @event nodeAttached
+ * @param {ve.Node} node The node that has been attached
+ */
+
+/**
+ * A node has been detached with ve.Node#setDocument . Its descendants are guaranteed
+ * to be detached too (and the event is emitted for descendants first, and for siblings
+ * in their order in the children list)
+ *
+ * @event nodeDetached
+ * @param {ve.Node} node The node that has been detached
+ */
+
 /* Methods */
 
 /**
@@ -562,4 +582,28 @@ ve.Document.prototype.getCoveredSiblingGroups = function ( range ) {
 ve.Document.prototype.rangeInsideOneLeafNode = function ( range ) {
 	var selected = this.selectNodes( range, 'leaves' );
 	return selected.length === 1 && selected[ 0 ].nodeRange.containsRange( range ) && selected[ 0 ].indexInNode === undefined;
+};
+
+/**
+ * Callback when a node is attached with ve.Node#setDocument
+ *
+ * The node and all its children are guaranteed to be attached
+ *
+ * @method
+ * @param {ve.Node} node The node attached
+ */
+ve.Document.prototype.nodeAttached = function ( node ) {
+	this.emit( 'nodeAttached', node );
+};
+
+/**
+ * Callback when a node is attached with ve.Node#setDocument
+ *
+ * The node and all its children are guaranteed to be attached
+ *
+ * @method
+ * @param {ve.Node} node The node detached
+ */
+ve.Document.prototype.nodeDetached = function ( node ) {
+	this.emit( 'nodeDetached', node );
 };

src/ve.Node.js
@@ -351,10 +351,21 @@ ve.Node.prototype.getDocument = function () {
  * This method is overridden by nodes with children.
  *
  * @method
- * @param {ve.Document} doc Document this node is a part of
+ * @param {ve.Document|null} doc Document this node is a part of
  */
 ve.Node.prototype.setDocument = function ( doc ) {
+	var oldDoc = this.doc;
+	if ( doc === oldDoc ) {
+		return;
+	}
+	if ( oldDoc ) {
+		this.doc = null;
+		oldDoc.nodeDetached( this );
+	}
 	this.doc = doc;
+	if ( doc ) {
+		doc.nodeAttached( this );
+	}
 };
 
 /**