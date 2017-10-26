81a88b4f106c67c80950b4c27e05f9ea6ec6a89c
Kunal Mehta <legoktm@member.fsf.org>
2017-10-25 18:54:33 -0700

    build: Replace JSON code coverage report with clover.xml one
    Bug: T179051
    Change-Id: Iaa4883d202b2afc53d909c978435833695497497

--- Gruntfile.js
+++ Gruntfile.js
@@ -324,7 +324,7 @@ module.exports = function ( grunt ) {
 					dir: 'coverage/',
 					subdir: '.',
 					reporters: [
-						{ type: 'json-summary', file: 'coverage-summary.json' },
+						{ type: 'clover' },
 						{ type: 'html' },
 						{ type: 'text-summary' }
 					],
