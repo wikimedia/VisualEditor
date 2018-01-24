5447d33e4125b3c66010116ad931b9bdde6cca08
Ed Sanders <esanders@wikimedia.org>
2018-01-23 20:42:04 -0800

    CommentNode: Protect against call after teardown
    Change-Id: I276cc64e522556f482f9fc7d0bb43f32d521defa

--- src/ce/nodes/ve.ce.CommentNode.js
+++ src/ce/nodes/ve.ce.CommentNode.js
@@ -95,7 +95,13 @@ ve.ce.CommentNode.prototype.onAttributeChange = function ( key, from, to ) {
  * @inheritdoc
  */
 ve.ce.CommentNode.prototype.createInvisibleIcon = function () {
-	var icon = new OO.ui.ButtonWidget( {
+	var icon;
+	// Check the node hasn't been destroyed, as this method is
+	// called after an rAF in ve.ce.FocusableNode
+	if ( !this.getModel() ) {
+		return;
+	}
+	icon = new OO.ui.ButtonWidget( {
 		classes: [ 've-ce-focusableNode-invisibleIcon' ],
 		framed: false,
 		icon: this.constructor.static.iconWhenInvisible,
