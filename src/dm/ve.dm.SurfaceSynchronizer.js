7ddd588f56c70cd7920a75b8ea24888c3a25b4d2
Ed Sanders <esanders@wikimedia.org>
2018-01-30 14:37:14 +0000

    SurfaceSynchronizer: Use palette for author colour
    Change-Id: I22a615300cbda2f6d2f8cf4bfa434e43b4382b22

--- src/dm/ve.dm.SurfaceSynchronizer.js
+++ src/dm/ve.dm.SurfaceSynchronizer.js
@@ -86,9 +86,14 @@ OO.mixinClass( ve.dm.SurfaceSynchronizer, ve.dm.RebaseClient );
  * @return {string} Color, RRGGBB
  */
 ve.dm.SurfaceSynchronizer.static.getAuthorColor = function ( authorId ) {
-	return ( 8 * ( 1 - Math.sin( 5 * authorId ) ) ).toString( 16 ).slice( 0, 1 ) +
-		( 6 * ( 1 - Math.cos( 3 * authorId ) ) ).toString( 16 ).slice( 0, 1 ) +
-		'0';
+	var palette = [
+		'1f77b4', 'ff7f0e', '2ca02c', 'd62728', '9467bd',
+		'8c564b', 'e377c2', '7f7f7f', 'bcbd22', '17becf',
+		'aec7e8', 'ffbb78', '98df8a', 'ff9896', 'c5b0d5',
+		'c49c94', 'f7b6d2', 'c7c7c7', 'dbdb8d', '9edae5'
+	];
+
+	return palette[ authorId % palette.length ];
 };
 
 /* Methods */
