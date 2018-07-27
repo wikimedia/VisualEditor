barionleg/visualegitor

Make mode for getting document data without metadata
Allow cloneFromRange to accept this new mode, and make
the doc clones in the visual diff use it, so that
metadata can be ignored in the diff (and later be diffed
separately).

Bug: T199298
Change-Id: Iea426be573f07c976b0c142357d5ed83e51db013

src/dm/ve.dm.VisualDiff.js
@@ -18,8 +18,8 @@
  * @param {Number} [timeout=1000] Timeout after which to stop performing linear diffs (in ms)
  */
 ve.dm.VisualDiff = function VeDmVisualDiff( oldDoc, newDoc, timeout ) {
-	this.oldDoc = oldDoc.cloneFromRange( undefined, true );
-	this.newDoc = newDoc.cloneFromRange( undefined, true );
+	this.oldDoc = oldDoc.cloneFromRange( undefined, true, 'omitMetadata' );
+	this.newDoc = newDoc.cloneFromRange( undefined, true, 'omitMetadata' );
 	this.oldDocNode = this.oldDoc.getDocumentNode();
 	this.newDocNode = this.newDoc.getDocumentNode();
 	this.oldDocChildren = this.getDocChildren( this.oldDocNode );

wikimedia/VisualEditor

src/dm/ve.dm.VisualDiff.js
@@ -18,8 +18,8 @@
  * @param {Number} [timeout=1000] Timeout after which to stop performing linear diffs (in ms)
  */
 ve.dm.VisualDiff = function VeDmVisualDiff( oldDoc, newDoc, timeout ) {
-	this.oldDoc = oldDoc.cloneFromRange( undefined, true );
-	this.newDoc = newDoc.cloneFromRange( undefined, true );
+	this.oldDoc = oldDoc.cloneFromRange( undefined, true, 'noMetadata' );
+	this.newDoc = newDoc.cloneFromRange( undefined, true, 'noMetadata' );
 	this.oldDocNode = this.oldDoc.getDocumentNode();
 	this.newDocNode = this.newDoc.getDocumentNode();
 	this.oldDocChildren = this.getDocChildren( this.oldDocNode );