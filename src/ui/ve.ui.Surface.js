8bb1eb5982e09270f30a263174f87eaac5a93282
Ed Sanders <esanders@wikimedia.org>
2018-09-13 12:50:26 +0100

    Scroll 60px past toolbar on iOS too to avoid context menu clash
    This causes the selection context to be hidden, so we have to decide
    which is the lesser of two evils.
    
    Bug: T202723
    Change-Id: I95eb5f570d218c37f58cb7a0a531c7f617450200

--- src/ui/ve.ui.Surface.js
+++ src/ui/ve.ui.Surface.js
@@ -515,21 +515,25 @@ ve.ui.Surface.prototype.scrollCursorIntoView = function () {
 	bottomBound = window.innerHeight; // bottom of the window
 	if (
 		OO.ui.isMobile() &&
-		// Scrolling on iOS closes the context
-		!ve.init.platform.constructor.static.isIos() &&
 		!this.getModel().getSelection().isCollapsed()
 	) {
 		profile = $.client.profile();
-		if ( profile.name === 'android' && profile.versionNumber >= 6 ) {
-			// Assume that if the selection has been expanded, then a context menu is visible
-			// above the selection. We don't want this to obscure the toolbar so add on an
-			// estimate of its height. (T202723)
+		// Assume that if the selection has been expanded, then a context menu is visible
+		// above the selection. We don't want this to obscure the toolbar so add on an
+		// estimate of its height. Note that scrolling on iOS closes the context, but it
+		// will re-open when the user touches the selection. (T202723)
+		if (
+			ve.init.platform.constructor.static.isIos() ||
 			// Older versions of Android draw the context menu in the address bar and so
 			// don't need to be fixed.
+			( profile.name === 'android' && profile.versionNumber >= 6 )
+		) {
 			topBound += 60;
 		}
 		// Also assume there are selection handles below on Android. (T204718)
-		bottomBound -= 30;
+		if ( profile.name === 'android' ) {
+			bottomBound -= 30;
+		}
 	}
 
 	cursorTop = clientRect.top - 5;
