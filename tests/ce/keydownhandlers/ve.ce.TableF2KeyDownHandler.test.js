d89186159dbdd7067717a40dcf1fa02a54486aee
Bjornskjald <github@bjorn.ml>
2018-10-24 18:25:25 +0200

    Add F2 keydown handler
    Bug: T172042
    Change-Id: I5eb8c4ba2c5ec8f312f188c50725f8847c0c26da

---
+++ tests/ce/keydownhandlers/ve.ce.TableF2KeyDownHandler.test.js
@@ -0,0 +1,42 @@
+/*!
+ * VisualEditor ContentEditable table f2 key down handler tests.
+ *
+ * @copyright 2011-2018 VisualEditor Team and others; see http://ve.mit-license.org
+ */
+
+QUnit.module( 've.ce.TableF2KeyDownHandler', {
+	// See https://github.com/platinumazure/eslint-plugin-qunit/issues/68
+	// eslint-disable-next-line qunit/resolve-async
+	beforeEach: function ( assert ) {
+		var done = assert.async();
+		return ve.init.platform.getInitializedPromise().then( done );
+	}
+} );
+
+QUnit.test( 'special key down: table f2', function ( assert ) {
+	var done = assert.async(),
+		promise = Promise.resolve(),
+		mergedCellsDoc = ve.dm.example.createExampleDocument( 'mergedCells' ),
+		cases = [
+			{
+				htmlOrDoc: mergedCellsDoc,
+				rangeOrSelection: {
+					type: 'table',
+					tableRange: new ve.Range( 0, 171 ),
+					fromCol: 1,
+					fromRow: 0
+				},
+				keys: [ 'F2' ],
+				expectedRangeOrSelection: new ve.Range( 11 ),
+				msg: 'Press F2 to edit a table cell'
+			}
+		];
+
+	cases.forEach( function ( caseItem ) {
+		promise = promise.then( function () {
+			return ve.test.utils.runSurfaceHandleSpecialKeyTest( assert, caseItem );
+		} );
+	} );
+
+	promise.finally( function () { done(); } );
+} );
