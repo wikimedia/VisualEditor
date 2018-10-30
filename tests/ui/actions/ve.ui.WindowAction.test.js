4c1cc16404b8f1ac08779e41fd75f483e180f4ad
Ed Sanders <esanders@wikimedia.org>
2018-10-30 21:36:56 +0000

    Basic test for WindowAction open/close
    Change-Id: I32c7ea14a61941d95ffeab944d14c65b20bc42a5

---
+++ tests/ui/actions/ve.ui.WindowAction.test.js
@@ -0,0 +1,32 @@
+/*!
+ * VisualEditor UserInterface Actions WindowAction tests.
+ *
+ * @copyright 2011-2018 VisualEditor Team and others; see http://ve.mit-license.org
+ */
+
+QUnit.module( 've.ui.WindowAction' );
+
+/* Tests */
+
+QUnit.test( 'Basic open/close', function ( assert ) {
+	var surface = ve.test.utils.createSurfaceFromHtml( '' ),
+		windowAction = ve.ui.actionFactory.create( 'window', surface ),
+		dialogs = surface.getDialogs(),
+		done = assert.async( 2 );
+
+	dialogs.once( 'opening', function ( win, opening ) {
+		opening.then( function () {
+			assert.ok( true, 'Window opened' );
+
+			windowAction.close( 'message' );
+			done();
+		} );
+	} );
+
+	dialogs.once( 'closing', function () {
+		assert.ok( true, 'Window closed' );
+		done();
+	} );
+
+	windowAction.open( 'message' );
+} );
