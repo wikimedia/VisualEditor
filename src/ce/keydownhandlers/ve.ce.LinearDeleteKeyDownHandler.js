barionleg/visualegitor

Support only surfacing part of the document
Bug: T76541
Change-Id: I227a0d704b9b337cff2102d424be9795d6362ed7

src/ce/keydownhandlers/ve.ce.LinearDeleteKeyDownHandler.js
@@ -232,7 +232,7 @@ ve.ce.LinearDeleteKeyDownHandler.static.execute = function ( surface, e ) {
 				!startNode.isUnwrappable() ||
 				// Content item at the start / end?
 				(
-					( startNode.canContainContent() || documentModel.getDocumentNode() === startNode ) &&
+					( startNode.canContainContent() || surface.root === startNode ) &&
 					( nodeRange.start === 0 || nodeRange.end === docLength )
 				)
 			) {

src/ce/ve.ce.SurfaceObserver.js
@@ -15,8 +15,7 @@
 ve.ce.SurfaceObserver = function VeCeSurfaceObserver( surface ) {
 	// Properties
 	this.surface = surface;
-	this.documentView = surface.getDocument();
-	this.domDocument = this.documentView.getDocumentNode().getElementDocument();
+	this.domDocument = surface.root.getElementDocument();
 	this.polling = false;
 	this.disabled = false;
 	this.timeoutId = null;
@@ -46,7 +45,6 @@ ve.ce.SurfaceObserver.prototype.clear = function () {
  */
 ve.ce.SurfaceObserver.prototype.detach = function () {
 	this.surface = null;
-	this.documentView = null;
 	this.domDocument = null;
 	this.rangeState = null;
 };
@@ -159,7 +157,7 @@ ve.ce.SurfaceObserver.prototype.pollOnceInternal = function ( signalChanges, sel
 	oldState = this.rangeState;
 	newState = new ve.ce.RangeState(
 		oldState,
-		this.documentView.getDocumentNode(),
+		this.surface.root,
 		selectionOnly
 	);
 	this.rangeState = newState;

wikimedia/VisualEditor

src/ce/keydownhandlers/ve.ce.LinearDeleteKeyDownHandler.js
@@ -232,7 +232,7 @@ ve.ce.LinearDeleteKeyDownHandler.static.execute = function ( surface, e ) {
 				!startNode.isUnwrappable() ||
 				// Content item at the start / end?
 				(
-					( startNode.canContainContent() || documentModel.getDocumentNode() === startNode ) &&
+					( startNode.canContainContent() || surface.attachedRoot === startNode ) &&
 					( nodeRange.start === 0 || nodeRange.end === docLength )
 				)
 			) {

src/ce/ve.ce.SurfaceObserver.js
@@ -15,8 +15,7 @@
 ve.ce.SurfaceObserver = function VeCeSurfaceObserver( surface ) {
 	// Properties
 	this.surface = surface;
-	this.documentView = surface.getDocument();
-	this.domDocument = this.documentView.getDocumentNode().getElementDocument();
+	this.domDocument = surface.attachedRoot.getElementDocument();
 	this.polling = false;
 	this.disabled = false;
 	this.timeoutId = null;
@@ -46,7 +45,6 @@ ve.ce.SurfaceObserver.prototype.clear = function () {
  */
 ve.ce.SurfaceObserver.prototype.detach = function () {
 	this.surface = null;
-	this.documentView = null;
 	this.domDocument = null;
 	this.rangeState = null;
 };
@@ -159,7 +157,7 @@ ve.ce.SurfaceObserver.prototype.pollOnceInternal = function ( signalChanges, sel
 	oldState = this.rangeState;
 	newState = new ve.ce.RangeState(
 		oldState,
-		this.documentView.getDocumentNode(),
+		this.surface.attachedRoot,
 		selectionOnly
 	);
 	this.rangeState = newState;