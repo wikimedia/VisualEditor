barionleg/visualegitor

Change#squash: Compact multiple transactions into one
This supersedes the more timid 22d6feea3ecf3f124e447f5c83ee4c310ddff56a .

Bug: T188026
Change-Id: I3516357027b8aa96081dde1f97b2d17891b03c89

src/ui/ve.ui.DebugBar.js
@@ -423,7 +423,7 @@ ve.ui.DebugBar.prototype.testSquasher = function () {
 		transactions = this.getSurface().getModel().getDocument().completeHistory.transactions;
 
 	function squashTransactions( transactions ) {
-		var change = new ve.dm.Change(
+		return new ve.dm.Change(
 			0,
 			transactions.map( function ( tx ) {
 				return tx.clone();
@@ -432,9 +432,7 @@ ve.ui.DebugBar.prototype.testSquasher = function () {
 				return new ve.dm.HashValueStore();
 			} ),
 			{}
-		);
-		change.squash();
-		return change.transactions;
+		).squashed().transactions;
 	}
 	if ( transactions.length < 3 ) {
 		// Nothing interesting here

wikimedia/VisualEditor

src/ui/ve.ui.DebugBar.js
@@ -425,7 +425,7 @@ ve.ui.DebugBar.prototype.testSquasher = function () {
 		transactions = this.getSurface().getModel().getDocument().completeHistory.transactions;
 
 	function squashTransactions( transactions ) {
-		var change = new ve.dm.Change(
+		return new ve.dm.Change(
 			0,
 			transactions.map( function ( tx ) {
 				return tx.clone();
@@ -434,9 +434,7 @@ ve.ui.DebugBar.prototype.testSquasher = function () {
 				return new ve.dm.HashValueStore();
 			} ),
 			{}
-		);
-		change.squash();
-		return change.transactions;
+		).squash().transactions;
 	}
 	if ( transactions.length < 3 ) {
 		// Nothing interesting here