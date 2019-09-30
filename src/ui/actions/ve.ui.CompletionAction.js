fb4f0a83b34c65ea423ee9c12539d23348284550
David Lynch <dlynch@wikimedia.org>
2019-09-30 06:40:07 -0500

    Completion framework
    Bug: T232601
    Change-Id: If6aee9df67e7a1234d47c0ba0c2f05ef47e5bd51

---
+++ src/ui/actions/ve.ui.CompletionAction.js
@@ -0,0 +1,158 @@
+/*!
+ * VisualEditor UserInterface CompletionAction class.
+ *
+ * @copyright 2011-2019 VisualEditor Team and others; see http://ve.mit-license.org
+ */
+
+/**
+ * Completion action.
+ *
+ * This is a very specialized action, which mostly exists to be a data-provider to a
+ * completion widget. Completion-types *must* override getSuggestions.
+ *
+ * @class
+ * @abstract
+ * @extends ve.ui.Action
+ *
+ * @constructor
+ * @param {ve.ui.Surface} surface Surface to act on
+ */
+ve.ui.CompletionAction = function VeUiCompletionAction() {
+	// Parent constructor
+	ve.ui.CompletionAction.super.apply( this, arguments );
+};
+
+/* Inheritance */
+
+OO.inheritClass( ve.ui.CompletionAction, ve.ui.Action );
+
+/* Static Properties */
+
+/**
+ * Length to which to limit the list of returned completions
+ *
+ * @static
+ * @property
+ */
+ve.ui.CompletionAction.static.defaultLimit = 8;
+
+/**
+ * Length of the trigger sequence for the action
+ *
+ * This many characters will be stripped from the start of the current input by
+ * CompletionWidget.
+ *
+ * @static
+ * @property
+ */
+ve.ui.CompletionAction.static.triggerLength = 1;
+
+/**
+ * Whether the current input should be included as a completion automatically
+ *
+ * @static
+ * @property
+ */
+ve.ui.CompletionAction.static.alwaysIncludeInput = true;
+
+/**
+ * List of allowed methods for this action.
+ *
+ * @static
+ * @property
+ */
+ve.ui.CompletionAction.static.methods = [ 'open' ];
+
+/* Methods */
+
+/**
+ * Show the completions
+ *
+ * @return {boolean} Action was executed
+ */
+ve.ui.CompletionAction.prototype.open = function () {
+	this.surface.completion.setup( this );
+
+	return true;
+};
+
+/**
+ * Retrieve suggested completions for the given input
+ *
+ * @abstract
+ * @param {string} input
+ * @param {number} [limit=20] restrict number of results to this
+ * @return {jQuery.Promise} promise that will return results
+ */
+ve.ui.CompletionAction.prototype.getSuggestions = null;
+
+/**
+ * Perform the insetion for the chosen suggestion
+ *
+ * @param {Object} data Whatever data was attached to the menu option widget
+ * @param {ve.Range} range The range the widget is considering
+ * @return {ve.dm.SurfaceFragment} The fragment containing the inserted content
+ */
+ve.ui.CompletionAction.prototype.insertCompletion = function ( data, range ) {
+	return this.surface.getModel().getLinearFragment( range )
+		.insertContent( data, true );
+};
+
+/**
+ * Should the widget abandon trying to find matches given the current state?
+ *
+ * @param {string} input
+ * @param {number} matches Number of matches before the input occurred
+ * @return {boolean} Whether to abandon
+ */
+ve.ui.CompletionAction.prototype.shouldAbandon = function ( input, matches ) {
+	// abandon after adding whitespace if there are no active potential matches
+	return matches === ( this.constructor.static.alwaysIncludeInput ? 1 : 0 ) &&
+		!!( input[ input.length - 1 ] && input[ input.length - 1 ].match( /\s/ ) );
+};
+
+// helpers
+
+/**
+ * Make a menu item for a given suggestion
+ *
+ * @protected
+ * @param  {string} suggestion
+ * @return {OO.ui.MenuOptionWidget}
+ */
+ve.ui.CompletionAction.prototype.getMenuItemForSuggestion = function ( suggestion ) {
+	return new OO.ui.MenuOptionWidget( { data: suggestion, label: suggestion } );
+};
+
+/**
+ * Filter a suggestion list based on the current input
+ *
+ * This is for an implementor who has fetched/gathered a list of potential
+ * suggestions and needs to trim them down to a viable set to display as
+ * completion options for a given input.
+ *
+ * It restricts the selection to only suggestions that start with the input,
+ * shortens the list to the configured defaultLimit, and adds the current
+ * input to the list if alwaysIncludeInput and there wasn't an exact match.
+ *
+ * @protected
+ * @param  {string[]} suggestions List of strings of valid completions
+ * @param  {string} input Input to filter the suggestions to
+ * @return {string[]}
+ */
+ve.ui.CompletionAction.prototype.filterSuggestionsForInput = function ( suggestions, input ) {
+	var exact = false;
+	input = input.toLowerCase();
+	suggestions = suggestions.filter( function ( word ) {
+		word = word.toLowerCase();
+		exact = exact || word === input;
+		return word.slice( 0, input.length ) === input && word !== input;
+	} );
+	if ( this.constructor.static.defaultLimit < suggestions.length ) {
+		suggestions.length = this.constructor.static.defaultLimit;
+	}
+	if ( !exact && this.constructor.static.alwaysIncludeInput && input.trim().length ) {
+		suggestions.push( input.trim() );
+	}
+	return suggestions;
+};
---
+++ src/ui/widgets/ve.ui.CompletionWidget.js
@@ -0,0 +1,123 @@
+/*!
+ * VisualEditor UserInterface CompletionWidget class.
+ *
+ * @copyright 2011-2019 VisualEditor Team and others; see http://ve.mit-license.org
+ */
+
+/**
+ * Widget that displays autocompletion suggestions
+ *
+ * @class
+ * @extends OO.ui.Widget
+ *
+ * @constructor
+ * @param {ve.ui.Surface} surface Surface to complete into
+ * @param {Object} [config] Configuration options
+ * @cfg {Object} [validate] Validation pattern passed to TextInputWidgets
+ * @cfg {boolean} [readOnly=false] Prevent changes to the value of the widget.
+ */
+ve.ui.CompletionWidget = function VeUiCompletionWidget( surface, config ) {
+	var $doc = surface.getView().getDocument().getDocumentNode().$element;
+
+	this.surface = surface;
+	this.surfaceModel = surface.getModel();
+
+	// Configuration
+	config = config || {
+		anchor: false
+	};
+
+	// Parent constructor
+	ve.ui.CompletionWidget.super.call( this, config );
+
+	this.$tabIndexed = this.$element;
+
+	this.menu = new OO.ui.MenuSelectWidget( {
+		widget: this,
+		$input: $doc,
+		width: 'auto'
+	} );
+
+	// Events
+	this.menu.connect( this, { choose: 'onMenuChoose' } );
+
+	// Setup
+	this.$element.addClass( 've-ui-completionWidget' )
+		.append(
+			this.menu.$element
+		);
+};
+
+/* Inheritance */
+
+OO.inheritClass( ve.ui.CompletionWidget, OO.ui.Widget );
+
+ve.ui.CompletionWidget.prototype.setup = function ( action ) {
+	var offset = this.surfaceModel.getSelection().getRange();
+	if ( !offset.isCollapsed() ) {
+		return;
+	}
+	this.action = action;
+	this.initialOffset = offset.end - this.action.constructor.static.triggerLength;
+
+	this.update();
+
+	this.surfaceModel.connect( this, { select: 'onModelSelect' } );
+};
+
+ve.ui.CompletionWidget.prototype.teardown = function () {
+	this.menu.toggle( false );
+	this.surfaceModel.disconnect( this );
+	this.action = undefined;
+};
+
+ve.ui.CompletionWidget.prototype.update = function () {
+	var direction = this.surface.getDir(),
+		range = this.getCompletionRange(),
+		boundingRect = this.surface.getView().getSelection( new ve.dm.LinearSelection( range ) ).getSelectionBoundingRect(),
+		style = {
+			top: boundingRect.bottom
+		},
+		data = this.surfaceModel.getDocument().data,
+		input = data.getText( false, range );
+
+	if ( direction === 'rtl' ) {
+		style.right = boundingRect.right;
+	} else {
+		style.left = boundingRect.left;
+	}
+	this.$element.css( style );
+
+	this.action.getSuggestions( input ).then( function ( items ) {
+		this.menu.clearItems().addItems( items.map( this.action.getMenuItemForSuggestion.bind( this.action ) ) );
+		if ( this.menu.getItems().length ) {
+			this.menu.highlightItem( this.menu.getItems()[ 0 ] );
+		}
+		this.menu.toggle( true );
+	}.bind( this ) );
+};
+
+ve.ui.CompletionWidget.prototype.onMenuChoose = function ( item ) {
+	var fragment = this.action.insertCompletion( item.getData(), this.getCompletionRange( true ) );
+
+	fragment.collapseToEnd().select();
+
+	this.teardown();
+};
+
+ve.ui.CompletionWidget.prototype.onModelSelect = function () {
+	var range = this.getCompletionRange();
+	if ( !range || range.isBackwards() || this.action.shouldAbandon( this.surfaceModel.getDocument().data.getText( false, range ), this.menu.getItems().length ) ) {
+		this.teardown();
+	} else {
+		this.update();
+	}
+};
+
+ve.ui.CompletionWidget.prototype.getCompletionRange = function ( withTrigger ) {
+	var offset = this.surfaceModel.getSelection().getRange();
+	if ( !offset.isCollapsed() || !this.action ) {
+		return false;
+	}
+	return new ve.Range( this.initialOffset + ( withTrigger ? 0 : this.action.constructor.static.triggerLength ), offset.end );
+};
