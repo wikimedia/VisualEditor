4c2a0ea13d1068ff5f93cc042e0b0ae07383af02
James D. Forrester <jforrester@wikimedia.org>
2021-02-20 10:02:44 -0800

    Revert "Comment out broken test after Chrome upgrade"
    This reverts commit 8c5e78c7f3d90bb79ef3b462f1629880db639c03.
    
    Now that VE and VE-MW both run tests via docker (in the jobs
    visualeditor-npm-browser-node-6-docker for the former and
    wmf-quibble-vendor-mysql-hhvm-docker for the latter), we're
    using the same version of Chrome in both, so this should now
    be fine to land.
    
    Change-Id: I1c34012e66a20754e298654a876a67d04dee18f9

--- tests/ce/ve.ce.Surface.test.js
+++ tests/ce/ve.ce.Surface.test.js
@@ -53,9 +53,11 @@ ve.test.utils.runSurfaceHandleSpecialKeyTest = function ( assert, caseItem ) {
 			// There are no execCommands for CTRL+BACKSPACE/DELETE (delete word)
 			// These enter commands should always be prevented
 			ENTER: 'insertParagraph',
-			'SHIFT+ENTER': 'insertParagraph',
-			'CTRL+ENTER': 'insertParagraph'
+			'SHIFT+ENTER': 'insertParagraph'
 		};
+
+	// Platform-variant
+	execCommands[ ( ve.getSystemPlatform() === 'mac' ? 'CMD' : 'CTRL' ) + '+ENTER' ] = 'insertParagraph';
 
 	if ( caseItem.setup ) {
 		caseItem.setup();
@@ -691,18 +693,12 @@ QUnit.test( 'onCopy', function ( assert ) {
 			expectedHtml: '<span data-ve-clipboard-key="">&nbsp;</span>a<b>b</b><i>c</i>',
 			noClipboardData: true,
 			msg: 'Clipboard span'
-		}
-		/*
-		// Our CI environment uses either Chrome 57 (mediawiki/extensions/VisualEditor)
-		// or Chrome 63 (VisualEditor/VisualEditor), but they produce different results,
-		// so this test will always fail in at least one of them.
+		},
 		{
 			rangeOrSelection: new ve.Range( 0, 61 ),
-			expectedText: 'abc\n\nd\n\ne\n\nf\n\ng\n\nhi\nj\n\nk\n\nl\n\nm\n\n', // Chrome 57
-			expectedText: 'abc\nd\n\ne\n\nf\n\ng\n\nhi\nj\n\nk\n\nl\n\nm\n\n',   // Chrome 63
+			expectedText: 'abc\n\nd\n\ne\n\nf\n\ng\n\nhi\n\nj\n\nk\n\nl\n\nm',
 			msg: 'Plain text of entire document'
 		}
-		*/
 	];
 
 	function testRunner( doc, rangeOrSelection, expectedData, expectedOriginalRange, expectedBalancedRange, expectedHtml, expectedText, noClipboardData, msg ) {
