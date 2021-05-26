beb396899f96fb1f6204bef69ef5c3755e8e1a37
Ed Sanders <esanders@wikimedia.org>
2021-05-26 14:36:35 +0100

    IndentationAction: Refactor case runner
    Change-Id: I40cbb8df9770fa834557d7a2987704df3f14ba7d

--- tests/ui/actions/ve.ui.IndentationAction.test.js
+++ tests/ui/actions/ve.ui.IndentationAction.test.js
@@ -9,64 +9,63 @@ QUnit.module( 've.ui.IndentationAction' );
 /* Tests */
 
 QUnit.test( 'increase/decrease', function ( assert ) {
-	var i,
-		cases = [
-			{
-				rangeOrSelection: new ve.Range( 14, 16 ),
-				method: 'decrease',
-				expectedRangeOrSelection: new ve.Range( 14, 16 ),
-				expectedData: function ( data ) {
-					data.splice( 11, 2, { type: '/list' }, { type: 'paragraph' } );
-					data.splice( 19, 2, { type: '/paragraph' }, { type: 'list', attributes: { style: 'bullet' } } );
-				},
-				expectedOriginalData: function ( data ) {
-					// generated: 'wrapper' is removed by the action and not restored by undo
-					delete data[ 12 ].internal;
-				},
-				undo: true,
-				msg: 'decrease indentation on partial selection of list item "Item 2"'
+	var cases = [
+		{
+			rangeOrSelection: new ve.Range( 14, 16 ),
+			method: 'decrease',
+			expectedRangeOrSelection: new ve.Range( 14, 16 ),
+			expectedData: function ( data ) {
+				data.splice( 11, 2, { type: '/list' }, { type: 'paragraph' } );
+				data.splice( 19, 2, { type: '/paragraph' }, { type: 'list', attributes: { style: 'bullet' } } );
 			},
-			{
-				rangeOrSelection: new ve.Range( 3, 19 ),
-				method: 'decrease',
-				expectedRangeOrSelection: new ve.Range( 1, 15 ),
-				expectedData: function ( data ) {
-					data.splice( 0, 2 );
-					data.splice( 8, 2 );
-					data.splice( 16, 1, { type: 'list', attributes: { style: 'bullet' } } );
-					delete data[ 0 ].internal;
-					delete data[ 8 ].internal;
-				},
-				expectedOriginalData: function ( data ) {
-					// generated: 'wrapper' is removed by the action and not restored by undo
-					delete data[ 2 ].internal;
-					delete data[ 12 ].internal;
-				},
-				undo: true,
-				msg: 'decrease indentation on Items 1 & 2'
+			expectedOriginalData: function ( data ) {
+				// generated: 'wrapper' is removed by the action and not restored by undo
+				delete data[ 12 ].internal;
 			},
-			{
-				rangeOrSelection: new ve.Range( 3, 19 ),
-				method: 'increase',
-				expectedRangeOrSelection: new ve.Range( 5, 21 ),
-				expectedData: function ( data ) {
-					data.splice( 0, 0, { type: 'list', attributes: { style: 'bullet' } }, { type: 'listItem' } );
-					data.splice( 23, 0, { type: '/list' }, { type: '/listItem' } );
-				},
-				undo: true,
-				msg: 'increase indentation on Items 1 & 2'
-			}
-		];
+			undo: true,
+			msg: 'decrease indentation on partial selection of list item "Item 2"'
+		},
+		{
+			rangeOrSelection: new ve.Range( 3, 19 ),
+			method: 'decrease',
+			expectedRangeOrSelection: new ve.Range( 1, 15 ),
+			expectedData: function ( data ) {
+				data.splice( 0, 2 );
+				data.splice( 8, 2 );
+				data.splice( 16, 1, { type: 'list', attributes: { style: 'bullet' } } );
+				delete data[ 0 ].internal;
+				delete data[ 8 ].internal;
+			},
+			expectedOriginalData: function ( data ) {
+				// generated: 'wrapper' is removed by the action and not restored by undo
+				delete data[ 2 ].internal;
+				delete data[ 12 ].internal;
+			},
+			undo: true,
+			msg: 'decrease indentation on Items 1 & 2'
+		},
+		{
+			rangeOrSelection: new ve.Range( 3, 19 ),
+			method: 'increase',
+			expectedRangeOrSelection: new ve.Range( 5, 21 ),
+			expectedData: function ( data ) {
+				data.splice( 0, 0, { type: 'list', attributes: { style: 'bullet' } }, { type: 'listItem' } );
+				data.splice( 23, 0, { type: '/list' }, { type: '/listItem' } );
+			},
+			undo: true,
+			msg: 'increase indentation on Items 1 & 2'
+		}
+	];
 
-	for ( i = 0; i < cases.length; i++ ) {
+	cases.forEach( function ( caseItem ) {
 		ve.test.utils.runActionTest(
-			'indentation', assert, cases[ i ].html || ve.dm.example.isolationHtml, false, cases[ i ].method, [], cases[ i ].rangeOrSelection, cases[ i ].msg,
+			'indentation', assert, caseItem.html || ve.dm.example.isolationHtml, false, caseItem.method, [], caseItem.rangeOrSelection, caseItem.msg,
 			{
-				expectedData: cases[ i ].expectedData,
-				expectedOriginalData: cases[ i ].expectedOriginalData,
-				expectedRangeOrSelection: cases[ i ].expectedRangeOrSelection,
-				undo: cases[ i ].undo
+				expectedData: caseItem.expectedData,
+				expectedOriginalData: caseItem.expectedOriginalData,
+				expectedRangeOrSelection: caseItem.expectedRangeOrSelection,
+				undo: caseItem.undo
 			}
 		);
-	}
+	} );
 } );
