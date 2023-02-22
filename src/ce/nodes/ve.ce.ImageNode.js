3ed672344f99024806d19528661915a6b62cc695
David Chan <david@troi.org>
2023-02-22 20:02:46 +0800

    Put zero-width space after inline focusable nodes
    This stops certain IMEs from reinterpreting ce=false text as part of editable
    candidate text.
    
    Bug: T330284
    Change-Id: I4278a152f236fe04cf1d4ac32a6084334197d12b

--- src/ce/nodes/ve.ce.ImageNode.js
+++ src/ce/nodes/ve.ce.ImageNode.js
@@ -45,7 +45,14 @@ OO.mixinClass( ve.ce.ImageNode, ve.ce.FocusableNode );
 
 OO.mixinClass( ve.ce.ImageNode, ve.ce.ResizableNode );
 
-/* Static Methods */
+/* Static properties */
+
+/*
+ * @inheritdoc ve.ce.FocusableNode
+ */
+ve.ce.ImageNode.static.addBreak = false;
+
+/* Static methods */
 
 /**
  * @inheritdoc ve.ce.Node
