506d4c01f07e6a0ec3114c635fdcbc0bbbc9f7e7
Ammarpad <ammarpad@yahoo.com>
2023-09-20 05:13:34 +0100

    ve.repeatString: Use native implementation
    Significantly faster and well supported.
    
    Change-Id: I4c203c905d2896acade05d8975e3fc146b4b8839

--- src/ce/ve.ce.TextState.js
+++ src/ce/ve.ce.TextState.js
@@ -86,7 +86,7 @@ ve.ce.TextState.static.getChunks = function ( element ) {
 			// otherwise we'll double count this node
 			if ( node === view.$element[ 0 ] ) {
 				// \u2603 is the snowman character: ☃
-				add( ve.repeatString( '\u2603', view.getOuterLength() ) );
+				add( '\u2603'.repeat( view.getOuterLength() ) );
 			}
 		} else if ( node.classList.contains( 've-ce-unicorn' ) ) {
 			add( '', 'unicorn' );
