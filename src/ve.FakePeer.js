fb93ae3cfeb7101239edfc1fcd4e081a27d7873c
David Chan <david@troi.org>
2023-10-31 20:30:08 +0800

    Fake PeerJS class, for convenient debugging.
    Change-Id: Iea91838ec329d8104254adfa75e86adcdcfd1bf7

---
+++ src/ve.FakePeer.js
@@ -0,0 +1,95 @@
+/*!
+ * VisualEditor fake PeerJS class.
+ *
+ * For convenient debugging. Create two FakePeers in one browser window. Then a single
+ * debugger can see all the communication within its call stack.
+ *
+ * @copyright 2011-2023 VisualEditor Team and others; see http://ve.mit-license.org
+ */
+
+ve.FakePeer = function veFakePeer() {
+	this.id = 'p' + this.constructor.static.peers.length;
+	this.constructor.static.peers.push( this );
+	this.connections = [];
+	this.handlers = new Map();
+	Promise.resolve( this.id ).then( this.callHandlers.bind( this, 'open' ) );
+};
+
+/* Initialization */
+
+OO.initClass( ve.FakePeer );
+
+/* Static properties */
+
+ve.FakePeer.static.peers = [];
+
+/* Methods */
+
+ve.FakePeer.prototype.on = function ( ev, f ) {
+	var handlers = this.handlers.get( ev );
+	if ( !handlers ) {
+		handlers = [];
+		this.handlers.set( ev, handlers );
+	}
+	handlers.push( f );
+};
+
+ve.FakePeer.prototype.callHandlers = function ( type ) {
+	var args = Array.prototype.slice.call( arguments, 1 );
+	( this.handlers.get( type ) || [] ).forEach( function ( handler ) {
+		handler.apply( null, args );
+	} );
+};
+
+ve.FakePeer.prototype.connect = function ( id ) {
+	var peer = this.constructor.static.peers.find( function ( peerI ) {
+		return peerI.id === id;
+	} );
+	if ( !peer ) {
+		throw new Error( 'Unknown id: ' + id );
+	}
+	var thisConn = new ve.FakePeerConnection( peer.id + '-' + this.id, peer );
+	var peerConn = new ve.FakePeerConnection( this.id + '-' + peer.id, this );
+	thisConn.other = peerConn;
+	peerConn.other = thisConn;
+	this.connections.push( thisConn );
+	peer.connections.push( peerConn );
+	Promise.resolve( peerConn ).then( peer.callHandlers.bind( peer, 'connection' ) );
+	Promise.resolve( thisConn.id ).then( thisConn.callHandlers.bind( thisConn, 'open' ) );
+	Promise.resolve( peerConn.id ).then( peerConn.callHandlers.bind( peerConn, 'open' ) );
+	return thisConn;
+};
+
+ve.FakePeerConnection = function VeFakePeerConnection( id, peer ) {
+	this.id = id;
+	this.peer = peer;
+	this.other = null;
+	this.handlers = new Map();
+};
+
+OO.initClass( ve.FakePeerConnection );
+
+ve.FakePeerConnection.prototype.on = function ( ev, f ) {
+	var handlers = this.handlers.get( ev );
+	if ( !handlers ) {
+		handlers = [];
+		this.handlers.set( ev, handlers );
+	}
+	handlers.push( f );
+};
+
+ve.FakePeerConnection.prototype.callHandlers = function ( type ) {
+	var args = Array.prototype.slice.call( arguments, 1 );
+	( this.handlers.get( type ) || [] ).forEach( function ( handler ) {
+		handler.apply( null, args );
+	} );
+};
+
+ve.FakePeerConnection.prototype.setOther = function ( other ) {
+	this.other = other;
+	Promise.resolve( this.id ).then( this.callHandlers.bind( this, 'open' ) );
+};
+
+ve.FakePeerConnection.prototype.send = function ( data ) {
+	Promise.resolve( data ).then( this.other.callHandlers.bind( this.other, 'data' ) );
+};
---
+++ tests/ve.FakePeer.test.js
@@ -0,0 +1,55 @@
+/*!
+ * VisualEditor FakePeer tests.
+ *
+ * @copyright 2011-2023 VisualEditor Team and others; see http://ve.mit-license.org
+ */
+
+QUnit.module( 've.FakePeer' );
+
+QUnit.test( 'interactions', function ( assert ) {
+	var log = [];
+	var done = assert.async();
+	var finalCheck = function () {
+		var expectedLog = [
+			'peer0open p0',
+			'peer1open p1',
+			'peer0conn',
+			'conn1open',
+			'conn0open',
+			'conn1data hello'
+		];
+		assert.deepEqual( log, expectedLog, 'log' );
+		done();
+	};
+
+	var mkPeer = function () {
+		// return new Peer( undefined, { host: 'localhost', port: 9000, path: '/myapp' } );
+		return new ve.FakePeer();
+	};
+	var peer0 = mkPeer();
+	peer0.on( 'open', function ( peer0Id ) {
+		log.push( 'peer0open ' + peer0Id );
+		var peer1 = mkPeer();
+		peer1.on( 'open', function ( id ) {
+			log.push( 'peer1open ' + id );
+			var conn1 = peer1.connect( peer0Id );
+			conn1.on( 'open', function () {
+				log.push( 'conn1open' );
+			} );
+			conn1.on( 'data', function ( data ) {
+				log.push( 'conn1data ' + data );
+				finalCheck();
+			} );
+		} );
+	} );
+	peer0.on( 'connection', function ( conn0 ) {
+		log.push( 'peer0conn' );
+		conn0.on( 'open', function () {
+			log.push( 'conn0open' );
+			conn0.send( 'hello' );
+		} );
+		conn0.on( 'data', function ( data ) {
+			log.push( 'conn0data ' + data );
+		} );
+	} );
+} );
