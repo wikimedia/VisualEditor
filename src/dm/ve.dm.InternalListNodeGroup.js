e56e72f0f5079df7e1d31f6353d00b1c6126bcc6
thiemowmde <thiemo.kreuz@wikimedia.de>
2025-07-31 17:17:45 +0200

    Add all necessary public methods to ve.dm.InternalListNodeGroup
    This is a re-submission of I4ebdebf and I9df413a and I6648c44
    (here squashed). The first two got temporarily reverted to unblock
    I9f2a08d. Turns out there was indeed a small mistake in the new
    removeNode method. This is only called from Cite. The mistake was
    hard to spot because of this. Now fixed.
    
    I do my best to keep the new code in InternalListNodeGroup as
    isolated as possible. It should not know anything about the document.
    It might mention "references" in comments but the code should not
    need to know anything about how references actually work.
    
    Some details worth mentioning:
    * With this patch it's almost possible to hide all properties in the
      class as private. I already mark them with a (soft) @private to
      make it easier to migrate existing code.
    * The only exception is when "appendNodeWithKnownIndex" accepts an
      index from a previous serialization. This needs to stay for now.
      All other situations that exposed the index before are not needed
      any more.
    * The old code uses a property "registeredListKey" that is set in
      Cite. VE and the VE lib shouldn't need to know anything about it.
      This patch solves this circular dependency.
    
    Bug: T397145
    Change-Id: I46d9e9a5f6f319e3cf7482e9a78353b6408625b6

--- src/dm/ve.dm.InternalListNodeGroup.js
+++ src/dm/ve.dm.InternalListNodeGroup.js
@@ -11,6 +11,7 @@
  */
 ve.dm.InternalListNodeGroup = function VeDmInternalListNodeGroup() {
 	/**
+	 * @private please do not use directly
 	 * @property {Object.<string,ve.dm.Node[]>} keyedNodes Indexed by the internal listKey.
 	 *
 	 * Practically, one of these arrays can contain multiple elements when a reference (with the
@@ -19,18 +20,24 @@ ve.dm.InternalListNodeGroup = function VeDmInternalListNodeGroup() {
 	this.keyedNodes = {};
 
 	/**
-	 * @property {ve.dm.Node[]} firstNodes When {@link keyedNodes} contains more than one node per
-	 * listKey then firstNodes can be used to identify the node that appears first in the document.
-	 * If there is only one node it's just that node. Array keys correspond to the values in the
-	 * {@link indexOrder} array. Order is meaningless but dictated by indexOrder instead.
+	 * @private please do not use directly
+	 * @property {Array.<ve.dm.Node|undefined>} firstNodes When {@link keyedNodes} contains more
+	 * than one node per listKey then firstNodes can be used to identify the node that appears first
+	 * in the document. If there is only one node it's just that node. Array keys correspond to the
+	 * values in the {@link indexOrder} array. Order is meaningless but dictated by indexOrder
+	 * instead.
 	 *
 	 * Practically, this is the first occurence of a reused reference (with the same group and name)
 	 * in a document. That document position dictates the reference's footnote number and the order
 	 * in which references are rendered in their reference list.
+	 *
+	 * Note this is possibly a sparse array with elements missing in case initialization happened
+	 * out of order. Skip these and use {@link indexOrder} as your primary source of truth.
 	 */
 	this.firstNodes = [];
 
 	/**
+	 * @private please do not use directly
 	 * @property {number[]} indexOrder Sorted to reflect the order of first appearance in the
 	 * document. Values are indexes for the {@link firstNodes} array.
 	 *
@@ -52,6 +59,190 @@ ve.dm.InternalListNodeGroup = function VeDmInternalListNodeGroup() {
 	 * used. The values are meaningless.
 	 */
 	this.uniqueListKeysInUse = {};
+};
+
+/**
+ * @return {boolean}
+ */
+ve.dm.InternalListNodeGroup.prototype.isEmpty = function () {
+	// TODO: Using this.indexOrder.length would be cheaper, but at this point we cannot be sure the
+	// internal data structures are intact.
+	return Object.keys( this.keyedNodes ).length === 0;
+};
+
+/**
+ * @param {string} key
+ * @return {ve.dm.Node[]|undefined} All nodes (1 or more, never 0) that (re)use the same key.
+ *  Undefined when the key is unknown.
+ */
+ve.dm.InternalListNodeGroup.prototype.getAllReuses = function ( key ) {
+	return this.keyedNodes[ key ];
+};
+
+/**
+ * @return {string[]}
+ */
+ve.dm.InternalListNodeGroup.prototype.getKeysInIndexOrder = function () {
+	const remainingKeys = Object.keys( this.keyedNodes );
+	return this.getFirstNodesInIndexOrder().map(
+		// FIXME: This should be a fast lookup, but we currently don't have a map for that
+		( node ) => remainingKeys.find( ( key, i ) => {
+			// TODO: Can we be sure the first node via this.firstNodes is always at position 0?
+			// If this is guaranteed we can replace this search with a single comparison.
+			if ( this.keyedNodes[ key ].includes( node ) ) {
+				// Performance optimization: Don't search again for the key we just found
+				remainingKeys.splice( i, 1 );
+				return true;
+			}
+			return false;
+		} )
+	);
+};
+
+/**
+ * @return {ve.dm.Node[]}
+ */
+ve.dm.InternalListNodeGroup.prototype.getFirstNodesInIndexOrder = function () {
+	const nodes = this.indexOrder.length > 1 ?
+		this.indexOrder.map( ( i ) => this.firstNodes[ i ] ) :
+		// Fallback in case there is nothing to sort or indexOrder got messed up
+		this.firstNodes;
+	// We need to filter non-existing values from non-sequential arrays, see tests
+	return nodes.filter( ( n ) => n );
+};
+
+/**
+ * @param {string} key
+ * @return {ve.dm.Node|undefined} Undefined in case there are no known nodes with this key
+ */
+ve.dm.InternalListNodeGroup.prototype.getFirstNode = function ( key ) {
+	const nodes = this.getAllReuses( key );
+	if ( !nodes ) {
+		return undefined;
+	}
+	// FIXME: This should be a fast lookup, but we currently don't have a map for that
+	for ( const node in this.firstNodes ) {
+		// TODO: Can we be sure the first node is at position 0? If this is guaranteed we can
+		// replace this search with a single comparison.
+		if ( nodes.includes( node ) ) {
+			return node;
+		}
+	}
+	// Fallback in case there is something wrong
+	return nodes[ 0 ];
+};
+
+/**
+ * Sort the indexOrder array within a group object.
+ *
+ * Items are sorted by the start offset of their firstNode, unless that node
+ * has the 'placeholder' attribute, in which case it moved to the end of the
+ * list, where it should be ignored.
+ */
+ve.dm.InternalListNodeGroup.prototype.sortGroupIndexes = function () {
+	this.indexOrder.sort( ( index1, index2 ) => {
+		const node1 = this.firstNodes[ index1 ];
+		const node2 = this.firstNodes[ index2 ];
+		// Sometimes there is no node at the time of sorting (T350902) so move these to the end to be ignored
+		if ( !node1 ) {
+			return !node2 ? 0 : 1;
+		} else if ( !node2 ) {
+			return -1;
+		}
+		// Sort placeholder nodes to the end, so they don't interfere with numbering
+		if ( node1.getAttribute( 'placeholder' ) ) {
+			return node2.getAttribute( 'placeholder' ) ? 0 : 1;
+		} else if ( node2.getAttribute( 'placeholder' ) ) {
+			return -1;
+		}
+		return node1.getRange().start - node2.getRange().start;
+	} );
+};
+
+/**
+ * @param {string} key
+ * @param {ve.dm.Node} newNode New node to append to the end of the list of nodes with the same key
+ */
+ve.dm.InternalListNodeGroup.prototype.appendNode = function ( key, newNode ) {
+	this.appendNodeWithKnownIndex( key, newNode, this.firstNodes.length );
+};
+
+/**
+ * @param {string} key
+ * @param {ve.dm.Node} newNode
+ * @param {number} index Existing index; ignored when this is not the first node for this key
+ */
+ve.dm.InternalListNodeGroup.prototype.appendNodeWithKnownIndex = function ( key, newNode, index ) {
+	if ( !( key in this.keyedNodes ) ) {
+		this.keyedNodes[ key ] = [];
+		// This is literally the first node, so record it as such
+		this.firstNodes[ index ] = newNode;
+		this.indexOrder.push( index );
+	}
+	this.keyedNodes[ key ].push( newNode );
+};
+
+/**
+ * @param {string} key
+ * @param {ve.dm.Node} newNode Node to insert at document position
+ */
+ve.dm.InternalListNodeGroup.prototype.insertNodeInDocumentOrder = function ( key, newNode ) {
+	const nodes = this.getAllReuses( key );
+	// Fall back to the cheaper method if possible
+	if ( !nodes ) {
+		this.appendNode( key, newNode );
+		return;
+	}
+
+	const start = newNode.getRange().start;
+	let i = 0;
+	// Warning, this assumes the nodes array is in document order!
+	while ( nodes[ i ] && nodes[ i ].getRange().start > start ) {
+		i++;
+	}
+
+	// Check if the old node we are going to move was a first node
+	const firstNodeIndex = this.firstNodes.indexOf( nodes[ i ] );
+	if ( firstNodeIndex !== -1 ) {
+		this.firstNodes[ firstNodeIndex ] = newNode;
+	}
+
+	// Finally insert the new node and push all following nodes one down
+	nodes.splice( i, 0, newNode );
+};
+
+/**
+ * @param {string} key
+ * @param {ve.dm.Node} node
+ */
+ve.dm.InternalListNodeGroup.prototype.unsetNode = function ( key, node ) {
+	const nodes = this.getAllReuses( key );
+	if ( !nodes ) {
+		return;
+	}
+
+	let i = nodes.indexOf( node );
+	if ( i !== -1 ) {
+		nodes.splice( i, 1 );
+		if ( !nodes.length ) {
+			delete this.keyedNodes[ key ];
+		}
+	}
+
+	// This is extra defensive for the moment because we have no control over all callers
+	i = this.firstNodes.indexOf( node );
+	if ( i !== -1 ) {
+		this.firstNodes[ i ] = nodes[ 0 ];
+		if ( !nodes.length ) {
+			// This intentionally leaves a gap in the array behind. Needed so that the numbers of
+			// the other elements don't change.
+			delete this.firstNodes[ i ];
+			i = this.indexOrder.indexOf( i );
+			if ( i !== -1 ) {
+				this.indexOrder.splice( i, 1 );
+			}
+		}
+	}
 };
 
 /**
