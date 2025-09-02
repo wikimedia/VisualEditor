46eacfcd27637cf7070b5bf6c4b78a9dd62df33d
David Chan <david@troi.org>
2025-09-02 17:24:50 +0100

    TinyVE: Add tinyve.dm.ContentBranchNode
    Bug: T398201
    Change-Id: Ia4e3f66792c98d2d60970e17c0d55776ce1a8dbb

---
+++ tinyve/tinyve.dm.ContentBranchNode.js
@@ -0,0 +1,29 @@
+/**
+ * TinyVE DM ContentBranchNode - Node that contains linearized content, e.g. paragraph
+ *
+ * This is a toy version of ve.dm.ContentBranchNode, which illustrates the main concepts
+ */
+
+/**
+ * A ContentBranchNode in the DM tree.
+ *
+ * In real VE, each branch node type has its own subclass of `ve.dm.ContentBranchNode`, and the
+ * subclasses are instantiated through a node factory so extensions can at types.
+ *
+ * @class
+ * @see {ve.dm.ContentBranchNode}
+ * @see {ve.dm.NodeFactory}
+ *
+ * @constructor
+ * @param {string} type The node type, e.g. 'p' or 'h1'
+ * @param {tinyve.dm.BranchNode|null} [parent] The parent node, if any
+ */
+tinyve.dm.ContentBranchNode = function TinyVeDmContentBranchNode( type, parent = null ) {
+	tinyve.dm.ContentBranchNode.super.call( this, type, parent );
+};
+
+OO.inheritClass( tinyve.dm.ContentBranchNode, tinyve.dm.BranchNode );
+
+tinyve.dm.ContentBranchNode.prototype.canContainContent = function () {
+	return true;
+};
