b0349cf6fa1d144ba2287a29350e97b12b7228c8
David Chan <david@troi.org>
2025-09-02 17:56:55 +0100

    TinyVE: Add tinyve.ce.ContentBranchNode
    Change-Id: I505d18c7d01cafac15750c3f5e9f319202488c43

---
+++ tinyve/tinyve.ce.ContentBranchNode.js
@@ -0,0 +1,89 @@
+/**
+ * TinyVE CE Content branch node
+ *
+ * This is a toy version of ve.ce.ContentBranchNode and subclasses, which illustrates the main concepts
+ */
+
+/**
+ * A Content branch node in the CE tree.
+ *
+ * This is a tree of nodes that own ContentEditable DOM nodes. There is one CE node for each
+ * DM node.
+ *
+ * In real VE, each node type has its own subclass of `ve.ce.ContentBranchNode`, and the
+ * subclasses are instantiated through a node factory so extensions can at types.
+ *
+ * @class
+ * @see {ve.ce.ContentBranchNode}
+ * @see {ve.ce.NodeFactory}
+ *
+ * @constructor
+ * @param {tinyve.dm.ContentBranchNodeNode} model Model for which this object is a view
+ * @param {tinyve.ce.Surface} surface Surface view to which this object belongs
+ */
+tinyve.ce.ContentBranchNode = function TinyVeCeContentBranchNode( model, surface ) {
+	// Parent constructor
+	tinyve.ce.ContentBranchNode.super.call( this, model, surface );
+
+	this.$element.addClass( 'tinyve-ce-ContentBranchNode' );
+};
+
+/* Inheritance */
+
+OO.inheritClass( tinyve.ce.ContentBranchNode, tinyve.ce.BranchNode );
+
+/* Methods */
+
+tinyve.ce.ContentBranchNode.prototype.initialize = function () {
+	this.$element[ 0 ].innerHTML = this.getRenderedContents();
+};
+
+/**
+ * Get an HTML rendering of the contents
+ *
+ * In full VE, `ve.ce.ContentBranchNode#getRenderedContents` returns a new DOM node, not a string
+ *
+ * @return {string} HTML rendering of the contents
+ */
+tinyve.ce.ContentBranchNode.prototype.getRenderedContents = function () {
+	const range = this.model.getRange();
+	const linearData = this.surface.model.documentModel.data.slice( range.start, range.end );
+	const html = [];
+
+	function getCloseTag( openTag ) {
+		return openTag.replace( '<', '</' ).replace( /\s.*>/, '>' );
+	}
+
+	function closeAnnotations( anns ) {
+		anns.forEach( ( ann ) => {
+			html.push( getCloseTag( ann ) );
+		} );
+	}
+	function openAnnotations( anns ) {
+		anns.forEach( ( ann ) => {
+			html.push( ann );
+		} );
+	}
+	let currentAnnotations = [];
+	linearData.forEach( ( item ) => {
+		let character;
+		let annotations;
+		if ( Array.isArray( item ) ) {
+			[ character, annotations ] = item;
+		} else {
+			character = item;
+			annotations = [];
+		}
+		// Find how many annotations match
+		let i = 0;
+		while ( i < annotations.length && annotations[ i ] === currentAnnotations[ i ] ) {
+			i++;
+		}
+		closeAnnotations( currentAnnotations.slice( i ) );
+		openAnnotations( annotations.slice( i ) );
+		currentAnnotations = annotations;
+		html.push( character );
+	} );
+	closeAnnotations( currentAnnotations );
+	return html.join( '' );
+};
