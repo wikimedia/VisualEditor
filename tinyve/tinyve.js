06290085ec4611586a71599b9bb1121a1544b443
David Chan <david@troi.org>
2025-09-02 18:13:38 +0100

    TinyVE: Add tinyve.init and tinyve.Target
    Change-Id: If24f80a3dd7e68c5c983187284e67b32c76ff9a5

--- tinyve/tinyve.js
+++ tinyve/tinyve.js
@@ -23,3 +23,32 @@ tinyve.Range = function TinyVeRange( from, to ) {
 	this.start = Math.min( from, to );
 	this.end = Math.max( from, to );
 };
+
+tinyve.Target = function TinyVeTarget( surface, config = {} ) {
+	// Parent constructor
+	tinyve.Target.super.apply( this, config );
+
+	this.surface = surface;
+	this.$element.addClass( 'tinyve-Target' );
+	this.$element.append( surface.$element );
+};
+
+OO.inheritClass( tinyve.Target, OO.ui.Element );
+
+tinyve.init = function ( $container, html ) {
+	const domDocument = new DOMParser().parseFromString( html, 'text/html' );
+	const linearData = tinyve.dm.converter.getDataFromDomSubtree( domDocument.body );
+	const documentModel = new tinyve.dm.Document( linearData );
+	const surfaceModel = new tinyve.dm.Surface( documentModel );
+	const surface = new tinyve.ui.Surface( surfaceModel );
+	const target = tinyve.init.target = new tinyve.Target( surface );
+	$container.empty().append( target.$element );
+
+	// Set up a global variable, for easy debugging.
+	// Full VE has globals like this, so you can do things like this in the console:
+	//
+	// ve.init.target.surface.setReadOnly( true )
+	// ve.init.target.surface.model.documentModel.data.getData()
+	// ve.init.target.surface.view.$documentNode.find( '.ve-ce-annotation' )
+	tinyve.init.target = target;
+};
