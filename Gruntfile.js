20aca43f21fb55802c2dc26d42f37f7586cd8cc1
James D. Forrester <jforrester@wikimedia.org>
2025-10-06 11:58:33 -0400

    build: Raise coverage requirements to current standards; update over-rides
    Some of the over-ridden files now pass, or no longer exist.
    
    Change-Id: I649e957ca77b03d75db81b9c44fef89a32054b1e

--- Gruntfile.js
+++ Gruntfile.js
@@ -428,38 +428,37 @@ module.exports = function ( grunt ) {
 					],
 					// https://github.com/karma-runner/karma-coverage/blob/v1.1.2/docs/configuration.md#check
 					check: {
-						global: coverAll( 60 ),
+						global: coverAll( 73 ),
 						each: {
 							functions: 20,
 							branches: 20,
 							statements: 20,
 							lines: 20,
 							excludes: [
-								'rebaser/src/dm/ve.dm.DocumentStore.js',
-								'rebaser/src/dm/ve.dm.PeerTransportServer.js',
 								'rebaser/src/dm/ve.dm.ProtocolServer.js',
 								'rebaser/src/dm/ve.dm.RebaseDocState.js',
-								'rebaser/src/dm/ve.dm.TransportServer.js',
 								'src/ve.track.js',
-								'src/ve.ext-peer.js',
 								'src/init/**/*.js',
 								// DM
+								'src/dm/ve.dm.Converter.js',
 								'src/dm/ve.dm.InternalList.js',
 								'src/dm/ve.dm.InternalListNodeGroup.js',
+								'src/dm/ve.dm.NodeFactory.js',
 								'src/dm/ve.dm.SourceSurfaceFragment.js',
 								'src/dm/ve.dm.SurfaceSynchronizer.js',
 								'src/dm/ve.dm.TableSlice.js',
 								'src/dm/annotations/ve.dm.CommentAnnotation.js',
+								// TODO: Fix AlienMetaItem for 100% coverage
+								'src/dm/metaitems/ve.dm.AlienMetaItem.js',
+								'src/dm/nodes/ve.dm.DocumentNode.js',
 								'src/dm/nodes/ve.dm.GeneratedContentNode.js',
-								'src/dm/nodes/ve.dm.HeadingNode.js',
 								'src/dm/nodes/ve.dm.ImageNode.js',
 								'src/dm/nodes/ve.dm.InternalItemNode.js',
+								'src/dm/nodes/ve.dm.SectionNode.js',
 								// CE
 								'src/ce/ve.ce.DragDropHandler.js',
 								'src/ce/ve.ce.SelectionManager.js',
 								'src/ce/ve.ce.SurfaceSynchronizer.js',
-								'src/ce/annotations/ve.ce.DeleteAnnotation.js',
-								'src/ce/annotations/ve.ce.InsertAnnotation.js',
 								'src/ce/nodes/ve.ce.CheckListItemNode.js',
 								'src/ce/nodes/ve.ce.GeneratedContentNode.js',
 								'src/ce/nodes/ve.ce.InternalItemNode.js',
@@ -485,15 +484,14 @@ module.exports = function ( grunt ) {
 								// TODO: Fix a few cases for 80% coverage
 								'src/*.js': coverAll( 50 ),
 								// DM
-								'src/dm/*.js': coverAll( 50 ),
+								'src/dm/*.js': coverAll( 75 ),
 								'src/dm/annotations/*.js': coverAll( 100 ),
 								'src/dm/lineardata/*.js': coverAll( 95 ),
-								// TODO: Fix AlienMetaItem for 100% coverage
-								'src/dm/metaitems/*.js': coverAll( 50 ),
+								'src/dm/metaitems/*.js': coverAll( 100 ),
 								// TODO: Fix a few cases for 80% coverage
-								'src/dm/nodes/*.js': coverAll( 50 ),
+								'src/dm/nodes/*.js': coverAll( 70 ),
 								// TODO: Fix a few cases for 95% coverage
-								'src/dm/selections/*.js': coverAll( 50 ),
+								'src/dm/selections/*.js': coverAll( 80 ),
 								// CE
 								'src/ce/*.js': coverAll( 50 ),
 								// TODO: Fix a few cases for 80% coverage
