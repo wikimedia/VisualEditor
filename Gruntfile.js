714b3ae07433f5852c1ef83e83e65c6c620fb607
Ed Sanders <esanders@wikimedia.org>
2025-10-15 16:03:42 +0100

    Move some Karma config to karma.conf.js
    Change-Id: I21a93e30ae97767077f98c7b7af2124b1b2a22f8

--- Gruntfile.js
+++ Gruntfile.js
@@ -50,21 +50,6 @@ module.exports = function ( grunt ) {
 	// We want to use `grunt watch` to start this and karma watch together.
 	grunt.renameTask( 'watch', 'runwatch' );
 
-	/**
-	 * Build an object of required coverage percentages
-	 *
-	 * @param {number} pc Percentage coverage required (for all aspects)
-	 * @return {Object} required coverage percentages
-	 */
-	function coverAll( pc ) {
-		return {
-			functions: pc,
-			branches: pc,
-			statements: pc,
-			lines: pc
-		};
-	}
-
 	function fixPaths( src, filepath ) {
 		const makeRelative = ( target ) => {
 			const currentDir = path.dirname( path.resolve( filepath ) );
@@ -418,94 +403,7 @@ module.exports = function ( grunt ) {
 					'src/**/*.js': [ 'coverage' ]
 				},
 				reporters: [ 'mocha', 'coverage' ],
-				coverageReporter: {
-					dir: 'coverage/',
-					subdir: '.',
-					reporters: [
-						{ type: 'clover' },
-						{ type: 'html' },
-						{ type: 'text-summary' }
-					],
-					// https://github.com/karma-runner/karma-coverage/blob/v1.1.2/docs/configuration.md#check
-					check: {
-						global: coverAll( 73 ),
-						each: {
-							functions: 20,
-							branches: 20,
-							statements: 20,
-							lines: 20,
-							excludes: [
-								'rebaser/src/dm/ve.dm.ProtocolServer.js',
-								'rebaser/src/dm/ve.dm.RebaseDocState.js',
-								'src/ve.track.js',
-								'src/init/**/*.js',
-								// DM
-								'src/dm/ve.dm.Converter.js',
-								'src/dm/ve.dm.InternalList.js',
-								'src/dm/ve.dm.InternalListNodeGroup.js',
-								'src/dm/ve.dm.SourceSurfaceFragment.js',
-								'src/dm/ve.dm.SurfaceSynchronizer.js',
-								'src/dm/ve.dm.TableSlice.js',
-								'src/dm/annotations/ve.dm.CommentAnnotation.js',
-								// TODO: Fix AlienMetaItem for 100% coverage
-								'src/dm/metaitems/ve.dm.AlienMetaItem.js',
-								'src/dm/nodes/ve.dm.DocumentNode.js',
-								'src/dm/nodes/ve.dm.GeneratedContentNode.js',
-								'src/dm/nodes/ve.dm.ImageNode.js',
-								'src/dm/nodes/ve.dm.InternalItemNode.js',
-								'src/dm/nodes/ve.dm.SectionNode.js',
-								// CE
-								'src/ce/ve.ce.DragDropHandler.js',
-								'src/ce/ve.ce.SelectionManager.js',
-								'src/ce/ve.ce.SurfaceSynchronizer.js',
-								'src/ce/nodes/ve.ce.CheckListItemNode.js',
-								'src/ce/nodes/ve.ce.GeneratedContentNode.js',
-								'src/ce/nodes/ve.ce.InternalItemNode.js',
-								'src/ce/keydownhandlers/ve.ce.TableDeleteKeyDownHandler.js',
-								// UI
-								'src/ui/*.js',
-								'src/ui/actions/*.js',
-								'src/ui/commands/*.js',
-								'src/ui/contextitems/*.js',
-								'src/ui/contexts/*.js',
-								'src/ui/datatransferhandlers/*.js',
-								'src/ui/dialogs/*.js',
-								'src/ui/inspectors/ve.ui.CommentAnnotationInspector.js',
-								'src/ui/inspectors/ve.ui.FragmentInspector.js',
-								'src/ui/layouts/*.js',
-								'src/ui/pages/*.js',
-								'src/ui/tools/*.js',
-								'src/ui/widgets/*.js',
-								'src/ui/windowmanagers/*.js'
-							],
-							overrides: {
-								// Core
-								// TODO: Fix a few cases for 80% coverage
-								'src/*.js': coverAll( 50 ),
-								// DM
-								'src/dm/*.js': coverAll( 75 ),
-								'src/dm/annotations/*.js': coverAll( 100 ),
-								'src/dm/lineardata/*.js': coverAll( 95 ),
-								'src/dm/metaitems/*.js': coverAll( 100 ),
-								// TODO: Fix a few cases for 80% coverage
-								'src/dm/nodes/*.js': coverAll( 70 ),
-								// TODO: Fix a few cases for 95% coverage
-								'src/dm/selections/*.js': coverAll( 80 ),
-								// CE
-								'src/ce/*.js': coverAll( 50 ),
-								// TODO: Fix a few cases for 80% coverage
-								'src/ce/annotations/*.js': coverAll( 50 ),
-								'src/ce/keydownhandlers/*.js': coverAll( 80 ),
-								'src/ce/nodes/*.js': coverAll( 50 ),
-								// TODO: Fix a few cases for 80% coverage
-								'src/ce/selections/*.js': coverAll( 50 ),
-								// UI
-								'src/ui/elements/*.js': coverAll( 50 ),
-								'src/ui/inspectors/*.js': coverAll( 50 )
-							}
-						}
-					}
-				}
+				configFile: 'karma.conf.js'
 			},
 			// Separate job for Firefox as we don't want a second coverage report.
 			firefox: {
